c514893ad42528733cd8acb6e5a293db29bdb086
Alessandro Caproni <acaproni@eso.org>
2017-02-23 11:36:32 +0100

    Improves the comment

--- Cdb/src/java/org/eso/ias/cdb/json/pojos/package-info.java
+++ Cdb/src/java/org/eso/ias/cdb/json/pojos/package-info.java
@@ -1,9 +1,10 @@
 /**
  * A package for the pojos used by Jakson2 libraries.
- * They differ from rdb pojos because we do not want to have recursion 
- * at this level that would make JSON files very long and unreadable.
+ * They differ from rdb pojos (org.eso.ias.cdb.pojos) 
+ * because they use the IDs instead of including objects directly
+ * that would make JSON files very long and unreadable.
  * 
- * Instead of embedding objecs (for example the ASCEs running in a DASU),
+ * In short, instead of embedding objects (for example the ASCEs running in a DASU),
  * we want to have only their IDs in JSON files.
  * 
  * @author acaproni
