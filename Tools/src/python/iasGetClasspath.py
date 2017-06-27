1b624d6336e934f5eb525b0088e206aa3d54be81
Alessandro Caproni <acaproni@eso.org>
2017-06-27 15:38:03 +0200

    Prints the classpath in the stdout

---
+++ Tools/src/python/iasGetClasspath.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+'''
+Writes the classpath in the stdout
+
+@author: acaproni
+'''
+
+from IASTools.CommonDefs import CommonDefs
+
+if __name__ == '__main__':
+    print CommonDefs.buildClasspath()
