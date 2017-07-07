86d0cc65b388c47ccf1eeada3b72e7e460398e75
Alessandro Caproni <acaproni@eso.org>
2017-07-07 11:13:57 +0200

    Builds java and scala API documentation (python still missing)
    iasBuildApiDocs build the java and scala API documentation in the passed
    folder. Scala API docs in a scala subdirectory and java API docs in a
    java subfolder.
    The script delegates the execution of javadoc and scaladoc to the
    objects in the IASApiDocs python module.
    Scala documentation includes also java as it can be more convenient to
    have all the documentation under the same web page.
    Generation of python API documentation still missing.

---
+++ Tools/src/python/iasBuildApiDocs.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+'''
+Build API documentation for java, scal and python
+Created on Jul 7, 2017
+
+@author: acaproni
+'''
+
+import sys
+from optparse import OptionParser
+from IASApiDocs.ScaladocBuilder import ScaladocBuilder
+from IASApiDocs.JavadocBuilder import JavadocBuilder
+from os.path import join
+
+if __name__ == '__main__':
+    # Parse the command line
+    parser = OptionParser()
+    parser.add_option("-d", "--destFolder", help="HTML destination folder", action="store", type="string", dest="destFolder")
+    parser.add_option("-s", "--sourceFolder", help="IAS source folder", action="store", type="string", dest="srcFolder")
+    (options, args) = parser.parse_args()
+    
+    if not options.destFolder:
+        print "No destination folder given"
+        sys.exit(-1)
+    else:
+        print "API documentation will be generated in",options.destFolder
+    
+    if not options.srcFolder:
+        print "No source folder given"
+        sys.exit(-1)
+    else:
+        print "Reading sources from",options.srcFolder
+        
+    # Build scala documentation
+    print "Building scaladoc"
+    scalaBuilder = ScaladocBuilder(options.srcFolder,join(options.destFolder,"scala"))
+    scalaBuilder.buildScaladocs()
+    
+    print "Building javadoc"
+    javaBuilder = JavadocBuilder(options.srcFolder,join(options.destFolder,"java"))
+    javaBuilder.buildJavadocs()
