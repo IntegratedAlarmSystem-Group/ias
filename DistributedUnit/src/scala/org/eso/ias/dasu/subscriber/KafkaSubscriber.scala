fdb2445f6d65f8cc2a6e070183c7e125db942214
Alessandro Caproni <acaproni@eso.org>
2017-11-30 17:57:33 +0100

    Uses the DASU ID as kafka group identifier (group.id)

--- DistributedUnit/src/scala/org/eso/ias/dasu/subscriber/KafkaSubscriber.scala
+++ DistributedUnit/src/scala/org/eso/ias/dasu/subscriber/KafkaSubscriber.scala
@@ -32,6 +32,8 @@ extends KafkaConsumerListener with InputSubscriber {
   require(Option(topicName).isDefined && !dasuId.isEmpty())
   require(Option(props).isDefined)
   
+  props.setProperty("group.id", dasuId)
+  
   /** The logger */
   private val logger = IASLogger.getLogger(this.getClass)
   
