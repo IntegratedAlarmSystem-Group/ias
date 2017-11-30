a1c590126f546df3ca0c4587346bbbb20ffebd37
Alessandro Caproni <acaproni@eso.org>
2017-11-30 18:00:36 +0100

    Adds a test if a DASu with kafka publisher and subscribers
    The test does not pass: the DASU gets the IASIO in input and produce the
    output but only the DASU listener is getting the event from the kafka
    queue.

--- DistributedUnit/test/runTests.sh
+++ DistributedUnit/test/runTests.sh
@@ -3,3 +3,4 @@ iasRun.py -l s org.scalatest.run org.eso.ias.dasu.test.TopologyTest
 iasRun.py -l s org.scalatest.run org.eso.ias.dasu.test.DasuOneASCETest
 iasRun.py -l s org.scalatest.run org.eso.ias.dasu.test.Dasu7ASCEsTest
 iasRun.py -l s org.scalatest.run org.eso.ias.dasu.test.JsonPublisherTest
+iasRun.py -l s org.scalatest.run org.eso.ias.dasu.test.KafkaDasuWithKafka
