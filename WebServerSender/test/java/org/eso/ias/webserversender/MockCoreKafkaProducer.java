3248c205e37d001423f53626ffd6a91c22a8e4b7
Sebastian <sebafehlandt@gmail.com>
2017-12-14 18:54:39 -0300

    Change MockCoreKafkaProducer in WebServerSender in order to send JSONs of Alarms instead of booleans

--- WebServerSender/test/java/org/eso/ias/webserversender/MockCoreKafkaProducer.java
+++ WebServerSender/test/java/org/eso/ias/webserversender/MockCoreKafkaProducer.java
@@ -14,8 +14,8 @@ public class MockCoreKafkaProducer {
 		SimpleStringProducer producer = new SimpleStringProducer("localhost:9092", "test", "PID1");
 		producer.setUp();
 
-		String msg2 = "{\"value\":\"true\",\"tStamp\":1600,\"mode\":\"OPERATIONAL\",\"id\":\"BooleanType-IDD\",\"fullRunningId\":\"(Monitored-System-ID:MONITORED_SOFTWARE_SYSTEM)@(plugin-ID:PLUGIN)@(Converter-ID:CONVERTER)@(BooleanType-ID:IASIO)\",\"valueType\":\"BOOLEAN\"}";
-		String msg1 = "{\"value\":\"false\",\"tStamp\":1600,\"mode\":\"MAINTENANCE\",\"id\":\"BooleanType-IDD\",\"fullRunningId\":\"(Monitored-System-ID:MONITORED_SOFTWARE_SYSTEM)@(plugin-ID:PLUGIN)@(Converter-ID:CONVERTER)@(BooleanType-ID:IASIO)\",\"valueType\":\"BOOLEAN\"}";
+    String msg2 = "{\"value\":\"SET\",\"tStamp\":1600,\"mode\":\"OPERATIONAL\",\"id\":\"AlarmType-ID\",\"fullRunningId\":\"(Monitored-System-ID:MONITORED_SOFTWARE_SYSTEM)@(plugin-ID:PLUGIN)@(Converter-ID:CONVERTER)@(AlarmType-ID:IASIO)\",\"valueType\":\"ALARM\"}";
+    String msg1 = "{\"value\":\"CLEARED\",\"tStamp\":1600,\"mode\":\"MAINTENANCE\",\"id\":\"AlarmType-ID\",\"fullRunningId\":\"(Monitored-System-ID:MONITORED_SOFTWARE_SYSTEM)@(plugin-ID:PLUGIN)@(Converter-ID:CONVERTER)@(AlarmType-ID:IASIO)\",\"valueType\":\"ALARM\"}";
 
 		String aux;
 
