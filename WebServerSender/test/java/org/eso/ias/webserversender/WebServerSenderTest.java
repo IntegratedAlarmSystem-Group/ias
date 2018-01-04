74edecbf433841c4e89288afc11ed9483536481f
Jazmine <jamaldonadof@ug.uchile.cl>
2018-01-04 18:19:43 -0300

    Update of tests using CountDownLatch properly (Work in Progress)

--- WebServerSender/test/java/org/eso/ias/webserversender/WebServerSenderTest.java
+++ WebServerSender/test/java/org/eso/ias/webserversender/WebServerSenderTest.java
@@ -3,6 +3,7 @@ package org.eso.ias.webserversender;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.eso.ias.webserversender.WebServerSender;
 import org.eclipse.jetty.server.Server;
@@ -15,50 +16,84 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WebServerSenderTest {
-private WebServerSender webServerSender;
-private String[] messages;
-private int messagesNumber;
-
-private final CountDownLatch closeLatch = new CountDownLatch(1);
-
-	public void runProducer() throws KafkaUtilsException {
-		SimpleStringProducer producer = new SimpleStringProducer("localhost:9092", "test", "PID1");
-		producer.setUp();
-
-		for (int i = 0; i < messagesNumber; i++) {
-			try {
-				Thread.sleep(100);
-				String msg = messages[i] + Long.toString(System.currentTimeMillis()) + "}";
-				producer.push(msg, null, msg);
-				producer.flush();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+	private WebServerSender webServerSender;
+	private String[] messages;
+	private int messagesNumber;
+	private Thread sender;
+	private Thread counter;
+	
+	private final CountDownLatch closeLatch = new CountDownLatch(1);
+//
+//	public void runProducer() throws KafkaUtilsException {
+//		SimpleStringProducer producer = new SimpleStringProducer("localhost:9092", "test", "PID1");
+//		producer.setUp();
+//
+//		for (int i = 0; i < messagesNumber; i++) {
+//			try {
+//				Thread.sleep(100);
+//				String msg = messages[i] + Long.toString(System.currentTimeMillis()) + "}";
+//				producer.push(msg, null, msg);
+//				producer.flush();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+//
+	public void runWebServerSender(String kafkaTopic, String webserverUri) throws InterruptedException {
+		webServerSender = new WebServerSender("WebServerSender", kafkaTopic, webserverUri);
+		sender = new Thread() {
+			public void run() {
+				try {
+					webServerSender.run();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
 			}
-		}
-	}
-
-	public void runWebServerSender(String kafkaTopic, String webserverUri) {
-		this.webServerSender = new WebServerSender("WebServerSender", kafkaTopic, webserverUri);
-		this.webServerSender.run();
-	}
-
-	public void runWebsocketServer() throws Exception {
-		Server server = new Server(8080);
-        WebSocketHandler wsHandler = new WebSocketHandler() {
-            @Override
-            public void configure(WebSocketServletFactory factory) {
-                factory.register(MyWebSocketHandler.class);
-            }
-        };
-        server.setHandler(wsHandler);
-        server.start();
-        server.join();
+		};
+		System.out.println("****************************Initializing sender!!");
+		sender.start();
+		System.out.println("****************************After sender!!");
+		TimeUnit.MILLISECONDS.sleep(10000);
+		closeLatch.countDown();
 	}
 	
-	@Before
-	public void setUp() throws Exception {
-		this.runWebsocketServer();
-	}
+//	public void initCounter(int millis) throws InterruptedException {
+//		counter = new Thread() {
+//			public void run() {
+//				try {
+//				TimeUnit.MILLISECONDS.sleep(millis);
+//				closeLatch.countDown();
+//				sender.join();
+//				}
+//				catch(Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//		System.out.println("***********************Initializing Counter!!");
+//		counter.start();
+//		
+//	}
+//
+//	public void runWebsocketServer() throws Exception {
+//		Server server = new Server(8080);
+//        WebSocketHandler wsHandler = new WebSocketHandler() {
+//            @Override
+//            public void configure(WebSocketServletFactory factory) {
+//                factory.register(MyWebSocketHandler.class);
+//            }
+//        };
+//        server.setHandler(wsHandler);
+//        server.start();
+//        server.join();
+//	}
+	
+//	@Before
+//	public void setUp() throws Exception {
+//		this.runWebsocketServer();
+//	}
 
 	@Test
 	public void testWebServerSender() throws Exception {
@@ -72,10 +107,16 @@ private final CountDownLatch closeLatch = new CountDownLatch(1);
 		this.messages[5] = "{\"value\":\"CLEARED\",\"mode\":\"MAINTENANCE\",\"iasValidity\":\"RELIABLE\",\"id\":\"AlarmType-ID\",\"fullRunningId\":\"(Monitored-System-ID:MONITORED_SOFTWARE_SYSTEM)@(plugin-ID:PLUGIN)@(Converter-ID:CONVERTER)@(AlarmType-ID:IASIO)\",\"valueType\":\"ALARM\",\"tStamp\":";
 		String kafkaTopic = "test";
 		String webserverUri = "ws://localhost:8080/";
-		this.runWebsocketServer();
+//		this.runWebsocketServer();
 		this.runWebServerSender(kafkaTopic, webserverUri);
-		this.runProducer();
+//		this.initCounter(1000);
+		//this.runProducer();
 		//assertEquals(1, 1);
 	}
-
+	
+	public static void main(String [] args) throws Exception {
+		System.out.println("\n*******************************************\n*****************************************");
+		WebServerSenderTest test = new WebServerSenderTest();
+		test.testWebServerSender();
+	}
 }
