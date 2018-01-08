b30d6919f793d6f4dd59e50acc2b856bf600211b
Jazmine <jamaldonadof@ug.uchile.cl>
2018-01-08 16:49:48 -0300

    Merge KafkaWebsocketConnector into WebServerSender

--- WebServerSender/src/java/org/eso/ias/webserversender/WebServerSender.java
+++ WebServerSender/src/java/org/eso/ias/webserversender/WebServerSender.java
@@ -2,19 +2,38 @@ package org.eso.ias.webserversender;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
+
 import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
+import org.eso.ias.kafkautils.KafkaHelper;
+import org.eso.ias.kafkautils.SimpleStringConsumer;
+import org.eso.ias.kafkautils.SimpleStringConsumer.KafkaConsumerListener;
+import org.eso.ias.kafkautils.SimpleStringConsumer.StartPosition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WebServerSender {
+@WebSocket(maxTextMessageSize = 64 * 1024)
+public class WebServerSender implements KafkaConsumerListener {
 
 	/**
 	 * Identifier
 	 */
 	String id;
-
+	
+	/**
+	 * WebSocket session required to send messages to the Web server
+	 */
+	public Session session;
+	
+	/**
+	 * IAS Core Kafka Consumer to get messages from the Core
+	 */
+	SimpleStringConsumer consumer;
+	
 	/**
 	 * Web socket client
 	 */
@@ -26,11 +45,6 @@ public class WebServerSender {
 	String kafkaTopic;
 
 	/**
-	 * Custom socket
-	 */
-	KafkaWebSocketConnector connector;
-
-	/**
 	 * Web Server URI
 	 */
 	String webserverUri;
@@ -40,28 +54,77 @@ public class WebServerSender {
 	ClientUpgradeRequest request = new ClientUpgradeRequest();
 
 	/**
-	 * WebSocket session required to send messages to the Web server
-	 */
-	Session session;
-
-	/**
 	 * The logger
 	 */
 	private static final Logger logger = LoggerFactory.getLogger(WebServerSender.class);
 
-  	/**
+	/**
 	 * Constructor
 	 *
-	 * @param id Identifier of the WebServerSender
+   	 * @param id Identifier of the KafkaWebSocketConnector
 	 * @param kafkaTopic Topic defined to send messages to the IAS Core to the IAS Web Server
+	 * TODO: Add servers to the arguments instead of use the default ones
+	 * @param webserverUri 
 	 */
 	public WebServerSender(String id, String kafkaTopic, String webserverUri) {
-		this.id = id;
-		this.kafkaTopic = kafkaTopic;
+    	this.id = id;
+    	this.kafkaTopic = kafkaTopic;
 		this.webserverUri = webserverUri;
-		this.connector = new KafkaWebSocketConnector(this.id, this.kafkaTopic);
+		this.consumer = new SimpleStringConsumer(KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS, kafkaTopic, this.id);
 	}
 
+	/**
+	 * Operations performed on connection close
+	 *
+	 * @param statusCode
+	 * @param reason
+	 */
+	@OnWebSocketClose
+	public void onClose(int statusCode, String reason) {
+	   logger.info("WebSocket connection closed:" + statusCode + ", " + reason);
+	   this.session = null;
+	   this.consumer.tearDown();
+	   //System.exit(0); // TODO: Add WebServer reconnection
+	}
+
+	/**
+	 * Operations performed on connection start
+	 *
+	 * @param session
+	 */
+	@OnWebSocketConnect
+	public void onConnect(Session session) {
+	   logger.info("WebSocket got connect: %s%n",session);
+	   this.session = session;
+	   try {
+	       this.consumer.setUp();
+	       this.consumer.startGettingEvents(StartPosition.END, this);
+	       logger.info("Starting to listen events\n");
+	   }
+	   catch (Throwable t) {
+	       logger.error("WebSocket couldn't send the message",t);
+	   }
+	}
+
+	/**
+	 * This method could get notifications for messages
+	 * published before depending on the log and offset
+	 * retention times. Therefore it discards the strings
+	 * not published by this test
+	 * @throws Exception
+	 *
+	 * @see org.eso.ias.kafkautils.SimpleStringConsumer.KafkaConsumerListener#stringEventReceived(java.lang.String)
+	 */
+	@Override
+	public synchronized void stringEventReceived(String event) {
+		try {
+			this.session.getRemote().sendStringByFuture( event );
+		}
+		catch (Exception e){
+			logger.error("Cannot send messages to the Web Server", e);
+		}
+	}
+	
 	/**
 	 * Initializes the WebSocket
 	 */
@@ -69,9 +132,8 @@ public class WebServerSender {
 		try {
 			this.uri = new URI(this.webserverUri);
 			this.client.start();
-			this.client.connect(this.connector, this.uri, this.request);
-			logger.info("Connecting to : " + uri.toString());
-			while(this.connector.session==null) {
+			this.client.connect(this, this.uri, this.request);
+			while(this.session==null) {
 				TimeUnit.MILLISECONDS.sleep(100);
 			}
 			logger.debug("Connection started!");
@@ -81,6 +143,9 @@ public class WebServerSender {
 		}
 	}
 
+	/**
+	 * Shutdown the WebSocket client
+	 */
 	public void stop() {
 		try {
 			this.client.stop();
