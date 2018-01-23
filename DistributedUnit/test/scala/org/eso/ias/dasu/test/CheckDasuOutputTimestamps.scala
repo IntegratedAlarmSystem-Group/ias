62ae5dff26bce883d415fc9741cc560807e466ff
Alessandro Caproni <acaproni@eso.org>
2018-01-23 14:08:53 -0300

    Tests if the same output is sent many times.

--- DistributedUnit/test/scala/org/eso/ias/dasu/test/CheckDasuOutputTimestamps.scala
+++ DistributedUnit/test/scala/org/eso/ias/dasu/test/CheckDasuOutputTimestamps.scala
@@ -4,6 +4,7 @@ import org.scalatest.FlatSpec
 import org.scalatest.BeforeAndAfter
 import org.eso.ias.dasu.DasuImpl
 import org.eso.ias.prototype.input.java.IASValue
+import org.ias.prototype.logging.IASLogger
 
 /**
  * Checks the timestamps of the output produced by a DASU
@@ -16,6 +17,9 @@ import org.eso.ias.prototype.input.java.IASValue
  * @see  [[https://github.com/IntegratedAlarmSystem-Group/ias/issues/52 Issue #52 on github]]
  */
 class CheckDasuOutputTimestamps extends FlatSpec with BeforeAndAfter {
+  
+  /** The logger */
+  val logger = IASLogger.getLogger(this.getClass);
   
   val autoRefreshTime = 1000L
   
@@ -90,5 +94,35 @@ class CheckDasuOutputTimestamps extends FlatSpec with BeforeAndAfter {
     Thread.sleep(3*autoRefreshTime)
     assert(f.outputStringsReceived.isEmpty)
     assert(f.outputValuesReceived.isEmpty)
+  }
+  
+  it must "re-send the same output" in {
+     f.dasu.get.enableAutoRefreshOfOutput(true)
+    val inputs: Set[IASValue[_]] = Set(f.buildValue(0))
+    f.inputsProvider.sendInputs(inputs)
+    
+    // Leave the DASU time to send the last computed output
+    Thread.sleep(5*autoRefreshTime)
+    
+    assert(f.outputStringsReceived.size>=5)
+    assert(f.outputValuesReceived.size>=5)
+    
+    f.dasu.get.enableAutoRefreshOfOutput(false)
+    
+    f.outputStringsReceived.foreach( s =>
+      logger.info("String received [{}]",s))
+    
+    val strOutput = f.outputStringsReceived.head
+    assert(f.outputStringsReceived.forall(s => s==strOutput))
+     
+    val firstValue =  f.outputValuesReceived.head
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.value==firstValue.value))
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.timestamp==firstValue.timestamp))
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.mode==firstValue.mode))
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.iasValidity==firstValue.iasValidity))
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.id==firstValue.id))
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.fullRunningId==firstValue.fullRunningId))
+    assert(f.outputValuesReceived.forall(iasVal => iasVal.valueType==firstValue.valueType))
+     
   }
 }
