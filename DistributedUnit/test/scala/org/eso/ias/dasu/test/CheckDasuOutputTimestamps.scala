d98dfc348d2e999fffd3a5693cb1936514332c6c
Alessandro Caproni <acaproni@eso.org>
2018-01-23 13:39:59 -0300

    Tests the basics of auto send of last calculated output

--- DistributedUnit/test/scala/org/eso/ias/dasu/test/CheckDasuOutputTimestamps.scala
+++ DistributedUnit/test/scala/org/eso/ias/dasu/test/CheckDasuOutputTimestamps.scala
@@ -3,6 +3,7 @@ package org.eso.ias.dasu.test
 import org.scalatest.FlatSpec
 import org.scalatest.BeforeAndAfter
 import org.eso.ias.dasu.DasuImpl
+import org.eso.ias.prototype.input.java.IASValue
 
 /**
  * Checks the timestamps of the output produced by a DASU
@@ -16,7 +17,9 @@ import org.eso.ias.dasu.DasuImpl
  */
 class CheckDasuOutputTimestamps extends FlatSpec with BeforeAndAfter {
   
-  val f = new DasuOneAsceCommon(1000)
+  val autoRefreshTime = 1000L
+  
+  val f = new DasuOneAsceCommon(autoRefreshTime)
   
   before {
     f.outputValuesReceived.clear()
@@ -30,5 +33,62 @@ class CheckDasuOutputTimestamps extends FlatSpec with BeforeAndAfter {
     f.dasu = None
     f.outputValuesReceived.clear()
     f.outputValuesReceived.clear()
+  }
+  
+  behavior of "The auto-resend of the last output of the DASU"
+  
+  it must "not refresh the output before getting the input" in {
+    f.dasu.get.enableAutoRefreshOfOutput(true)
+    // Leave the DASU time to send the last computed output
+    Thread.sleep(5*autoRefreshTime)
+    assert(f.outputStringsReceived.isEmpty)
+    assert(f.outputValuesReceived.isEmpty)
+  }
+  
+  it must "refresh the output once it has been generated" in {
+    f.dasu.get.enableAutoRefreshOfOutput(true)
+    val inputs: Set[IASValue[_]] = Set(f.buildValue(0))
+    f.inputsProvider.sendInputs(inputs)
+    
+    // Leave the DASU time to send the last computed output
+    Thread.sleep(5*autoRefreshTime)
+    
+    assert(f.outputStringsReceived.size>=5)
+    assert(f.outputValuesReceived.size>=5)
+  }
+  
+  it must "enable/disable the auto-refresh" in {
+    f.dasu.get.enableAutoRefreshOfOutput(true)
+    val inputs: Set[IASValue[_]] = Set(f.buildValue(0))
+    f.inputsProvider.sendInputs(inputs)
+    
+    // Leave the DASU time to send the last computed output
+    Thread.sleep(5*autoRefreshTime)
+    
+    assert(f.outputStringsReceived.size>=5)
+    assert(f.outputValuesReceived.size>=5)
+    
+    f.dasu.get.enableAutoRefreshOfOutput(false)
+    f.outputStringsReceived.clear()
+    f.outputValuesReceived.clear()
+    
+    // Leave the DASU time to send the last computed output
+    Thread.sleep(3*autoRefreshTime)
+    assert(f.outputStringsReceived.isEmpty)
+    assert(f.outputValuesReceived.isEmpty)
+    
+    f.dasu.get.enableAutoRefreshOfOutput(true)
+    Thread.sleep(3*autoRefreshTime)
+    assert(!f.outputStringsReceived.isEmpty)
+    assert(!f.outputValuesReceived.isEmpty)
+    
+    f.dasu.get.enableAutoRefreshOfOutput(false)
+    f.outputStringsReceived.clear()
+    f.outputValuesReceived.clear()
+    
+    // Leave the DASU time to send the last computed output
+    Thread.sleep(3*autoRefreshTime)
+    assert(f.outputStringsReceived.isEmpty)
+    assert(f.outputValuesReceived.isEmpty)
   }
 }
