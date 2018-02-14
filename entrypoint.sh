83e707e3fbade45bb94a84f4ef9340dfbb796c43
Jazmine <jamaldonadof@ug.uchile.cl>
2018-02-14 12:37:46 -0300

    Update entrypoint to refer to the cdb folder correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 source Tools/config/ias-bash-profile.sh
 iasRun -l s -Dorg.eso.ias.converter.kafka.servers=kafka:9092 org.eso.ias.converter.Converter ConverterID -jcdb / &
-iasRun -l s -Dorg.eso.ias.kafka.brokers=kafka:9092 org.eso.ias.supervisor.Supervisor SupervisorID -jcdb /cdb &
+iasRun -l s -Dorg.eso.ias.kafka.brokers=kafka:9092 org.eso.ias.supervisor.Supervisor SupervisorID -jcdb / &
 #iasRun -l j org.eso.ias.webserversender.WebServerSender &
 sleep infinity
