2c282ecd0d17ec16120fb1fe24a1e24f6a0934ca
acaproni <acaproni@osboxes>
2018-03-01 16:43:31 -0300

    Improves the tests of the sendingg without input changes

--- DistributedUnit/test/scala/org/eso/ias/dasu/test/CheckDasuOutputTimestamps.scala
+++ DistributedUnit/test/scala/org/eso/ias/dasu/test/CheckDasuOutputTimestamps.scala
@@ -174,20 +174,29 @@ class CheckDasuOutputTimestamps extends FlatSpec with BeforeAndAfter {
     val inputs2: Set[IASValue[_]] = Set(f.buildValue(20)) // Again cleared
     f.inputsProvider.sendInputs(inputs2)
     
+    // One for the immediate sending +2 for the aut-sending 
     Thread.sleep(2*f.dasu.get.throttling)
-    assert(f.outputValuesReceived.size==2)
-    assert(f.outputStringsReceived.size==2)
+    assert(f.outputValuesReceived.size==3)
+    assert(f.outputStringsReceived.size==3)
     
     val out1= f.outputValuesReceived(0)
     val out2= f.outputValuesReceived(1)
+    val out3= f.outputValuesReceived(2)
     
     assert(out1.valueType==out2.valueType)
+    assert(out1.valueType==out3.valueType)
     assert(out1.id==out2.id)
+    assert(out1.id==out3.id)
     assert(out1.mode==out2.mode)
+    assert(out1.mode==out3.mode)
     assert(out1.iasValidity==out2.iasValidity)
+    assert(out1.iasValidity==out3.iasValidity)
     assert(out1.fullRunningId==out2.fullRunningId)
+    assert(out1.fullRunningId==out3.fullRunningId)
     assert(out1.value==out2.value)
+    assert(out1.value==out3.value)
     assert(out1.sentToBsdbTStamp.get<out2.sentToBsdbTStamp.get)
+    assert(out2.sentToBsdbTStamp.get<out3.sentToBsdbTStamp.get)
   }
   
   
