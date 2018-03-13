476368a04bffc6992002c65053f52bba4d33d8a3
FaFilippo <33449219+FaFilippo@users.noreply.github.com>
2018-03-13 11:16:29 +0100

    Add Logging to IASrun
    Add Logging to IASrun

--- Tools/src/python/iasRun.py
+++ Tools/src/python/iasRun.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env python
+
 '''
 Created on Sep 26, 2016
-
 @author: acaproni
 '''
-
 import argparse
 import cmd
 import os
 import sys
 # Add import for default log with python
 import logging
+import sys
+import os, errno
 
 from IASTools.CommonDefs import CommonDefs
 from subprocess import call
@@ -21,19 +22,22 @@ def setProps(propsDict,className,logFileNameId):
     """
     Adds to the passed dictionary, the properties to be passed to all java/scala
     executables.
-    
-    These properties are common to all IAS executables and can for example 
+
+    These properties are common to all IAS executables and can for example
     be configuration properties.
-    
+
     @param propsDict: A dictionary of properties in the form name:value
-    @param className The name of the class to run 
+    @param className The name of the class to run
     @param logFileNameId A string identifier to append to the name of the log file
     """
+
+    #Log info for setProps
+    logger.info("Start set Properties")
     # Environment variables
     propsDict["ias.root.folder"]=os.environ["IAS_ROOT"]
     propsDict["ias.logs.folder"]=os.environ["IAS_LOGS_FOLDER"]
     propsDict["ias.tmp.folder"]=os.environ["IAS_TMP_FOLDER"]
-    
+
     # get the name of the class without dots
     # i.e. if className = "org.eso.ias.supervisor.Supervisor" we want
     # the file to be named "Supervisor"
@@ -44,35 +48,32 @@ def setProps(propsDict,className,logFileNameId):
     else:
         temp = className.rsplit(".",1)
         logFileName = temp[len(temp)-1]
-    
-    # Append the log identifier if it has been passed in the command line
-    if len(logFileNameId.strip())>0:
-        logFileName = logFileName+"-"+logFileNameId.strip()
-    propsDict["ias.logs.filename"]= logFileName
-    
-    propsDict["ias.config.folder"]=os.environ["IAS_CONFIG_FOLDER"]
-    
-    # Set the config file for sl4j (defined in Logging)
-    logbackConfigFileName="logback.xml"
-    fs = FileSupport(logbackConfigFileName,"config")
-    try:
-        path = fs.findFile()
-        propsDict["logback.configurationFile"]=path
-    except:
-        print("No log4j config file ("+logbackConfigFileName+") found: using defaults")
-        
-    # JVM always uses UTC
-    propsDict["-Duser.timezone"]="UTC"
-    
-    # Add environment variables
-    
+
+        # Append the log identifier if it has been passed in the command line
+        if len(logFileNameId.strip())>0:
+            logFileName = logFileName+"-"+logFileNameId.strip()
+            propsDict["ias.logs.filename"]= logFileName
+            propsDict["ias.config.folder"]=os.environ["IAS_CONFIG_FOLDER"]
+            # Set the config file for sl4j (defined in Logging)
+            logbackConfigFileName="logback.xml"
+            fs = FileSupport(logbackConfigFileName,"config")
+            try:
+                path = fs.findFile()
+                propsDict["logback.configurationFile"]=path
+            except:
+                print("No log4j config file ("+logbackConfigFileName+") found: using defaults")
+                # JVM always uses UTC
+                propsDict["-Duser.timezone"]="UTC"
+                logger.info("Properties Set")
+                # Add environment variables
+
 def addUserProps(propsDict,userProps):
     """
     Adds to the dictionary the user properties passed in the command line.
-    
     @param propsDict: A dictionary of properties in the form name:value
     @param userProps: a list of java properties in the form name=value
     """
+    logger.info("Start add user properties")
     if userProps==None or len(userProps)==0:
         return
     for prop in userProps:
@@ -85,31 +86,31 @@ def addUserProps(propsDict,userProps):
             print("Expected format is name=value")
             print()
             sys.exit(-1)
-        # Is th eprop. already defined?
+            # Is th eprop. already defined?
         if parts[0] in propsDict:
             print("\nWARNING: overriding ",parts[0],"java property")
             print("\told value",propsDict[parts[0]],"new value",parts[1])
             print()
         propsDict[parts[0]]=parts[1]
-    
+    logger.info("User properties set")
+
 def formatProps(propsDict):
     """
     Format the dictionary of properties in a list of strings
     list [ "-Dp1=v1", -Dp2=v2"]
-    
     @param propsDict: A dictionary of properties in the form name:value
-    @return A list of java/scala properties 
+    @return A list of java/scala properties
     """
     if len(propsDict)==0:
         return []
-    
     ret = []
-    
     keys = list(propsDict.keys())
     for key in keys:
         propStr = "-D"+key+"="+propsDict[key]
         ret.append(propStr)
     return ret
+
+
 
 def javaOpts():
     """
@@ -117,19 +118,19 @@ def javaOpts():
     from scala and java:
     * scala passes options to the java executable setting the JAVA_OPTS environment variable
     * java does not recognize JAVA_OPTS but accepts options as parameters in the command line
-           
     To unify the handling of java option for the 2 programming languages,
     this method returns a list of java options (string).
     Depending on the executable they will be passed differently.
-    
     @return: A list of java options like ['-Xmx512M', '-Xms16M']
     """
+    logger.info("Check if java variable are set")
     try:
         javaOpts=os.environ["JAVA_OPTS"]
         return javaOpts.split()
     except:
         # JAVA_OPTS environment variable not defined
-        return []
+        logger.warning("Java enviroment variable not set")
+    return []
 
 if __name__ == '__main__':
     """
@@ -142,99 +143,141 @@ if __name__ == '__main__':
                         help='The programming language: one between scala (or shortly s) and java (or j)',
                         action='store',
                         choices=['java', 'j', 'scala','s'],
-                        required=True)
+                        required=True
+                        )
     parser.add_argument(
                         '-i',
                         '--logfileId',
                         help='The identifier to be appended to the name of the log file',
                         action='store',
                         default="",
-                        required=False)
+                        required=False
+                        )
+
     parser.add_argument(
                         '-D',
                         '--jProp',
                         help='Set a java property: -Dname=value',
                         nargs="*",
                         action='append',
-                        required=False)
+                        required=False
+                        )
+
     parser.add_argument(
                         '-v',
                         '--verbose',
                         help='Increase the verbosity of the output',
                         action='store_true',
                         default=False,
-                        required=False)
+                        required=False
+                        )
+
     parser.add_argument(
                         '-a',
                         '--assertions',
                         help='Disable assertions',
                         action='store_false',
                         default=True,
-                        required=False)
-    
+                        required=False7
+                        )
+
     parser.add_argument('className', help='The name of the class to run the program')
     parser.add_argument('params', nargs=argparse.REMAINDER,
-                    help='Command line parameters')
+                        help='Command line parameters')
     args = parser.parse_args()
-    
     verbose = args.verbose
     if verbose:
         print("\nVerbose mode ON")
-    
-    # Get java options from JAVA_OPTS environment variable 
+        logger.info("\nVerbose mode ON")
+    Insert the configuration for the logger
+    """
+    LEVELS = {'debug': logging.DEBUG,'info': logging.INFO,'warning': logging.WARNING,'error': logging.ERROR,'critical': logging.CRITICAL}
+
+    if len(sys.argv) > 1:
+        level_name = sys.argv[1]
+        level = LEVELS.get(level_name, logging.NOTSET)
+        logging.basicConfig(level=level)
+
+    #Define logger with logging import
+    logger = logging.getLogger()
+    #Set the level of the message visualize
+    logger.setLevel(logging.DEBUG)
+
+    #Set the format of the log
+    logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    #Set path where save the file and the name of the file.
+    logPath="../IAS_LOGS_FOLDER"
+    fileName="iasRun"
+    try:
+        os.makedirs(logPath)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
+    #Start stream for write into file, from here when it's insert the logger. write all into file.
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+    # Get java options from JAVA_OPTS environment variable
     javaOptions=javaOpts()
-    
     # Build the command line
     if args.language=='s' or args.language=='scala':
         cmd=['scala']
         if verbose:
-            print("Running a SCALA program.") 
-    else:
-        cmd=['java']
-        if verbose:
-            print("Running a JAVA program.")
-    
+            print("Running a SCALA program.")
+            logger.info("Running a SCALA program")
+        else:
+            cmd=['java']
+            if verbose:
+                print("Running a JAVA program.")
+                logger.info("Running a JAVA program")
     # Assertions are enabled differently in java (command line) and scala ($JAVA_OPTS)
     enableAssertions = args.assertions
     if enableAssertions:
         javaOptions.append("-ea")
         if verbose:
             print("Assertions are enabled.")
-    else:
-        if verbose:
-            print("Assertions disabled.")
-            
-    
+            logger.info("Assertions are enabled")
+        else:
+            if verbose:
+                print("Assertions disabled.")
+                logger.info("Assertions disabled")
+
     # Actual environment to pass to the executable
     #
     # It is enriched by setting JAVA_OPTS for scala
     d = dict(os.environ)
-    
-    # Add java options (in the command line for java executables and 
+
+    # Add java options (in the command line for java executables and
     # in JAVA_OPTS env. variable for scala)
     if args.language=='s' or args.language=='scala':
         s=" ".join(map(str, javaOptions))
         if verbose:
             print("Options to pass to the java executable",s)
-            d['JAVA_OPTS']=s 
+            logger.info("Options to pass to the java executable %s",s)
+            d['JAVA_OPTS']=s
     else:
         for opt in javaOptions:
             if verbose:
                 print("Adding",opt,"java option")
-            cmd.append(opt) 
-        
+                logger.info("Adding %s java option",opt)
+            cmd.append(opt)
+
     # Is the environment ok?
     # Fail fast!
     if not CommonDefs.checkEnvironment():
         print("Some setting missing in IAS environment.")
         print("Set the environment with ias-bash_profile before running IAS applications")
+        logger.warning("Some setting missing in IAS environment\nSet the environment with ias-bash_profile before running IAS applications")
         print()
         sys.exit(-1)
-    
+
     # Create tmp and logs folders if not exists already
     FileSupport.createLogsFolder()
     FileSupport.createTmpFolder()
-    
+
     # Add the properties
     #
     # Default and user defined properties are in a dictionary:
@@ -243,6 +286,8 @@ if __name__ == '__main__':
     setProps(props, args.className,args.logfileId)
     if args.jProp is not None:
         addUserProps(props,args.jProp)
+        logger.info("Set the user prop")
+
     if len(props)>0:
         stingOfPros = formatProps(props)
         # Sort to enhance readability
@@ -250,17 +295,19 @@ if __name__ == '__main__':
         cmd.extend(formatProps(props))
         if verbose:
             print("java properties:")
+            logger.info("java properties:")
             for p in stingOfPros:
                 print("\t",p[2:])
+                logger.info("\t%s",p[2:])
     else:
         if (verbose):
             print("No java properties defined")
-    
-    
+            logger.info("No java properties defined")
+
     #add the classpath
     theClasspath=CommonDefs.buildClasspath()
     if (args.language=='j' or args.language=='java'):
-        theClasspath=CommonDefs.addScalaJarsToClassPath(theClasspath) 
+        theClasspath=CommonDefs.addScalaJarsToClassPath(theClasspath)
     cmd.append("-cp")
     cmd.append(theClasspath)
     if verbose:
@@ -269,39 +316,38 @@ if __name__ == '__main__':
         print("Classpath:")
         for jar in jars:
             print("\t",jar)
-    
+            logger.info("Classpath set")
+
     # Add the class
     cmd.append(args.className)
-    
+
     # Finally the command line parameters
     if len(args.params)>0:
         cmd.extend(args.params)
-    
+
     if verbose:
-        print("Launching",args.className, end=' ')
+        print("Launching",args.className, end='')
         if len(args.params)>0:
             print("with params:")
             for arg in args.params:
                 print("\t",arg)
         else:
             print()
-    
+    logger.info("Launch element")
+
     if verbose:
         arrowDown =  chr(8595)
         delimiter = ""
         for t in range(16):
             delimiter = delimiter + arrowDown
-            
+
         print("\n",delimiter,args.className,"output",delimiter)
-        
     call(cmd)
-    
+
     if verbose:
         arrowUp =  chr(8593)
         delimiter = ""
         for t in range(17):
             delimiter = delimiter + arrowUp
-        print(delimiter,args.className,"done",delimiter)
-        print()
-    
-    
+    print(delimiter,args.className,"done",delimiter)
+    print()
