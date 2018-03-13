5bd2019c304b6da84a9f5da71e9c43db9a78733a
FaFilippo <33449219+FaFilippo@users.noreply.github.com>
2018-03-13 15:14:10 +0100

    Add logConf to test.

--- Tools/test/python/testCreateModule.py
+++ Tools/test/python/testCreateModule.py
@@ -12,10 +12,7 @@ from IASTools.FileSupport import FileSupport
 from IASTools.ModuleSupport import ModuleSupport
 from shutil import rmtree
 
-#Import logging
-import logging
-import sys
-import os, errno
+from logConf import Log
 
 class TestCreateModule(unittest.TestCase):
 
@@ -66,39 +63,6 @@ class TestCreateModule(unittest.TestCase):
 
 
     if __name__ == '__main__':
-        """
-        Insert the configuration for the logger
-        """
-        LEVELS = {'debug': logging.DEBUG,'info': logging.INFO,'warning': logging.WARNING,'error': logging.ERROR,'critical': logging.CRITICAL}
-
-        if len(sys.argv) > 1:
-            level_name = sys.argv[1]
-            level = LEVELS.get(level_name, logging.NOTSET)
-            logging.basicConfig(level=level)
-
-        #Define logger with logging import
-        logger = logging.getLogger()
-
-        #Set the level of the message visualize
-        logger.setLevel(logging.DEBUG)
-
-        #Set the format of the log
-        logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-        #Set path where save the file and the name of the file.
-        logPath="../IAS_LOGS_FOLDER"
-        fileName="TestCreateModule"
-        try:
-            os.makedirs(logPath)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
-        fileHandler.setFormatter(logFormatter)
-        logger.addHandler(fileHandler)
-
-        #Start stream for write into file, from here when it's insert the logger. write all into file.
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setFormatter(logFormatter)
-        logger.addHandler(consoleHandler)
+        logger = Log.GetLogger(os.path.basename(__file__).split(".")[0])
         logger.info("start main")
         unittest.main()
