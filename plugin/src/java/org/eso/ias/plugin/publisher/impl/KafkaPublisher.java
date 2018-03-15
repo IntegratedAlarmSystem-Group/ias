dffdf1d9cfff3a5602230953fba5a3f59be16b75
Alessandro Caproni <acaproni@eso.org>
2018-03-15 14:21:04 -0300

    Removes a noisy println #76

--- plugin/src/java/org/eso/ias/plugin/publisher/impl/KafkaPublisher.java
+++ plugin/src/java/org/eso/ias/plugin/publisher/impl/KafkaPublisher.java
@@ -106,7 +106,6 @@ public class KafkaPublisher extends PublisherBase {
 	@Override
 	protected long publish(MonitorPointData mpData) throws PublisherException {
 		String jsonStrToSend = mpData.toJsonString();
-		System.out.println("jsonStrToSend "+jsonStrToSend);
 		// The partition is explicitly set: the passed key will not used
 		// for partitioning in the topic
 		ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName, partition,pluginId,jsonStrToSend);
