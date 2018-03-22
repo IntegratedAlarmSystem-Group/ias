6149bfca8a4c813e137e4a29072e81746de39f21
FaFilippo <33449219+FaFilippo@users.noreply.github.com>
2018-03-22 14:32:36 +0100

    improve LogConf

--- Tools/test/python/logConf.py
+++ Tools/test/python/logConf.py
@@ -1,39 +1,60 @@
 import logging
+
 import sys
+
 import os, errno
+
 import datetime
+
+
+
+
 
 class Log():
 
-    def GetLogger(fileName):
-        LEVELS = {'debug': logging.DEBUG,'info': logging.INFO,'warning': logging.WARNING,'error': logging.ERROR,'critical': logging.CRITICAL}
 
-        if len(sys.argv) > 1:
-            level_name = sys.argv[1]
-            level = LEVELS.get(level_name, logging.NOTSET)
-            logging.basicConfig(level=level)
 
-        #Define logger with logging import
-        logger = logging.getLogger()
-        #Set the level of the message visualize
-        logger.setLevel(logging.DEBUG)
+  def GetLoggerFile(fileName):
+    #take the path for logs folder inside $IAS_ROOT
 
-        #Set the format of the log
-        logFormatter = logging.Formatter("%(asctime)s | %(pathname)s | %(name)s | [%(threadName)-12.12s] | %(module)s.%(lineno)d | [%(levelname)-5.5s] | %(message)s", "%Y-%m-%dT%H:%M:%S")
-        #Set path where save the file and the name of the file.
-        logPath=os.environ["IAS_ROOT"]
-        try:
-            os.makedirs(logPath)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        now = datetime.datetime.now()
-        fileName=fileName+now.isoformat()
-        fileHandler = logging.FileHandler("{0}/logs/{1}.log".format(logPath, fileName))
-        fileHandler.setFormatter(logFormatter)
-        logger.addHandler(fileHandler)
-        #Start stream for write into file, from here when it's insert the logger. write all into file.
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setFormatter(logFormatter)
-        logger.addHandler(consoleHandler)
-        return logger
+    logPath=os.environ["IAS_ROOT"]
+    #If the file doesn't exist it's created
+
+    try:
+
+        os.makedirs(logPath)
+
+    except OSError as e:
+
+        if e.errno != errno.EEXIST:
+
+         raise
+
+    #Format of the data for filename
+    now = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S.%f')[:-3]
+    fileName=fileName+now
+    #path of the file
+    file=("{0}/logs/{1}.log".format(logPath, fileName))
+
+    # set up logging to file - see previous section for more details
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s%(msecs)d  | %(levelname)s | [%(filename)s %(lineno)d] [%(threadName)s] | %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S.',
+                        filename=file)
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(asctime)s%(msecs)d %(levelname)-8s [%(filename)s %(lineno)d] %(message)s' , '%H:%M:%S.')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+
+    # Now, define a couple of other loggers which might represent areas in your
+    # application:
+
+    logger1 = logging.getLogger()
+
+
+    return logger1
