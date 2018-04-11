4d3fdc2d8a8e9daeb69d25bf8da09b0ad4827703
Alessandro Caproni <acaproni@eso.org>
2018-04-11 12:52:12 -0300

    Adds support for templates to Identifier - #80

--- BasicTypes/test/runTests.sh
+++ BasicTypes/test/runTests.sh
@@ -2,6 +2,7 @@
 iasRun -l s org.scalatest.run org.eso.ias.basictypes.test.TestInOut
 iasRun -l s org.scalatest.run org.eso.ias.basictypes.test.TestValidity
 iasRun -l s org.scalatest.run org.eso.ias.basictypes.test.TestIdentifier
+iasRun -l s org.scalatest.run org.eso.ias.basictypes.test.TestTemplatedIdentifier
 iasRun -l s org.scalatest.run org.eso.ias.basictypes.test.TestJavaConversion
 iasRun -l j org.junit.runner.JUnitCore org.eso.ias.basictypes.test.IasValueJsonSerializerTest
 iasRun -l j org.junit.runner.JUnitCore org.eso.ias.basictypes.test.IASValueTest
