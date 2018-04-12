fcdb6b7eac21c0fa74b2cb0e6a7f8d0c4629861b
Alessandro Caproni <acaproni@eso.org>
2018-04-12 16:52:47 -0300

    Adds a test for the TemplateHelper

--- Supervisor/test/runTests.sh
+++ Supervisor/test/runTests.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorTest
+iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.TemplateHelperTest
+iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorWithTemplatesTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorWithKafkaTest
