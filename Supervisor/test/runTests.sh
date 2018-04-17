7cd19d34e958a29716bbeba7dc64bf79835cbdab
Alessandro Caproni <acaproni@eso.org>
2018-04-17 13:57:35 -0300

    Adds a test of functioning of the TF with templated parameters

--- Supervisor/test/runTests.sh
+++ Supervisor/test/runTests.sh
@@ -2,4 +2,5 @@
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.TemplateHelperTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorWithTemplatesTest
+iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.TemplatedInputTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorWithKafkaTest
