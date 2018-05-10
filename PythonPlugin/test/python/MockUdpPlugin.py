495857de46c6ccbe454c29b7df62796d53860f9b
Alessandro Caproni <acaproni@eso.org>
2018-05-10 16:36:15 -0300

    A mock UDP python plugin that sends some monitor points

---
+++ PythonPlugin/test/python/MockUdpPlugin.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+'''
+A plugin that sends some monitor points through the UDP
+socket to the java receiver.
+
+The scope of this plugin is to close the loop from python
+to the java plugin that pushes values in the BSDB.
+
+Created on May 10, 2018
+
+@author: acaproni
+'''
+import time
+from IasPlugin.UdpPlugin import UdpPlugin
+from IasPlugin.IasType import IASType
+from IasPlugin.OperationalMode import OperationalMode
+from IasPlugin.Alarm import Alarm
+
+if __name__ == '__main__':
+    udpPlugin = UdpPlugin("localhost",10101)
+    udpPlugin.start()
+    time.sleep(1)
+    udpPlugin.submit("ID-Double", 122.54, IASType.DOUBLE, operationalMode=OperationalMode.INITIALIZATION)
+    udpPlugin.submit("ID-Long", 1234567, IASType.DOUBLE, operationalMode=OperationalMode.STARTUP)
+    udpPlugin.submit("ID-Bool", False, IASType.BOOLEAN, operationalMode=OperationalMode.OPERATIONAL)
+    udpPlugin.submit("ID-Char", 'X', IASType.CHAR, operationalMode=OperationalMode.DEGRADED)
+    udpPlugin.submit("ID-String", 'Testing for test', IASType.STRING, operationalMode=OperationalMode.CLOSING)
+    udpPlugin.submit("ID-Alarm", Alarm.SET, IASType.ALARM, operationalMode=OperationalMode.SHUTTEDDOWN)
+    
+    time.sleep(.5)
+    udpPlugin.shutdown()
+    
+    
