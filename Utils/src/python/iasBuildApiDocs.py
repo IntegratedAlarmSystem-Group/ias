65b5d77c6265462f1f76fe7209fb101623979c93
Alessandro Caproni <acaproni@eso.org>
2018-06-06 09:46:01 -0400

    Parses arguments with ArgumentParser instead of OptionParser

--- Utils/src/python/iasBuildApiDocs.py
+++ Utils/src/python/iasBuildApiDocs.py
@@ -7,7 +7,7 @@ Created on Jul 7, 2017
 '''
 
 import sys
-from optparse import OptionParser
+from argparse import ArgumentParser
 from IASApiDocs.ScaladocBuilder import ScaladocBuilder
 from IASApiDocs.JavadocBuilder import JavadocBuilder
 from IASApiDocs.PydocBuilder import PydocBuilder
@@ -17,50 +17,54 @@ import os
 if __name__ == '__main__':
 
     # Parse the command line
-    parser = OptionParser()
-    parser.add_option("-d", "--destFolder", help="HTML destination folder", action="store", type="string", dest="destFolder")
-    parser.add_option("-s", "--sourceFolder", help="IAS source folder", action="store", type="string", dest="srcFolder")
-    parser.add_option(
-                        '-lso',
-                        '--levelStdOut',
-                        help='Logging level: Set the level of the message for the file logger, default: Debug level',
-                        action='store',
-                        choices=['info', 'debug', 'warning', 'error', 'critical'],
-                        default='info',
-                        required=False)
-    parser.add_option(
-                        '-lcon',
-                        '--levelConsole',
-                        help='Logging level: Set the level of the message for the console logger, default: Debug level',
-                        action='store',
-                        choices=['info', 'debug', 'warning', 'error', 'critical'],
-                        default='info',
-                        required=False)
-    (options, args) = parser.parse_args()
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-d", 
+        "--destFolder", 
+        help="HTML destination folder", 
+        action="store",
+        dest="destFolder",
+        required=True)
+    parser.add_argument(
+        "-s", 
+        "--sourceFolder", 
+        help="IAS source folder", 
+        action="store", 
+        dest="srcFolder",
+        required=True)
+    parser.add_argument(
+        '-lso',
+        '--levelStdOut',
+        help='Logging level: Set the level of the message for the file logger, default: Debug level',
+        action='store',
+        choices=['info', 'debug', 'warning', 'error', 'critical'],
+        default='info',
+        required=False)
+    parser.add_argument(
+        '-lcon',
+        '--levelConsole',
+        help='Logging level: Set the level of the message for the console logger, default: Debug level',
+        action='store',
+        choices=['info', 'debug', 'warning', 'error', 'critical'],
+        default='info',
+        required=False)
+    args = parser.parse_args()
+    print(args)
+    
     stdoutLevel=args.levelStdOut
     consoleLevel=args.levelConsole
     logger=Log.initLogging(__file__,stdoutLevel,consoleLevel)
-    if not options.destFolder:
-        logger.info("No destination folder given")
-        sys.exit(-1)
-    else:
-        logger.info("API documentation will be generated in %s",options.destFolder)
-
-    if not options.srcFolder:
-        logger.info("No source folder given")
-        sys.exit(-1)
-    else:
-        logger.info("Reading sources from %s",options.srcFolder)
+    logger.info("Reading sources from %s",args.srcFolder)
 
     # Build scala documentation
     logger.info("Building scaladoc")
-    scalaBuilder = ScaladocBuilder(options.srcFolder,join(options.destFolder,"scala"))
+    scalaBuilder = ScaladocBuilder(args.srcFolder,join(args.destFolder,"scala"))
     scalaBuilder.buildScaladocs()
 
     logger.info("Building javadoc")
-    javaBuilder = JavadocBuilder(options.srcFolder,join(options.destFolder,"java"))
+    javaBuilder = JavadocBuilder(args.srcFolder,join(args.destFolder,"java"))
     javaBuilder.buildJavadocs()
 
     logger.info("Building pydoc")
-    pythonBuilder = PydocBuilder(options.srcFolder,join(options.destFolder,"python"))
+    pythonBuilder = PydocBuilder(args.srcFolder,join(args.destFolder,"python"))
     pythonBuilder.buildPydocs()
