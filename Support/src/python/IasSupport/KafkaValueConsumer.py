eaad17629430c83647c24db0019fe3ba919569d9
Alessandro Caproni <acaproni@eso.org>
2018-06-14 18:01:19 -0400

    Fixes the start of reading from the latest IasValue in the topic

--- Support/src/python/IasSupport/KafkaValueConsumer.py
+++ Support/src/python/IasSupport/KafkaValueConsumer.py
@@ -3,9 +3,13 @@ Created on Jun 12, 2018
 
 @author: acaproni
 '''
+import logging
 from kafka import KafkaConsumer
 from threading import Thread
 from IasSupport.IasValue import IasValue
+from kafka.structs import TopicPartition
+
+logger = logging.getLogger(__file__)
 
 class IasValueListener(object):
     """
@@ -40,6 +44,9 @@ class KafkaValueConsumer(Thread):
     # The kafka consumer
     consumer = None
     
+    # The topic
+    topic = None
+    
     def __init__(self, 
                  listener,
                  kafkabrokers,
@@ -59,20 +66,23 @@ class KafkaValueConsumer(Thread):
             raise ValueError("The listener msut be a subclass of IasValueListener")
         self.listener=listener
         
+        if topic is None:
+            raise ValueError("The topic can't be None")
+        self.topic=topic
+        
         self.consumer = KafkaConsumer(
-            topic,
             bootstrap_servers=kafkabrokers,
             client_id=clientid,
-            group_id=groupid,
-            auto_offset_reset="latest",
-            consumer_timeout_ms=1000,
             enable_auto_commit=True)
         Thread.setDaemon(self, True)
+        logger.info('Kafka consumer %s connected to %s and topic %s',clientid,kafkabrokers,topic)
         
     def run(self):
         """
         The thread to get IasValues from the BSDB
         """
+        logger.info('Thread to poll for IasValues started')
+        self.consumer.seek_to_end()
         while True:
             try:
                 messages = self.consumer.poll(500)
@@ -84,4 +94,15 @@ class KafkaValueConsumer(Thread):
                     json = cr.value.decode("utf-8")
                     iasValue = IasValue.fromJSon(json)
                     self.listener.iasValueReceived(iasValue)
+        logger.info('Thread terminated')
+        
+    def start(self):
+        partitionsIds=self.consumer.partitions_for_topic(self.topic)
+        logger.info('%d partitions found on topic %s: %s',len(partitionsIds),self.topic,partitionsIds)
+        partitions=[]
+        for pId in partitionsIds:
+            partitions.append(TopicPartition(self.topic,pId))
+        self.consumer.assign(partitions)
+        logger.info('Starting thread to poll for IasValues')
+        Thread.start(self)
         
