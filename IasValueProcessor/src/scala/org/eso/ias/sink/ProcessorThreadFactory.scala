a56799caddb0ae461e3873dee8385fd3401933b7
Alessandro Caproni <acaproni@eso.org>
2018-06-20 16:46:07 -0400

    Adds a log message for each created thread

--- IasValueProcessor/src/scala/org/eso/ias/sink/ProcessorThreadFactory.scala
+++ IasValueProcessor/src/scala/org/eso/ias/sink/ProcessorThreadFactory.scala
@@ -3,8 +3,13 @@ package org.eso.ias.sink
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.atomic.AtomicLong
 
+import org.eso.ias.logging.IASLogger
+
 
 class ProcessorThreadFactory(val processorId: String) extends ThreadFactory {
+
+  /** The logger */
+  private val logger = IASLogger.getLogger(classOf[ProcessorThreadFactory])
 
   /**
     * Count the number of created threads
@@ -20,8 +25,10 @@ class ProcessorThreadFactory(val processorId: String) extends ThreadFactory {
   override def newThread(runnable: Runnable): Thread = {
     require(Option(runnable).isDefined)
     val actual = count.incrementAndGet()
-    val ret = new Thread(runnable,s"$processorId-$actual")
+    val threadId = s"$processorId-$actual"
+    val ret = new Thread(runnable,threadId)
     ret.setDaemon(true)
+    logger.debug("New thread {} created",threadId)
     ret
   }
 }
