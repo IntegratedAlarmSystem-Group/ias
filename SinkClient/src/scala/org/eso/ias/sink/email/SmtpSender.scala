e860428a595d3d1b2c2be51f1d9606f25dd1645a
Alessandro Caproni <acaproni@eso.org>
2018-06-28 07:57:37 -0400

    First untested version of the connection to the SMTP server

--- SinkClient/src/scala/org/eso/ias/sink/email/SmtpSender.scala
+++ SinkClient/src/scala/org/eso/ias/sink/email/SmtpSender.scala
@@ -1,5 +1,12 @@
 package org.eso.ias.sink.email
 
+import java.util.{Date, Properties}
+
+import org.eso.ias.logging.IASLogger
+import com.typesafe.scalalogging.Logger
+import javax.mail.internet.MimeMessage
+import javax.mail.{Address, Message, Session, Transport}
+
 /**
   * The sender of notifications by emails.
   *
@@ -8,6 +15,43 @@ package org.eso.ias.sink.email
   * @param pswd the optional passowrd to pass to the server
   */
 class SmtpSender(val server: String, val loginName: Option[String], val pswd: Option[String]) extends Sender {
+
+   /** The logger */
+  private val logger: Logger = IASLogger.getLogger(classOf[SmtpSender])
+
+  logger.info("SMTP server {}, user name {}, password =*****",server,loginName)
+
+
+  private def sendMessage(recipients: String, subject: String, body: String): Unit = {
+    val smtpPropName="mail.smtp.host"
+    val props: Properties = System.getProperties()
+    if (!props.contains(smtpPropName)) {
+      props.put(smtpPropName,server)
+    }
+    val session: Session = Session.getInstance(props, null)
+
+    val mimeMsg: MimeMessage = new MimeMessage(session)
+    mimeMsg.addHeader("Content-type", "text/plain; charset=UTF-8")
+    mimeMsg.addHeader("format", "flowed")
+    mimeMsg.addHeader("Content-Transfer-Encoding", "8bit")
+    import javax.mail.internet.InternetAddress
+    mimeMsg.setFrom(new InternetAddress("no_reply@ias.org", "NoReply-IAS"))
+
+    val replyToAddress: Array[Address] = InternetAddress.parse("no_reply@ias.org", false).asInstanceOf[Array[Address]]
+    mimeMsg.setReplyTo(replyToAddress)
+
+    mimeMsg.setSubject(subject, "UTF-8")
+    mimeMsg.setText(body, "UTF-8")
+    mimeMsg.setSentDate(new Date())
+    mimeMsg.setRecipients(Message.RecipientType.TO, recipients)
+
+    val tr: Transport = session.getTransport("smtp");
+    if (loginName.isDefined) tr.connect(server, loginName.get , pswd.get)
+    else tr.connect(server,null,null)
+    mimeMsg.saveChanges();      // don't forget this
+    tr.sendMessage(mimeMsg, mimeMsg.getAllRecipients());
+    tr.close();
+  }
 
 
   /**
@@ -32,5 +76,17 @@ class SmtpSender(val server: String, val loginName: Option[String], val pswd: Op
     require(Option(recipients).isDefined && recipients.nonEmpty,"No recipients given")
     require(Option(alarmId).isDefined && !alarmId.trim.isEmpty,"Invalid alarm ID")
     require(Option(alarmState).isDefined,"No alarm state to notify")
+  }
+}
+
+object SmtpSender {
+  def main(args: Array[String]): Unit = {
+    val server="smtp-internal.osf.alma.cl"
+    val user =""
+    val paswd=""
+
+    val smtpSender = new SmtpSender(server,None,None)
+
+
   }
 }
