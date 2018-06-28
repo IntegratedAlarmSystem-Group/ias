ad7fe082ead732ab7c6e77f2c21e58761cab18fe
Alessandro Caproni <acaproni@eso.org>
2018-06-28 17:00:23 -0400

    Adds the main to start the mail notification sender

--- SinkClient/src/scala/org/eso/ias/sink/email/NotificationsSender.scala
+++ SinkClient/src/scala/org/eso/ias/sink/email/NotificationsSender.scala
@@ -1,15 +1,25 @@
 package org.eso.ias.sink.email
 
 import java.time._
+import java.util
 import java.util.concurrent.{Executors, ThreadFactory, TimeUnit}
 
 import com.typesafe.scalalogging.Logger
-import org.eso.ias.cdb.pojos.IasTypeDao
+import org.eso.ias.cdb.CdbReader
+import org.eso.ias.cdb.json.{CdbFiles, CdbJsonFiles, JsonReader}
+import org.eso.ias.cdb.pojos.{IasDao, IasTypeDao, IasioDao}
+import org.eso.ias.cdb.rdb.RdbReader
+import org.eso.ias.dasu.subscriber.{InputSubscriber, KafkaSubscriber}
+import org.eso.ias.heartbeat.HbProducer
+import org.eso.ias.heartbeat.publisher.HbKafkaProducer
+import org.eso.ias.heartbeat.serializer.HbJsonSerializer
+import org.eso.ias.kafkautils.KafkaHelper
 import org.eso.ias.logging.IASLogger
-import org.eso.ias.sink.ValueListener
-import org.eso.ias.types.{Alarm, IASTypes, IASValue, Validity}
+import org.eso.ias.sink.{IasValueProcessor, ValueListener}
+import org.eso.ias.supervisor.Supervisor._
+import org.eso.ias.types._
 
-import scala.collection.mutable
+import scala.collection.{JavaConverters, mutable}
 import scala.collection.mutable.ListBuffer
 import scala.util.{Failure, Try}
 
@@ -28,9 +38,6 @@ import scala.util.{Failure, Try}
   */
 class NotificationsSender(id: String, val sender: Sender) extends ValueListener(id) {
   require(Option(sender).isDefined)
-
-  /** The logger */
-  val msLogger: Logger = IASLogger.getLogger(classOf[NotificationsSender])
 
   /** The alarms that have a registered recipients to send notification */
   val alarmsToTrack:  mutable.Map[String, AlarmStateTracker] = mutable.Map.empty
@@ -56,7 +63,7 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
   val timeIntervalToSendEmails: Int = Integer.getInteger(
     NotificationsSender.SendEmailsTimeIntervalPropName,
     NotificationsSender.SendEmailsTimeIntervalDefault)
-  msLogger.info("Will send digest emails every {} minutes",timeIntervalToSendEmails)
+  NotificationsSender.msLogger.info("Will send digest emails every {} minutes",timeIntervalToSendEmails)
 
   val timeTostart: (Int, Int) = {
     val prop = System.getProperty(
@@ -64,7 +71,7 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
       NotificationsSender.StartTimeOfPeriodicNotificationsDefault).split(":")
     (Integer.parseInt(prop(0)),Integer.parseInt(prop(1)))
   }
-  msLogger.info("First digest at {}:{} UTC",timeTostart._1.toString,timeTostart._2.toString)
+  NotificationsSender.msLogger.info("First digest at {}:{} UTC",timeTostart._1.toString,timeTostart._2.toString)
 
   /** Start the periodic tasl to send the digests */
   private def startTimer() = {
@@ -82,12 +89,12 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
 
     executor.scheduleAtFixedRate(new Runnable() {
       override def run(): Unit = {
-        msLogger.info("TASK")
+        NotificationsSender.msLogger.info("TASK")
         sendDigests()
       }
           },initalDelay,timeIntervalSecs, TimeUnit.SECONDS);
-    msLogger.debug("Periodic thread scheduled to run in {} secs every {} secs",initalDelay.toString,timeIntervalSecs)
-    msLogger.info("Periodic send of digest scheduled every {} minutes",timeIntervalToSendEmails.toString)
+    NotificationsSender.msLogger.debug("Periodic thread scheduled to run in {} secs every {} secs",initalDelay.toString,timeIntervalSecs)
+    NotificationsSender.msLogger.info("Periodic send of digest scheduled every {} minutes",timeIntervalToSendEmails.toString)
   }
 
   /**
@@ -100,7 +107,7 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
       iasioDao.getIasType==IasTypeDao.ALARM &&
       Option(iasioDao.getEmails).isDefined &&
       !iasioDao.getEmails.trim.isEmpty)
-    msLogger.info("Tracks and send email notifications for {} alarms",alarmsWithRecipients.size)
+    NotificationsSender.msLogger.info("Tracks and send email notifications for {} alarms",alarmsWithRecipients.size)
 
     // Saves the ID of the alarms to send notifications in the map to track the changes of states
     alarmsWithRecipients.foreach({
@@ -117,7 +124,6 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
     tempMapOfAlarmsForUsers.keys.foreach(user => alarmsForUser(user)=tempMapOfAlarmsForUsers(user).toList)
 
     startTimer()
-
   }
 
   /**
@@ -137,21 +143,21 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
     // Send one email to each user
     //
     // The email contains the summary of all the alarsm about which the user wants to get notifications
-    msLogger.debug("Sending digests")
+    NotificationsSender.msLogger.debug("Sending digests")
     alarmsForUser.keys.foreach(user => {
-      msLogger.debug("Sending digest of {} alarms to {}",alarmsForUser(user).mkString(","),user)
+      NotificationsSender.msLogger.debug("Sending digest of {} alarms to {}",alarmsForUser(user).mkString(","),user)
 
       val alarmStates: List[AlarmStateTracker] = alarmsForUser(user).map(alarmId => alarmsToTrack(alarmId))
-      msLogger.debug("Digests will report the state of {} alarm states for user {}",alarmStates.length,user)
+      NotificationsSender.msLogger.debug("Digests will report the state of {} alarm states for user {}",alarmStates.length,user)
       val sendOp = Try(sender.digestNotify(user,alarmStates))
       if (sendOp.isFailure) {
-        msLogger.error("Error sending periodic notification to {}",user, sendOp.asInstanceOf[Failure[_]].exception)
+        NotificationsSender.msLogger.error("Error sending periodic notification to {}",user, sendOp.asInstanceOf[Failure[_]].exception)
       } else {
-        msLogger.debug("Digest sent to {}",user)
+        NotificationsSender.msLogger.debug("Digest sent to {}",user)
       }
     })
     alarmsToTrack.keys.foreach(id => alarmsToTrack(id)=alarmsToTrack(id).reset())
-    msLogger.debug("Digests sent to the sender")
+    NotificationsSender.msLogger.debug("Digests sent to the sender")
   }
 
   /**
@@ -164,9 +170,9 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
     require(Option(alarmId).isDefined && !alarmId.isEmpty)
     require(Option(state).isDefined)
     val recipients = iasValuesDaos(alarmId).getEmails.split(",")
-    msLogger.debug("Sending tonitifcation of alarm {} status change to {}", alarmId, recipients.mkString(","))
+    NotificationsSender.msLogger.debug("Sending tonitifcation of alarm {} status change to {}", alarmId, recipients.mkString(","))
     val sendOp = Try(sender.notify(recipients.map(_.trim).toList, alarmId, state))
-    if (sendOp.isFailure) msLogger.error("Error sending alarm state notification notification to {}", recipients.mkString(","), sendOp.asInstanceOf[Failure[_]].exception)
+    if (sendOp.isFailure) NotificationsSender.msLogger.error("Error sending alarm state notification notification to {}", recipients.mkString(","), sendOp.asInstanceOf[Failure[_]].exception)
   }
 
 
@@ -176,7 +182,7 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
     * @param iasValues the values read from the BSDB
     */
   override protected def process(iasValues: List[IASValue[_]]): Unit = synchronized {
-    msLogger.debug("Processing {} values read from BSDB",iasValues.length)
+    NotificationsSender.msLogger.debug("Processing {} values read from BSDB",iasValues.length)
     // Iterates over non-alarms IASIOs
     val valuesToUpdate = iasValues.filter(v => v.valueType==IASTypes.ALARM && alarmsToTrack.contains(v.id))
       .foreach(value => {
@@ -202,7 +208,7 @@ class NotificationsSender(id: String, val sender: Sender) extends ValueListener(
           case (None, Some(x)) => if (x.alarm != alarm) notifyAlarm(value.id,alarmsToTrack(value.id).getActualAlarmState().get)
         }
       })
-    msLogger.debug("{} values processed",iasValues.length)
+    NotificationsSender.msLogger.debug("{} values processed",iasValues.length)
   }
 }
 
@@ -218,4 +224,136 @@ object NotificationsSender {
 
   /** The default time (UTC) of the day to start periodic notifications */
   val StartTimeOfPeriodicNotificationsDefault = "08:00"
+
+  /** The logger */
+  val msLogger: Logger = IASLogger.getLogger(classOf[NotificationsSender])
+
+  def printUsage() = {
+    """Usage: emailSender <identifier> [-jcdb JSON-CDB-PATH]
+		-jcdb force the usage of the JSON CDB
+		   * identifier: the identifier of the mail sender
+		   * JSON-CDB-PATH: the path of the JSON CDB"""
+  }
+
+  /** Launch the emmail sender */
+  def main(args: Array[String]): Unit = {
+    require(args.nonEmpty,s"No command line\n${printUsage()}")
+    require(args.length==1 || args.length==3,s"Invalid number of parameters\n${printUsage()}")
+    require(if(args.size == 3) args(1)=="-jcdb" else true, s"Invalid command line params\n${printUsage()}")
+
+    // The id of the sender
+    val emailSenderId = args(0).trim
+
+    // The identifier
+    val emailSenderIdentifier = new Identifier(emailSenderId,IdentifierType.SINK,None)
+
+    // Get the CDB
+    val cdbReader: CdbReader = {
+      if (args.size == 3) {
+        msLogger.debug("Using JSON CDB in {}",args(2))
+        val cdbFiles: CdbFiles = new CdbJsonFiles(args(2))
+        new JsonReader(cdbFiles)
+      } else {
+        new RdbReader()
+      }
+    }
+
+    logger.debug("Getting the IAS frm the CDB")
+    val iasDao: IasDao = {
+      val iasOpt = cdbReader.getIas
+      if (!iasOpt.isPresent) {
+        msLogger.error("Error getting the IAS from the CDB")
+        System.exit(-1)
+      }
+      iasOpt.get()
+    }
+
+    /** The configuration of IASIOs from the CDB */
+    logger.debug("Getting the IASIOs frm the CDB")
+    val iasioDaos: List[IasioDao] = {
+      val temp: util.Set[IasioDao] = cdbReader.getIasios.orElseThrow(() => new IllegalArgumentException("IasDaos not found in CDB"))
+      JavaConverters.asScalaSet(temp).toList
+    }
+    logger.debug("Get {} IASIOs frm the CDB",iasioDaos.length)
+
+    cdbReader.shutdown()
+    logger.debug("CdbReader closed")
+
+    val (smtpServer, login,pswd) = {
+      val smtpServerOpt = Option(iasDao.getSmtp)
+      if (smtpServerOpt.isEmpty) {
+        println("ERROR: SMTP not found in the IAS configuration in the CDB")
+        System.exit(-2)
+      }
+      // The string from the CDB
+      // The format is user:password@smtpHost
+      val smtpConnStr = smtpServerOpt.get
+      if (smtpConnStr.count(_=='@')!=1 || smtpConnStr.count(_==':')!=1) {
+        msLogger.error(s"SMTP connection string [{}] has wrong format: expected [username]:[password]@smtphost",smtpConnStr)
+        System.exit(-2)
+      }
+
+      val serverParts = smtpConnStr.split('@')
+      if (serverParts.length!=2) {
+        msLogger.error(s"SMTP connection string [{}] has wrong format: expected [username]:[password]@smtphost",smtpConnStr)
+        System.exit(-3)
+      }
+      val server=serverParts(1).trim
+      val loginPswd=serverParts(0)
+
+      val loginNameParts = loginPswd.split(':')
+
+      if (loginNameParts.length==0) { // No login and no pswd
+        (server, None, None)
+      } else if (loginNameParts.length==1) { // Only login
+        (server, Some(loginNameParts(0).trim), None)
+      } else { // login and pswd but login can be empty "" that is an error
+        if (loginNameParts(0).isEmpty) {
+          msLogger.error(s"SMTP connection string [{}] has wrong format: expected [username]:[password]@smtphost",smtpConnStr)
+          System.exit(-4)
+        } else {
+          (server, Some(loginNameParts(0).trim), Some(loginNameParts(1)))
+        }
+      }
+    }
+    msLogger.info("SMTP server {}",smtpServer.asInstanceOf[String])
+    val mailSender: Sender = new SmtpSender(
+      server = smtpServer.asInstanceOf[String],
+      loginName = login.asInstanceOf[Option[String]],
+      pswd = pswd.asInstanceOf[Option[String]])
+
+
+    val valueListener: ValueListener = new NotificationsSender(emailSenderId,mailSender)
+    msLogger.debug("NotificationsSender instantiated")
+
+
+    val kafkaBrokers: Option[String] = {
+      val temp = Option(iasDao.getBsdbUrl)
+      if (temp.isEmpty) None
+      else if (temp.get.isEmpty) None
+      else temp
+    }
+
+    val hbProducer: HbProducer = {
+      val kafkaServers = System.getProperties.getProperty(KafkaHelper.BROKERS_PROPNAME,KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS)
+      new HbKafkaProducer(emailSenderId,kafkaServers,new HbJsonSerializer())
+    }
+    msLogger.debug("HB producer instantiated")
+
+    val inputsProvider: InputSubscriber = KafkaSubscriber(emailSenderId,None,kafkaBrokers,System.getProperties)
+    msLogger.debug("IAS values consumer instantiated")
+
+    val valuesProcessor: IasValueProcessor = new IasValueProcessor(
+      emailSenderIdentifier,
+      List(valueListener),
+      hbProducer,
+      inputsProvider,
+      iasDao,
+      iasioDaos)
+    msLogger.debug("IAS values processor instantiated")
+
+    // Start
+    msLogger.info("Starting the loop...")
+    valuesProcessor.init()
+  }
 }
