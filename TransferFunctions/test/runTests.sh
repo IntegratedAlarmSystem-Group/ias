6f321bb1bdcecf24505454eb5d6f75717bf7e019
Alessandro Caproni <acaproni@eso.org>
2018-07-02 20:22:26 -0400

    The StatusWord helper to deal with the state returned by utulity modules

--- TransferFunctions/test/runTests.sh
+++ TransferFunctions/test/runTests.sh
@@ -2,3 +2,4 @@
 iasRun -l s org.scalatest.run org.eso.ias.transfer.test.DelayedAlarmTest
 iasRun -l s org.scalatest.run org.eso.ias.transfer.test.BackupSelectorTest
 iasRun -l s org.scalatest.run org.eso.ias.transfer.test.ThresholdBackupAndDelayTest
+iasRun -l s org.scalatest.run org.eso.ias.transfer.test.utilitymodule.StatusWordTest
