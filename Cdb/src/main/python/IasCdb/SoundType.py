3c11a9d59161ab5f2680036093cd388441c6adbc
Alessandro Caproni <acaproni@eso.org>
2018-09-03 17:58:20 -0300

    DAO for enumarated

---
+++ Cdb/src/main/python/IasCdb/SoundType.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class SoundType(Enum):
+    '''
+    The type of sound
+
+    See SoundTypeDao.java
+    '''
+    NONE=0
+    TYPE1=1
+    TYPE2=2
+    TYPE3=3
+    TYPE4=4
+    
