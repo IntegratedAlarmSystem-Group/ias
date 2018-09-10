0d3b9685aed4d3c3813f32cebaad9181f369ffff
Alessandro Caproni <acaproni@eso.org>
2018-09-10 17:42:43 -0300

    Adds a test for AntPadInhibitor

--- TransferFunctions/src/test/runTests.sh
+++ TransferFunctions/src/test/runTests.sh
@@ -3,3 +3,5 @@ iasRun -l s org.scalatest.run org.eso.ias.transfer.test.DelayedAlarmTest
 iasRun -l s org.scalatest.run org.eso.ias.transfer.test.BackupSelectorTest
 iasRun -l s org.scalatest.run org.eso.ias.transfer.test.ThresholdBackupAndDelayTest
 iasRun -l s org.scalatest.run org.eso.ias.transfer.test.utilitymodule.StatusWordTest
+iasRun -l s org.scalatest.run org.eso.ias.transfer.test.AntPadInhibitorTest
+
