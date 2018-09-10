0a594627a1eb5ebd8db1315b79a0e35276df8ab0
Alessandro Caproni <acaproni@eso.org>
2018-09-10 16:54:58 -0300

    Initialize the templateInstance to empty

--- CompElement/src/main/java/org/eso/ias/asce/transfer/TransferExecutor.java
+++ CompElement/src/main/java/org/eso/ias/asce/transfer/TransferExecutor.java
@@ -50,7 +50,7 @@ public abstract class TransferExecutor {
 	 * If defined, this is the instance of the ASCE where the TF runs;
 	 * if empty the ASCE is not generated out of a template
 	 */
-	private Optional<Integer> templateInstance;
+	private Optional<Integer> templateInstance = Optional.empty();
 
     /**
      * The logger
