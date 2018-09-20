14bde4ca8475ec6e6bdffe78a86279aa6b6db7a1
Alessandro Caproni <acaproni@eso.org>
2018-09-20 16:17:23 -0300

    Renames TopologyTest to DasuTopologyTest

--- DistributedUnit/src/test/runTests.sh
+++ DistributedUnit/src/test/runTests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-iasRun -l s org.scalatest.run org.eso.ias.dasu.test.TopologyTest
+iasRun -l s org.scalatest.run org.eso.ias.dasu.test.DasuTopologyTest
 iasRun -l s org.scalatest.run org.eso.ias.dasu.test.DasuOneASCETest
 iasRun -l s org.scalatest.run org.eso.ias.dasu.test.CheckDasuOutputTimestamps
 iasRun -l s org.scalatest.run org.eso.ias.dasu.test.Dasu7ASCEsTest
