2c9d0e96af196c30bb0c2d508aa8c3a918687194
Alessandro Caproni <acaproni@eso.org>
2018-09-20 16:27:36 -0300

    Moves topology tests into Cdb

--- DistributedUnit/src/test/runTests.sh
+++ DistributedUnit/src/test/runTests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-iasRun -l s org.scalatest.run org.eso.ias.dasu.test.DasuTopologyTest
 iasRun -l s org.scalatest.run org.eso.ias.dasu.test.DasuOneASCETest
 iasRun -l s org.scalatest.run org.eso.ias.dasu.test.CheckDasuOutputTimestamps
 iasRun -l s org.scalatest.run org.eso.ias.dasu.test.Dasu7ASCEsTest
