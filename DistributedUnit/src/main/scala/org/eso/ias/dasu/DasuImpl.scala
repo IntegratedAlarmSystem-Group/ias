7426bfe778894d3aeb7aa4c24d0969da33f37b54
Alessandro Caproni <acaproni@eso.org>
2018-09-22 09:53:03 -0300

    Lower the log level of a message to DEBUG

--- DistributedUnit/src/main/scala/org/eso/ias/dasu/DasuImpl.scala
+++ DistributedUnit/src/main/scala/org/eso/ias/dasu/DasuImpl.scala
@@ -173,7 +173,7 @@ class DasuImpl (
    */
   val delayedUpdateTask = new Runnable {
       override def run() = {
-        logger.info("DASU [{}] running the throttling task",id)
+        logger.debug("DASU [{}] running the throttling task",id)
         updateAndPublishOutput()
       }
   }
