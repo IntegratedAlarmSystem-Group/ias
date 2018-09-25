ce94e6e00db32fe149a4ea0d81de5bddc806512f
Alessandro Caproni <acaproni@eso.org>
2018-09-25 18:37:04 +0000

    Statistcs extends StatsCollectorBase

--- Supervisor/src/main/scala/org/eso/ias/supervisor/StatsLogger.scala
+++ Supervisor/src/main/scala/org/eso/ias/supervisor/StatsLogger.scala
@@ -2,22 +2,21 @@ package org.eso.ias.supervisor
 
 import java.lang.management.ManagementFactory
 import java.util
-import java.util.Collections
 import java.util.concurrent._
 import java.util.concurrent.atomic.{AtomicLong, AtomicReference}
 
-import org.eso.ias.dasu.executorthread.ScheduledExecutor
+import org.eso.ias.dasu.StatsCollectorBase
 import org.eso.ias.logging.IASLogger
 
-import scala.collection.JavaConverters
 import scala.util.Try
 
 /**
   * Stats logger produces statistics for the Supervisor
   *
+  * @param id The identifier of the Supervisor
   * @param dasusIds The IDs of the DAUS deployed in the Supervisur
   */
-class StatsLogger (val dasusIds: Set[String] ) {
+class StatsLogger (id: String, val dasusIds: Set[String]) extends StatsCollectorBase(id,StatsLogger.StatisticsTimeInterval) {
   require(Option(dasusIds).isDefined && !dasusIds.isEmpty,"Invalid undefined or empty list of DASU IDs")
 
   /** Total number of inputs forwarded to the DASU to be processed since when the Supervisor started */
@@ -25,7 +24,6 @@ class StatsLogger (val dasusIds: Set[String] ) {
 
   /** Total number of inputs forwarded to the DASU to be processed in the last time interval */
   val inputsProcessed = new AtomicLong(0)
-
 
   /**
     * The map to associate the execution time and processed inputs
@@ -39,11 +37,7 @@ class StatsLogger (val dasusIds: Set[String] ) {
   val dasusInputsAndFrequency: util.Map[String, (Long, Long)] = new ConcurrentHashMap[String,(Long,Long)]()
   dasusIds.foreach(id => dasusInputsAndFrequency.put(id,(0,0)))
 
-  /** The scheduler to log statistics */
-  private val scheduer: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
-  /** Th efuture to cancel logging of statistics */
-  private var future = new AtomicReference[ScheduledFuture[_]]()
 
   /** Add the number of inputs received to the accumulator */
   def numberOfInputsReceived(numberOfInputs: Int): Unit =  {
@@ -68,30 +62,8 @@ class StatsLogger (val dasusIds: Set[String] ) {
     dasusInputsAndFrequency.put(id, (newNumOfInputs, iterations))
   }
 
-  /** Start logging statistics */
-  def start() = synchronized {
-    Option(future.get()) match {
-      case Some(f) => StatsLogger.logger.warn("StatsCollector already started")
-      case None => future.set(scheduer.scheduleAtFixedRate( () =>
-        logStats(),
-        StatsLogger.StatisticsTimeInterval,
-        StatsLogger.StatisticsTimeInterval,
-        TimeUnit.MINUTES))
-    }
-    StatsLogger.logger.info("StatsLogger started: will publish stats every "+StatsLogger.StatisticsTimeInterval+" minutes")
-  }
-
-  /** Stop loging statistics */
-  def cleanUp() = synchronized {
-    Option(future.getAndSet(null)).foreach(f => {
-      f.cancel(false)
-      StatsLogger.logger.info("StatsLogger closed")
-    })
-
-  }
-
   /** Emit the logs with the statistics and reset the counters */
-  def logStats(): Unit = {
+  override def logStats(): Unit = {
     val totProcessedInputs = totInputsProcessed.getAndSet(0)
 
     val getUsedHeapMemory = ManagementFactory.getMemoryMXBean.getHeapMemoryUsage.getUsed
@@ -100,8 +72,8 @@ class StatsLogger (val dasusIds: Set[String] ) {
     message.append("Stats: ")
     message.append(totProcessedInputs)
     message.append(" used heap ");
-    message.append(getUsedHeapMemory)
-    message.append(" IASIOs processed so far ")
+    message.append(getUsedHeapMemory/1024)
+    message.append("Kb; IASIOs processed so far ")
     message.append(" (")
     message.append(totProcessedInputs/StatsLogger.StatisticsTimeInterval)
     message.append("/min); input in the last interval ")
