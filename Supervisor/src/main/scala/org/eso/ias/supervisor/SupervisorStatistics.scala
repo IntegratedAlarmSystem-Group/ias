6fc473e93fc40b7f4369681ff953c8707430d04b
Alessandro Caproni <acaproni@eso.org>
2018-09-28 11:44:20 +0000

    Renames the StatsCollector and logs the number of threads used by the JVM

--- Supervisor/src/main/scala/org/eso/ias/supervisor/SupervisorStatistics.scala
+++ Supervisor/src/main/scala/org/eso/ias/supervisor/SupervisorStatistics.scala
@@ -17,7 +17,7 @@ import scala.util.Try
   * @param id The identifier of the Supervisor
   * @param dasusIds The IDs of the DAUS deployed in the Supervisur
   */
-class StatsLogger (id: String, val dasusIds: Set[String]) extends StatsCollectorBase(id,StatsLogger.StatisticsTimeInterval) {
+class SupervisorStatistics(id: String, val dasusIds: Set[String]) extends StatsCollectorBase(id,SupervisorStatistics.StatisticsTimeInterval) {
   require(Option(dasusIds).isDefined && dasusIds.nonEmpty,"Invalid undefined or empty list of DASU IDs")
 
   /** Total number of inputs forwarded to the DASU to be processed since when the Supervisor started */
@@ -68,19 +68,25 @@ class StatsLogger (id: String, val dasusIds: Set[String]) extends StatsCollector
     val totProcessedInputs = totInputsProcessed.getAndSet(0)
 
     val getUsedHeapMemory = ManagementFactory.getMemoryMXBean.getHeapMemoryUsage.getUsed
+    val totNumberOfThreads = ManagementFactory.getThreadMXBean.getThreadCount
 
     val message= StringBuilder.newBuilder
     message.append("Stats: ")
     message.append(totProcessedInputs)
     message.append(" used heap ")
     message.append(getUsedHeapMemory/1024)
-    message.append("Kb; IASIOs processed so far ")
+    message.append("Kb; alive thrreads ")
+    message.append(totNumberOfThreads)
+    message.append("; IASIOs processed so far ")
     message.append(totInputsProcessed.get)
     message.append(" (")
-    message.append(totProcessedInputs/StatsLogger.StatisticsTimeInterval)
+    message.append(totProcessedInputs/SupervisorStatistics.StatisticsTimeInterval)
     message.append("/min); input in the last interval ")
     message.append(inputsProcessed.get)
-    message.append("; statistics collected from DASUS: [")
+    SupervisorStatistics.logger.info(message.toString())
+
+    message.clear()
+    message.append("Statistics collected from DASUS: [")
     dasusInputsAndFrequency.keySet().forEach(id => {
       val dasuStats = dasusInputsAndFrequency.get(id)
       message.append("DASU ")
@@ -88,21 +94,21 @@ class StatsLogger (id: String, val dasusIds: Set[String]) extends StatsCollector
       message.append(" #inputs=")
       message.append(dasuStats._1)
       message.append(' ')
-      message.append(dasuStats._2/StatsLogger.StatisticsTimeInterval)
+      message.append(dasuStats._2/SupervisorStatistics.StatisticsTimeInterval)
       message.append("/min")
       message.append("] ")
     })
-    StatsLogger.logger.info(message.toString())
+    SupervisorStatistics.logger.info(message.toString())
     // Reset counters
     inputsProcessed.set(0)
     dasusInputsAndFrequency.keySet().forEach(id => dasusInputsAndFrequency.put(id, (0,0)))
   }
 }
 
-object StatsLogger {
+object SupervisorStatistics {
 
   /** The logger */
-  val logger: Logger = IASLogger.getLogger(StatsLogger.getClass)
+  val logger: Logger = IASLogger.getLogger(SupervisorStatistics.getClass)
 
   /** The time interval to log statistics (minutes) */
   val DefaultStatisticsTimeInterval = 10
