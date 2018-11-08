59b4de159d6eb401e1568cc72840b663d55fc7e9
Alessandro Caproni <acaproni@eso.org>
2018-11-08 18:10:17 -0300

    Get the property files from the module or the $IAS_ROOT

--- SinkClient/src/main/iasLTDBConnector.sh
+++ SinkClient/src/main/iasLTDBConnector.sh
@@ -19,6 +19,24 @@ else
 fi
 echo "Log4j config file = $LOG4JPROPS"
 
+CONN_PROPS="LtdbCassandraConnector.properties"
+if [ -e "../../config/$CONN_PROPS" ]
+then
+    CONN_PROPS_FILE="../../config/$CONN_PROPS"
+else
+    CONN_PROPS_FILE="$IAS_ROOT/config/$CONN_PROPS"
+fi
+echo "Connector properties file = $CONN_PROPS_FILE"
+
+STANDALONE_PROPS="LtdbCassandraStandalone.properties"
+if [ -e "../../config/$STANDALONE_PROPS" ]
+then
+    STANDALONE_PROPS_FILE="../../config/$STANDALONE_PROPS"
+else
+    STANDALONE_PROPS_FILE="$IAS_ROOT/config/$STANDALONE_PROPS"
+fi
+echo "Standalone properties file = $STANDALONE_PROPS_FILE"
+
 export KAFKA_LOG4J_OPTS="-Dlog4j.configurationFile=file:$LOG4JPROPS -Dlog4j.configuration=file:$LOG4JPROPS -Dias.logs.folder=$LOG_DIR"
-$KAFKA_HOME/bin/connect-standalone.sh standalone.properties connector.properties
+$KAFKA_HOME/bin/connect-standalone.sh $STANDALONE_PROPS_FILE $CONN_PROPS_FILE
 
