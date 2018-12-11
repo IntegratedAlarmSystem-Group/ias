8c5792b36312b7c8f881e600f435ecceb4c05968
acaproni <acaproni@eso.org>
2018-12-11 13:37:08 -0300

    Adds the skeleton of the class to test alarms generation

---
+++ Monitor/src/test/scala/org/eso/ias/monitor/test/HbAlarmGenerationTest.scala
@@ -0,0 +1,17 @@
+package org.eso.ias.monitor.test
+
+import org.scalatest.FlatSpec
+
+/**
+  * Test the generation of alarms for missing HBs
+  *
+  * The test
+  * - uses the config file in the test folder
+  * - produces the HBs
+  * - check the generation of alarms
+  */
+class HbAlarmGenerationTest extends FlatSpec {
+
+
+
+}
