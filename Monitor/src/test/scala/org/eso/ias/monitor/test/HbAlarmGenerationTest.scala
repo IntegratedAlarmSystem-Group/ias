00e667ea2517359889c3e1dcc4fe326eb0105746
Alessandro Caproni <acaproni@eso.org>
2018-12-11 17:17:23 -0300

    Tests the updating of alarm when HB is/is not received

--- Monitor/src/test/scala/org/eso/ias/monitor/test/HbAlarmGenerationTest.scala
+++ Monitor/src/test/scala/org/eso/ias/monitor/test/HbAlarmGenerationTest.scala
@@ -1,5 +1,8 @@
 package org.eso.ias.monitor.test
 
+import java.util.concurrent.TimeUnit
+
+import org.eso.ias.heartbeat.HeartbeatStatus
 import org.eso.ias.heartbeat.consumer.HbKafkaConsumer
 import org.eso.ias.heartbeat.publisher.HbKafkaProducer
 import org.eso.ias.heartbeat.serializer.HbJsonSerializer
@@ -34,11 +37,13 @@ class HbAlarmGenerationTest extends FlatSpec with BeforeAndAfterAll with BeforeA
   val p3Identifier = new Identifier("p3",IdentifierType.PLUGIN, Some(monitoredSystemIdentifier))
 
   val pluginIds = Set(p1Identifier.id,p2Identifier.id,p3Identifier.id)
+  val pluginsFrIds = Set(p1Identifier.fullRunningID,p2Identifier.fullRunningID,p3Identifier.fullRunningID)
 
   // Identifiers of the 2 converters
   val conv1Identifier = new Identifier("conv1",IdentifierType.CONVERTER, Some(p1Identifier))
   val conv2Identifier = new Identifier("conv2",IdentifierType.CONVERTER, Some(p2Identifier))
   val converterIds = Set(conv1Identifier.id, conv2Identifier.id)
+  val converterFrIds = Set(conv1Identifier.fullRunningID, conv2Identifier.fullRunningID)
 
   // Identifiers of the 3 clients
   val client1Identifier = new Identifier("client1",IdentifierType.CLIENT,None)
@@ -46,16 +51,23 @@ class HbAlarmGenerationTest extends FlatSpec with BeforeAndAfterAll with BeforeA
   val client3Identifier = new Identifier("client3",IdentifierType.CLIENT,None)
   val client4Identifier = new Identifier("client4",IdentifierType.CLIENT,None)
   val clientIds = Set(client1Identifier.id,client2Identifier.id,client3Identifier.id,client4Identifier.id)
+  val clientFrIds = Set(
+    client1Identifier.fullRunningID,
+    client2Identifier.fullRunningID,
+    client3Identifier.fullRunningID,
+    client4Identifier.fullRunningID)
 
   // Identifier of the sink client
   val sink1Identifier = new Identifier("sink1",IdentifierType.SINK,None)
   val sinkIds = Set(sink1Identifier.id)
+  val sinkFrIds = Set(sink1Identifier.fullRunningID)
 
   /** Identifiers of supervisors */
   val superv1Identifier = new Identifier("superv1",IdentifierType.SUPERVISOR)
   val superv12dentifier = new Identifier("superv2",IdentifierType.SUPERVISOR)
   val superv3Identifier = new Identifier("superv3",IdentifierType.SUPERVISOR)
   val supervisorIds = Set(superv1Identifier.id,superv12dentifier.id,superv3Identifier.id)
+  val supervisorFrIds = Set(superv1Identifier.fullRunningID,superv12dentifier.fullRunningID,superv3Identifier.fullRunningID)
 
   /** The producer of HBs */
   var hbProducer: HbKafkaProducer = _
@@ -68,6 +80,18 @@ class HbAlarmGenerationTest extends FlatSpec with BeforeAndAfterAll with BeforeA
 
   /** Threshold to send alarms if the HB has net been received */
   val threshold: Long = 5
+
+  /**
+    * Sends the HBs with the passed fullRunningIds
+    * @param frIds The fullRunningIds of the HBs to send
+    *
+    */
+  def sendHBs(frIds: Set[String]): Unit = {
+    frIds.foreach(frid => {
+      val str = hbSerializer.serializeToString(frid,HeartbeatStatus.RUNNING,Map.empty, System.currentTimeMillis())
+      hbProducer.push(str)
+    })
+  }
 
   override def beforeAll(): Unit = {
     hbProducer = new HbKafkaProducer("HBProducer-Test",KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS,hbSerializer)
@@ -84,9 +108,13 @@ class HbAlarmGenerationTest extends FlatSpec with BeforeAndAfterAll with BeforeA
     hbConsumer = new HbKafkaConsumer(KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS,"HB-Consumer")
 
     hbMonitor = new HbMonitor(hbConsumer,pluginIds,converterIds,clientIds,sinkIds,supervisorIds,threshold)
+    hbMonitor.start()
   }
 
-  override def afterEach(): Unit = super.afterEach()
+  override def afterEach(): Unit = {
+    hbMonitor.shutdown()
+    super.afterEach()
+  }
 
   behavior of "The HB monitor"
 
@@ -102,8 +130,34 @@ class HbAlarmGenerationTest extends FlatSpec with BeforeAndAfterAll with BeforeA
     assert(hbMonitor.threshold==threshold)
   }
 
-  it must "set all th ealarm CLEAR at the beginning" in {
+  it must "set CLEAR all the alarms at the beginning" in {
     MonitorAlarm.values().foreach( ma => assert(ma.getAlarm==Alarm.CLEARED))
+  }
+
+  it must "SET all alarms if HB not received in time" in {
+    Thread.sleep(TimeUnit.MILLISECONDS.convert(threshold+1,TimeUnit.SECONDS))
+    MonitorAlarm.values().foreach( ma => assert(ma.getAlarm!=Alarm.CLEARED))
+  }
+
+  it must "CLEAR or SET the alarms when HB is/is not received" in {
+    logger.info("Giving time to invalidate")
+    Thread.sleep(TimeUnit.MILLISECONDS.convert(threshold+1,TimeUnit.SECONDS))
+    MonitorAlarm.values().foreach( ma => assert(ma.getAlarm!=Alarm.CLEARED))
+    sendHBs(pluginsFrIds)
+    sendHBs(converterIds)
+    sendHBs(clientFrIds)
+    sendHBs(sinkFrIds)
+    sendHBs(supervisorFrIds)
+    logger.info("Giving time to update")
+    Thread.sleep(TimeUnit.MILLISECONDS.convert(threshold+1,TimeUnit.SECONDS))
+    MonitorAlarm.values().foreach( ma => {
+      logger.info("Alarm {} is {}",ma.name(),ma.getAlarm)
+    })
+    MonitorAlarm.values().foreach( ma => assert(ma.getAlarm==Alarm.CLEARED,ma.toString+" should be CLEAR"))
+
+    logger.info("Giving time to invalidate")
+    Thread.sleep(TimeUnit.MILLISECONDS.convert(threshold+1,TimeUnit.SECONDS))
+    MonitorAlarm.values().foreach( ma => assert(ma.getAlarm!=Alarm.CLEARED))
   }
 
 
