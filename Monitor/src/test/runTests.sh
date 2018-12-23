6d30a228d457d292d30f6cb8247d3e777a06861d
Alessandro Caproni <acaproni@eso.org>
2018-12-23 18:19:20 -0300

    Test the MonitorAlarm

--- Monitor/src/test/runTests.sh
+++ Monitor/src/test/runTests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 iasRun -l s org.scalatest.run org.eso.ias.monitor.test.ConfigTest
+iasRun -l s org.scalatest.run org.eso.ias.monitor.test.MonitorAlarmTest
 iasRun -l s org.scalatest.run org.eso.ias.monitor.test.HbAlarmGenerationTest
 
