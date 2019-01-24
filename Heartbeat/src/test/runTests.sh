df9019e846525d658dc40c5abc3f1199fe0a26f5
Alessandro Caproni <acaproni@eso.org>
2019-01-24 06:33:29 -0300

    Adds a test for the Heartbeat

--- Heartbeat/src/test/runTests.sh
+++ Heartbeat/src/test/runTests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-iasRun -l s org.scalatest.run org.eso.ias.heartbeat.test.JsonSerialization
+iasRun -l s org.scalatest.run org.eso.ias.heartbeat.test.TestJsonSerialization
+iasRun -l s org.scalatest.run org.eso.ias.heartbeat.test.TestHeartbeat
 iasRun -l s org.scalatest.run org.eso.ias.heartbeat.test.TestEngine
 iasRun -l s org.scalatest.run org.eso.ias.heartbeat.test.TestKafkaPublisher
---
+++ Heartbeat/src/test/scala/org/eso/ias/heartbeat/test/TestHeartbeat.scala
@@ -0,0 +1,51 @@
+package org.eso.ias.heartbeat.test
+
+import org.eso.ias.heartbeat.{Heartbeat, HeartbeatProducerType}
+import org.scalatest.FlatSpec
+
+/**
+  * Test the heartbeat
+  */
+class TestHeartbeat extends FlatSpec {
+
+  behavior of "The Heartbeat"
+
+  it must "correctly build the objects from type and name" in {
+    val name = "ToolName"
+    val toolTp = HeartbeatProducerType.CLIENT
+
+    val hb = Heartbeat(toolTp,name)
+
+    assert(hb.name==name)
+    assert(hb.hbType==toolTp)
+    assert(hb.hostName.nonEmpty)
+    assert(hb.stringRepr.nonEmpty)
+  }
+
+  it must "correctly build the objects from their string rpresentation" in {
+    val name = "ToolName"
+    val toolTp = HeartbeatProducerType.PLUGIN
+
+    val hb = Heartbeat(toolTp,name)
+
+    val hbFromStr = Heartbeat(hb.stringRepr)
+    assert(hbFromStr.name==hb.name)
+    assert(hbFromStr.hostName==hb.hostName)
+    assert(hbFromStr.hbType==hb.hbType)
+    assert(hbFromStr.stringRepr==hb.stringRepr)
+  }
+
+  it must "support all the tool types" in {
+    val supportedTypes = HeartbeatProducerType.values()
+
+    supportedTypes.foreach( tp => {
+      val name = "toolNameame-"+tp.name()
+      val hb = Heartbeat(tp,name)
+      assert(hb.hbType==tp)
+      assert(hb.name==name)
+      assert(hb.hostName.nonEmpty)
+    })
+  }
+
+
+}
