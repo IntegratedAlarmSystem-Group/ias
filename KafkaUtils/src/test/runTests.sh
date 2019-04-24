04b9c3096dfbe6d68b801cb64714b72d03b2c427
Alessandro Caproni <acaproni@eso.org>
2019-04-24 04:30:32 -0400

    Adds a test for slow processors of IASIOs (#161)

--- KafkaUtils/src/test/runTests.sh
+++ KafkaUtils/src/test/runTests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.kafkautils.test.ConsumerProducerTest
 iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.kafkautils.test.KafkaIasiosConsumerTest
+iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.kafkautils.test.SlowIasiosProcessorTest
 testValueProdCons
