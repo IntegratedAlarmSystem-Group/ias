979c68f9a59fb13f15ddeeec0f18b8f45613eb38
acaproni <RR!u-3JW>
2019-11-14 10:13:45 -0500

    Runs the TF in the python class
    The python code runs always in the same thread as required
    by jep.

--- CompElement/src/main/java/org/eso/ias/asce/transfer/PythonExecutorTF.java
+++ CompElement/src/main/java/org/eso/ias/asce/transfer/PythonExecutorTF.java
@@ -2,6 +2,7 @@ package org.eso.ias.asce.transfer;
 
 import jep.Interpreter;
 import jep.SharedInterpreter;
+import org.eso.ias.asce.CompEleThreadFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.*;
 
 /**
  * Execution of TF in python is orchestrated by objects of this class:
@@ -38,6 +40,13 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
     private Object pythonImpl = null;
 
     /**
+     * The thread to run python code in jep
+     *
+     * jep requires that all python calls must run in the same thread
+     */
+    private final ExecutorService executor;
+
+    /**
      * Constructor
      *
      * @param cEleId            : The id of the ASCE
@@ -48,14 +57,62 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
     public PythonExecutorTF(String cEleId, String cEleRunningId, long validityTimeFrame, Properties props) {
         super(cEleId, cEleRunningId, validityTimeFrame, props);
         logger.debug("Python TF executor built for ASCE {}",compElementRunningId);
+
+        executor = Executors.newSingleThreadExecutor(new CompEleThreadFactory("Python code executor thread for ASCE "+cEleId));
     }
 
+    /**
+     * Run the TF by delegating to the python implementation in th eproper thread
+     *
+     * @param compInputs: the inputs to the ASCE
+     * @param actualOutput: the actual output of the ASCE
+     * @return the new output
+     * @throws Exception
+     */
     @Override
     public IasIOJ<T> eval(Map<String, IasIOJ<?>> compInputs, IasIOJ<T> actualOutput) throws Exception {
         Objects.requireNonNull(pythonInterpreter,"eval invoked by the python interpreter is null");
-        logger.debug("Setting up params for python TF for ASCE {}",compElementRunningId);
+        logger.debug("Running python TF for ASCE {}",compElementRunningId);
 
-        pythonInterpreter.exec("inputs = {}}");
+        Callable<IasIOJ<T>>  callable = new Callable<IasIOJ<T>>() {
+            @Override
+            public IasIOJ<T> call() throws Exception {
+
+                logger.debug("The thread will run the python TF for ASCE {}",compElementRunningId);
+                IasIOJ<T> ret = PythonExecutorTF.this.evalPythonTask(compInputs,actualOutput);
+                logger.debug("The python TF for ASCE {} terminated without errors",compElementRunningId);
+                return ret;
+            }
+        };
+
+        logger.debug("Submitting python TF for ASCE {}",compElementRunningId);
+        Future<IasIOJ<T>> future = executor.submit(callable);
+
+        IasIOJ<T> newOutput;
+        try {
+            logger.debug("Waiting for termination python TF task for ASCE {}",compElementRunningId);
+            newOutput=future.get(1,TimeUnit.MINUTES);
+            logger.debug("Python TF task for ASCE {} terminated",compElementRunningId);
+        } catch (Exception e) {
+            e.printStackTrace();;
+            throw new Exception(
+                    "Exception caught while waiting for the execution of the python TF task of ASCE "+compElementRunningId,
+                    e);
+        }
+        return newOutput;
+    }
+
+    /**
+     * Run the TF in the python code
+     *
+     * @param compInputs: the inputs to the ASCE
+     * @param actualOutput: the actual output of the ASCE
+     * @return the new output
+     * @throws Exception
+     */
+    private IasIOJ<T> evalPythonTask(Map<String, IasIOJ<?>> compInputs, IasIOJ<T> actualOutput) throws Exception {
+        logger.debug("Setting up params to send to python object for ASCE {}",compElementRunningId);
+        pythonInterpreter.exec("inputs = {}");
         for (String key: compInputs.keySet()) {
             logger.debug("Converting input {}", key);
 
@@ -64,8 +121,11 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
             pythonInterpreter.set("mode", compInputs.get(key).getMode());
             pythonInterpreter.set("iasType", compInputs.get(key).getType());
             pythonInterpreter.set("validity", compInputs.get(key).getValidity());
-            pythonInterpreter.set("value", compInputs.get(key).getValue());
-            pythonInterpreter.set("prodTStamp", compInputs.get(key).productionTStamp());
+            assert compInputs.get(key).getValue().isPresent() : "Input value shall not be empty";
+            pythonInterpreter.set("value", compInputs.get(key).getValue().get());
+            assert compInputs.get(key).productionTStamp().isPresent() : "Input production timestamp shall not be empty";
+            pythonInterpreter.set("prodTStamp", compInputs.get(key).productionTStamp().get());
+
 
             pythonInterpreter.exec("props = {}");
             Map<String, String> props = compInputs.get(key).getProps();
@@ -77,9 +137,10 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
                 }
             }
 
-            pythonInterpreter.exec("input = IASIO(id,runningId,mode,iasType,validity,value,prodTStamp,props");
+            pythonInterpreter.exec("input = IASIO(id,runningId,mode,iasType,validity,value,prodTStamp,props)");
             pythonInterpreter.exec("inputs[id]=input");
         }
+        logger.debug("Inputs ready to be sent to python TF");
 
 
         logger.debug("Converting output {}",actualOutput.getId());
@@ -88,8 +149,16 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
         pythonInterpreter.set("mode", actualOutput.getMode());
         pythonInterpreter.set("iasType", actualOutput.getType());
         pythonInterpreter.set("validity", actualOutput.getValidity());
-        pythonInterpreter.set("value", actualOutput.getValue());
-        pythonInterpreter.set("prodTStamp", actualOutput.productionTStamp());
+        if (actualOutput.getValue().isPresent()) {
+            pythonInterpreter.set("value", actualOutput.getValue().get());
+        } else {
+            pythonInterpreter.exec("value = None");
+        }
+        if (actualOutput.productionTStamp().isPresent()) {
+            pythonInterpreter.set("prodTStamp", actualOutput.productionTStamp().get());
+        } else {
+            pythonInterpreter.exec("prodTStamp = None");
+        }
 
         pythonInterpreter.exec("props = {}");
         Map<String,String> props = actualOutput.getProps();
@@ -100,11 +169,11 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
                 pythonInterpreter.exec("props[key]=value");
             }
         }
+        logger.debug("Output ready to be sent to python TF");
 
-        pythonInterpreter.exec("actualOutput = IASIO(id,runningId,mode,iasType,validity,value,prodTStamp,props");
-
-        logger.debug("Invoking the python TF of ASCE {}",compElementRunningId);
-
+        pythonInterpreter.exec("actualOutput = IASIO(id,runningId,mode,iasType,validity,value,prodTStamp,props)");
+        logger.debug("Invoking eval on the python TF for ASCE {}",compElementRunningId);
+        pythonInterpreter.exec("pyTF.eval(inputs,actualOutput)");
 
         return null;
     }
@@ -112,6 +181,42 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
     @Override
     public void initialize(Set<IasioInfo> inputsInfo, IasioInfo outputInfo) throws Exception {
         logger.debug("Initializing python TF for {}",compElementRunningId);
+
+        Callable<?>  callable = new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                logger.debug("The thread will run the python code for ASCE {}",compElementRunningId);
+                PythonExecutorTF.this.initializePythonTask(inputsInfo, outputInfo);
+                logger.debug("The thread run the python code for ASCE {} terminated without errors",compElementRunningId);
+                return null;
+            }
+        };
+
+
+        logger.debug("Submitting initialization task of python TF for ASCE {}",compElementRunningId);
+        Future<?> future = executor.submit(callable);
+
+
+        try {
+            logger.debug("Waiting for termination of initialization task of python TF for ASCE {}",compElementRunningId);
+            future.get(1,TimeUnit.MINUTES);
+            logger.debug("Initialization task of python TF for ASCE {} terminated",compElementRunningId);
+        } catch (Exception e) {
+            throw new Exception(
+                    "Exception caught while waiting for the initialization of the python TF of ASCE "+compElementRunningId,
+                    e);
+        }
+
+        logger.info("Python TF for {} initialized",compElementRunningId);
+    }
+
+    /**
+     * Runs the initialize in the python code
+     *
+     * This method must be run with the executor because jep requires that
+     * all python code must be run in the same thread
+     */
+    private void initializePythonTask(Set<IasioInfo> inputsInfo, IasioInfo outputInfo) throws Exception {
         pythonInterpreter = new SharedInterpreter();
 
         logger.debug("Python interpreter built for {}",compElementRunningId);
@@ -156,12 +261,12 @@ public class PythonExecutorTF<T> extends JavaTransferExecutor<T> {
 
         logger.debug("Initializing python TF implementation of ASCE {}",compElementRunningId);
         pythonInterpreter.exec("pyTF.initialize(inputs,out)");
-
-        logger.info("Python TF for {} initialized",compElementRunningId);
     }
 
     @Override
     public void shutdown() throws Exception {
         logger.debug("Shutting down python TF for {}",compElementRunningId);
+        executor.shutdownNow();
+        logger.info("Java executor of python TF for {} is shut down",compElementRunningId);
     }
 }
