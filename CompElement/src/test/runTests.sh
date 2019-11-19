12aa3e50c8655d57eb9adad3a3cd9174d8cf891b
acaproni <RR!u-3JW>
2019-11-19 06:18:29 -0500

    Tests the implementation of the python TF

--- CompElement/src/test/runTests.sh
+++ CompElement/src/test/runTests.sh
@@ -5,5 +5,6 @@ iasRun -l s org.scalatest.run org.eso.ias.asce.test.TestTransferFunction
 iasRun -l s org.scalatest.run org.eso.ias.asce.test.TestMinMaxThreshold
 iasRun -l s org.scalatest.run org.eso.ias.asce.test.TestMultiplicityTF
 iasRun -l s org.scalatest.run org.eso.ias.asce.test.TestSlowTF
+iasRun -l s org.scalatest.run org.eso.ias.asce.test.TestPyTF
 iasRun -l s org.scalatest.run org.eso.ias.asce.test.TestMinMaxPyTF
 
