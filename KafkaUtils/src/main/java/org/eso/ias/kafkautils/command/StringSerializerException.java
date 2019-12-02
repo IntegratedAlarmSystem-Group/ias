a98b42401ff54ffcbf2c601015c07d44352fe47d
acaproni <RR!u-3JW>
2019-12-02 10:35:02 -0500

    Adds serialization to Json to/from strings to commands and replies - Issue#61

---
+++ KafkaUtils/src/main/java/org/eso/ias/kafkautils/command/StringSerializerException.java
@@ -0,0 +1,26 @@
+package org.eso.ias.kafkautils.command;
+
+/**
+ * Th eexception generated when marshalling or unmarshalling commands
+ * and replies to and from strings.
+ */
+public class StringSerializerException extends Exception {
+    public StringSerializerException() {
+    }
+
+    public StringSerializerException(String message) {
+        super(message);
+    }
+
+    public StringSerializerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StringSerializerException(Throwable cause) {
+        super(cause);
+    }
+
+    public StringSerializerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
