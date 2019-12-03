e248018ec3d1615a486c93ae3bc3470745d3c0e1
acaproni <RR!u-3JW>
2019-12-03 11:34:27 -0500

    Adds the test of the CommandManager

--- CommandsAndReplies/src/test/runTests.sh
+++ CommandsAndReplies/src/test/runTests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.command.test.TestCmdReplySerialization
+iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.command.test.TestCommandManager
 
