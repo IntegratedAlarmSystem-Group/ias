93ba3eb78e5d63e20d5552be06a1a54bf06a474e
acaproni <RR!u-3JW>
2019-12-04 11:05:26 -0500

    CommandManager builds the reply instead of the listener
    This ensure the correctness of the Reply and allow to set the timestamp when the command has been received.
    The listener sends only the execution status and, optionally, the properties.

--- CommandsAndReplies/src/main/java/org/eso/ias/command/CommandListener.java
+++ CommandsAndReplies/src/main/java/org/eso/ias/command/CommandListener.java
@@ -1,16 +1,50 @@
 package org.eso.ias.command;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
 /**
  * The interface of the listener of commands received from
  * the command topic
  */
 public interface CommandListener {
 
+    public class CmdExecutionResult {
+
+        /** The execution status of the command */
+        public final CommandExitStatus status;
+
+        /** Additional properties, if any */
+        public final Optional<Map<String, String>> properties;
+
+        /**
+         * Constructor
+         *
+         * @param status The exit status of the ocmmand
+         * @param props The map of propertis, if any
+         */
+        public CmdExecutionResult(CommandExitStatus status, Map<String, String> props) {
+            Objects.requireNonNull(status);
+            this.status=status;
+            properties = Optional.ofNullable(props);
+        }
+
+        /**
+         * Constructor with no properties
+         *
+         * @param status The exit status of the command
+         */
+        public CmdExecutionResult(CommandExitStatus status) {
+            this(status,null);
+        }
+    }
+
     /**
      * A new command has been received and must be exexuted by the listener.
      *
      * @param cmd The command received from the command topic
-     * @return  The reply produced executing the passed command
+     * @return  The not null reply produced executing the passed command
      */
-    public ReplyMessage newCommand(CommandMessage cmd);
+    public CmdExecutionResult newCommand(CommandMessage cmd);
 }
