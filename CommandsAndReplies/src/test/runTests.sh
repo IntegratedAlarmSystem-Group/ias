caf43021ba5ce5ea4947bc9bc2ba15e7cae86971
acaproni <RR!u-3JW>
2019-12-16 11:18:35 -0500

    Adds a test for python Command and Reply

--- CommandsAndReplies/src/test/runTests.sh
+++ CommandsAndReplies/src/test/runTests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.command.test.TestCmdReplySerialization
 iasRun -l j org.junit.platform.console.ConsoleLauncher -c org.eso.ias.command.test.TestCommandManager
+testCmdReply
 
