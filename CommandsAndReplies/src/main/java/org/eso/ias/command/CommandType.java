f00d068409e15766f8a2ecd3ad4dad333f4f2b83
acaproni <RR!u-3JW>
2019-12-16 11:20:33 -0500

    Adds ACK and TF_CHANGED

--- CommandsAndReplies/src/main/java/org/eso/ias/command/CommandType.java
+++ CommandsAndReplies/src/main/java/org/eso/ias/command/CommandType.java
@@ -4,8 +4,10 @@ package org.eso.ias.command;
  * The commands supported by the core
  */
 public enum CommandType {
-    PING,
-    SHUTDOWN,
-    RESTART,
-    SET_LOG_LEVEL
+    PING, // Do nothing
+    SHUTDOWN, // Shuts down the process
+    RESTART, // Restart the process
+    SET_LOG_LEVEL, // Set the log level of the process
+    ACK, // ACK an alarm
+    TF_CHANGED // Signal that a TF has been changed
 }
