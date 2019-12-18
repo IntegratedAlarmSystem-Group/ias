8b12042315d64c97d01382b1a436d1b04a49153b
acaproni <RR!u-3JW>
2019-12-18 03:20:37 -0500

    Use IAS logger instead of the standard one

--- KafkaUtils/src/main/python/IasKafkaUtils/KafkaValueConsumer.py
+++ KafkaUtils/src/main/python/IasKafkaUtils/KafkaValueConsumer.py
@@ -3,17 +3,18 @@ Created on Jun 12, 2018
 
 @author: acaproni
 '''
-import logging
-import time
 from datetime import datetime
 from threading import Thread
 
-from IasBasicTypes.IasValue import IasValue
-from IasBasicTypes.Iso8601TStamp import Iso8601TStamp
+import time
 from kafka import KafkaConsumer
 from kafka.structs import TopicPartition
 
-logger = logging.getLogger(__file__)
+from IASLogging.logConf import Log
+from IasBasicTypes.IasValue import IasValue
+from IasBasicTypes.Iso8601TStamp import Iso8601TStamp
+
+logger=Log.initLogging(__file__)
 
 class IasValueListener(object):
     """
