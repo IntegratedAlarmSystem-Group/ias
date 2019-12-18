f76306ce7895032e10b549bec405b41f24e545f1
acaproni <RR!u-3JW>
2019-12-18 08:56:27 -0500

    Adds the expeted nmumber of parameters for each command

--- CommandsAndReplies/src/main/java/org/eso/ias/command/CommandType.java
+++ CommandsAndReplies/src/main/java/org/eso/ias/command/CommandType.java
@@ -4,10 +4,19 @@ package org.eso.ias.command;
  * The commands supported by the core
  */
 public enum CommandType {
-    PING, // Do nothing
-    SHUTDOWN, // Shuts down the process
-    RESTART, // Restart the process
-    SET_LOG_LEVEL, // Set the log level of the process
-    ACK, // ACK an alarm
-    TF_CHANGED // Signal that a TF has been changed
+    PING(0), // Do nothing
+    SHUTDOWN(0), // Shuts down the process
+    RESTART(0), // Restart the process
+    SET_LOG_LEVEL(1), // Set the log level of the process
+    ACK(2), // ACK an alarm: one parameter is the ID of the alarm, the othe ris the comment of the operator
+    TF_CHANGED(1); // Signal that a TF has been changed with the ID of the TF
+
+    /**
+     * The expected number of parameters of the command
+     */
+    public final int expectedNumOfParameters;
+
+    private CommandType(int nParams) {
+        expectedNumOfParameters=nParams;
+    }
 }
