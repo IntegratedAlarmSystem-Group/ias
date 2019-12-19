85a0505119ae51a861fe17e26c3145fa6d771120
acaproni <RR!u-3JW>
2019-12-19 08:15:33 -0500

    Adds a test for the supervisor command executor

--- Supervisor/src/test/runTests.sh
+++ Supervisor/src/test/runTests.sh
@@ -3,3 +3,4 @@ iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorWithTemplatesTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.TemplatedInputTest
 iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorWithKafkaTest
+iasRun -l s org.scalatest.run org.eso.ias.supervisor.test.SupervisorCmdExecutorTest
