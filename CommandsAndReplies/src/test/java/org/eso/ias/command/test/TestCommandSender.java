cfff2f342768d5dae5156c90de616ab22fe1e43e
acaproni <RR!u-3JW>
2019-12-30 10:36:11 -0500

    Tests the sync send and the restart

--- CommandsAndReplies/src/test/java/org/eso/ias/command/test/TestCommandSender.java
+++ CommandsAndReplies/src/test/java/org/eso/ias/command/test/TestCommandSender.java
@@ -2,60 +2,129 @@ package org.eso.ias.command.test;
 
 import org.eso.ias.command.CommandSender;
 import org.eso.ias.command.CommandType;
+import org.eso.ias.heartbeat.consumer.HbKafkaConsumer;
+import org.eso.ias.heartbeat.consumer.HbListener;
+import org.eso.ias.heartbeat.consumer.HbMsg;
 import org.eso.ias.kafkautils.KafkaHelper;
 import org.eso.ias.types.Identifier;
 import org.eso.ias.types.IdentifierType;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scala.Option;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestCommandSender {
+/**
+ * Test the command center by running the {@link CommandManagerSimulator}.
+ *
+ * The simulator gets and executes commands that this test sends using the {@link CommandSender}.
+ */
+public class TestCommandSender implements HbListener {
+
+    /** The logger */
+    private static final Logger logger = LoggerFactory.getLogger(TestCommandSender.class);
+
+    public static final String id ="TestCommandSenderID";
 
     /**
      * The Identifier of this tool
      */
-    public static final Identifier id = new Identifier("senderID", IdentifierType.CLIENT, Option.empty());
+    public static final Identifier identifier = new Identifier(id, IdentifierType.CLIENT, Option.empty());
 
     /**
      * The {@link CommandSender} to test
      */
-    public static CommandSender cmdSender;
+    public static final CommandSender cmdSender = new CommandSender(identifier, KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS);
 
-    private static final String destId = "CmdExeId";
+    /** The id of the simulator */
+    private static final String destId = CommandManagerSimulator.id;
+
+    /** The process of the simulator i.e. iasRun */
+    private static Process simulatorProc;
 
     /**
      * The Identifier of the command executor
      */
     public static final Identifier executorId = new Identifier(destId, IdentifierType.CLIENT, Option.empty());
 
+    /** The consumer of HBs */
+    private HbKafkaConsumer hbConsumer;
+
+    /** The HBs received */
+    public static final List<HbMsg> hbs = Collections.synchronizedList(new Vector<>());
+
     @BeforeAll
     public static void setUpAll() throws Exception {
-        cmdSender = new CommandSender(id, KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS);
+        logger.info("Setting up the CommandSender");
         cmdSender.setUp();
-
+        simulatorProc=runSimulator();
     }
 
-    /**
-     * @see AutoCloseable
-     */
+    @BeforeEach
+    public void setUp() throws Exception {
+        hbs.clear();
+        hbConsumer = new HbKafkaConsumer(KafkaHelper.DEFAULT_BOOTSTRAP_BROKERS,id+"-TestHbCons");
+        hbConsumer.addListener(this);
+        hbConsumer.start();
+    }
+
     @AfterEach
-    public void close() throws Exception {
+    public void tearDown() throws Exception {
+        hbConsumer.shutdown();
     }
 
     @AfterAll
-    public static void tearDownAll() throws Exception {
+    public static void close() throws Exception {
+        logger.info("Sending command to terminate the simulator");
+        cmdSender.sendAsync(destId,CommandType.SHUTDOWN,null,null);
+        logger.debug("Closing the command sender");
+        Thread.sleep(1000);
         cmdSender.close();
+        logger.info("Terminated");
     }
 
+    /**
+     * Run the simulator invoking iasRun
+     *
+     * @return
+     */
+    private static Process runSimulator() throws Exception  {
+        logger.info("Running the simulator");
+        List<String> cmdToRun = new Vector<>();
+        cmdToRun.add("iasRun");
+        cmdToRun.add("-l");
+        cmdToRun.add("j");
+        cmdToRun.add("org.eso.ias.command.test.CommandManagerSimulator");
+        ProcessBuilder procBuilder = new ProcessBuilder(cmdToRun);
+        procBuilder.inheritIO();
+        Process p = procBuilder.start();
+        // Give some time to init
+        logger.info("Give the simulator time to be ready...");
+        Thread.sleep(10000);
+        assertTrue(p.isAlive());
+        logger.info("Simulator started");
+        return p;
+    }
 
     /**
-     * Send a synch command to a non existent destination and wait for the reply that must never arrive
+     * An HB has been received
+     *
+     * @param hbMsg
+     */
+    @Override
+    public void hbReceived(HbMsg hbMsg) {
+        hbs.add(hbMsg);
+    }
+
+    /**
+     * Send a sync command to a non existent destination and wait for the reply that must never arrive
      *
      * @throws Exception
      */
@@ -63,7 +132,30 @@ public class TestCommandSender {
     public void testMissingReply() throws Exception {
         String dest = "DoesNotExist";
 
-        boolean ret =cmdSender.sendSync(dest, CommandType.PING,null,null,3, TimeUnit.SECONDS);
+        logger.info("Sending (sync) PING to {}",dest);
+        boolean ret =cmdSender.sendSync(dest, CommandType.PING,null,null,20, TimeUnit.SECONDS);
         assertFalse(ret);
     }
+
+    /** Synchronously sends a command and wait for its execution */
+    @Test
+    public void testSyncSend() throws Exception {
+        logger.info("Sending (sync) PING to {}",destId);
+        assertTrue(
+                cmdSender.sendSync(destId,CommandType.PING,null,null,30,TimeUnit.SECONDS),
+                "Send sync did not terminate i.e. timeout");
+    }
+
+    @Test
+    public void testRestart() throws Exception {
+        logger.info("Sending (sync) RESTART to {}",destId);
+      assertTrue(
+                cmdSender.sendSync(destId,CommandType.RESTART,null,null,30,TimeUnit.SECONDS),
+                "Send sync did not terminate i.e. timeout");
+      hbs.clear();
+      logger.info("Give the process time to restart");
+      Thread.sleep(10000);
+      assertTrue(!hbs.isEmpty(),"NO HB received from the restarted process");
+    }
+
 }
