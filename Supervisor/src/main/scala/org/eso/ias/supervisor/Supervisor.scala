c95d3271117f1159f52b77ba3ded2dac08a24489
acaproni <RR!u-3JW>
2020-03-18 04:32:53 -0400

    Gets the CdbReader with the CdbReaderFactory

--- Supervisor/src/main/scala/org/eso/ias/supervisor/Supervisor.scala
+++ Supervisor/src/main/scala/org/eso/ias/supervisor/Supervisor.scala
@@ -6,7 +6,7 @@ import java.util.concurrent.{CountDownLatch, TimeUnit}
 import ch.qos.logback.classic.Level
 import com.typesafe.scalalogging.Logger
 import org.apache.commons.cli.{CommandLine, CommandLineParser, DefaultParser, HelpFormatter, Options}
-import org.eso.ias.cdb.CdbReader
+import org.eso.ias.cdb.{CdbReader, CdbReaderFactory}
 import org.eso.ias.cdb.json.{CdbFiles, CdbJsonFiles, JsonReader}
 import org.eso.ias.cdb.pojos._
 import org.eso.ias.cdb.rdb.RdbReader
@@ -567,16 +567,7 @@ object Supervisor {
 
     val supervisorId = parsedArgs._1.get
 
-    val reader: CdbReader = {
-      if (parsedArgs._2.isDefined) {
-        val jsonCdbPath = parsedArgs._2.get
-        Supervisor.logger.info("Using JSON CDB @ {}",jsonCdbPath)
-        val cdbFiles: CdbFiles = new CdbJsonFiles(jsonCdbPath)
-        new JsonReader(cdbFiles)
-      } else {
-        new RdbReader()
-      }
-    }
+    val reader: CdbReader = CdbReaderFactory.getCdbReader(args)
 
     /** 
      *  Refresh rate and validityThreshold: it uses the first defined ones:
