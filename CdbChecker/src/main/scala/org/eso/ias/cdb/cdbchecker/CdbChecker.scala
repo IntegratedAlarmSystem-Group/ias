9766379eaf9bd6a9fdb4c4047ed9c07523fb4fc3
acaproni <RR!u-3JW>
2020-04-03 09:58:46 -0400

    Shuts the CDB reader down before exiting

--- CdbChecker/src/main/scala/org/eso/ias/cdb/cdbchecker/CdbChecker.scala
+++ CdbChecker/src/main/scala/org/eso/ias/cdb/cdbchecker/CdbChecker.scala
@@ -29,6 +29,7 @@ class CdbChecker(args: Array[String]) {
 
   /** The reader of the JSON of RDB CDB */
   val reader: CdbReader = CdbReaderFactory.getCdbReader(args)
+  reader.init()
 
   // Are there errors in the IAS?
   val iasDaoOpt: Option[IasDao] = {
@@ -270,6 +271,7 @@ class CdbChecker(args: Array[String]) {
   } else {
     CdbChecker.logger.info("NO cycles found in the DASUs")
   }
+  reader.shutdown()
 
   /**
     * Build the map of the ASCEs to run in each DASU
