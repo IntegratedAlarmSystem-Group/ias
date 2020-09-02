d74f53561fdab521e3500ece4cce0814410ffd59
Alessandro Caproni <acaproni@eso.org>
2020-09-02 16:51:59 +0530

    Implements the copyFiles task

--- WafBuildTools/src/main/python/IasWafBuildTools/JarTasks.py
+++ WafBuildTools/src/main/python/IasWafBuildTools/JarTasks.py
@@ -47,16 +47,33 @@ class getJarsFromZip(Task):
         self.exec_command("rm -rf "+tempFolder)
 
 
-class getExternalJars(Task):
+class copyFiles(Task):
     """
     Get external JARS from extTools folder
 
-    - input: the Node of the ZIP file must be passed as the first item of the inputs list
-    - output: the jars to be taken from the zip file must be passed as Node in the outputs list
+    This task copies the input nodes in the output nodes.
+    Each input is copied in the output i.e. self.inputs[0] is copied in self.outputs[0] (this allows for renaming)
+    The number of inputs must be equal to the number of outputs.
+
+    - self.input: the Node of the ZIP file must be passed as the first item of the inputs list
+    - self.outputs: the jars to be taken from the zip file must be passed as Node in the outputs list
       (note that the output nodes also contains the path where the jar must be put)
 
     :return:
     """
 
     def run(self):
-        pass
+        print("copyFiles")
+        ins = list(map(lambda x: x.abspath(), self.inputs))
+        outs= list(map(lambda x: x.abspath(), self.outputs))
+
+        print(ins)
+        print(outs)
+
+        if len(ins)!=len(outs):
+            print("Number of inputs and outputs differ")
+            return
+
+        for i in range(len(ins)):
+            print("cp",ins[i],"=>",outs[i])
+            self.exec_command("cp "+ins[i]+" "+outs[i])
