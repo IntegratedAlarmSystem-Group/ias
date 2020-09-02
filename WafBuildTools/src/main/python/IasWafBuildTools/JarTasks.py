25980e03883ca5536b7fe7c77035d9268119757e
Alessandro Caproni <acaproni@eso.org>
2020-09-02 17:40:40 +0530

    Adds a task to extract files from a tar file

--- WafBuildTools/src/main/python/IasWafBuildTools/JarTasks.py
+++ WafBuildTools/src/main/python/IasWafBuildTools/JarTasks.py
@@ -14,7 +14,7 @@ class getJarsFromZip(Task):
     """
         Get the jars from the ZIP file in input
 
-        - input: the Node of the ZIP file must be passed as the first item of the inputs list
+        - input: the Node of the ZIP file must be passed as the first and only item of the inputs list
         - output: the jars to be taken from the zip file must be passed as Node in the outputs list
                   (note that the output nodes also contains the path where the jar must be copied)
     """
@@ -37,6 +37,45 @@ class getJarsFromZip(Task):
 
         print("Unzipping",self.inputs[0].abspath())
         self.exec_command("unzip -qq -d "+tempFolder+" "+self.inputs[0].abspath())
+
+        print("Extracting jars")
+        for path in Path(tempFolder).rglob('*.jar'):
+            if path.name in cleanedJarNames:
+                print('Found', path.name, "in", self.inputs[0].abspath())
+                self.exec_command("cp "+os.fspath(path.resolve())+" "+cleanedJarNames[path.name])
+
+        # Remove the temp folder
+        print("Removing temporary folder",tempFolder)
+        self.exec_command("rm -rf "+tempFolder)
+
+
+class getJarsFromTar(Task):
+    """
+        Get the jars from the TAR file in input
+
+        - input: the Node of the TAR file must be passed as the first and only item of the inputs list
+        - output: the jars to be taken from the tar file must be passed as Node in the outputs list
+                  (note that the output nodes also contains the path where the jar must be copied)
+    """
+    def run(self):
+        print("ZIP file to scan:", self.inputs)
+        print("JARS to get from zip file:", self.outputs)
+
+        # The dictionary associates the names of the jar to their destination paths
+        cleanedJarNames = {}
+        for j in self.outputs:
+            p = Path(j.abspath())
+            cleanedJarNames[p.name]=j.abspath()
+
+        # get the output folder from one of the jars
+        outDir = self.outputs[0].abspath()[:self.outputs[0].abspath().rfind('/')+1]
+        print("Output folder:",outDir)
+
+        tempFolder = mkdtemp(prefix="getJarsFromTar.", suffix=".temp")
+        print("Using temporary folder", tempFolder)
+
+        print("Un-tarring",self.inputs[0].abspath())
+        self.exec_command("tar xf "+self.inputs[0].abspath()+" --directory "+tempFolder)
 
         print("Extracting jars")
         for path in Path(tempFolder).rglob('*.jar'):
