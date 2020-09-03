885cdbec42f0d049366f0e14594ea6cbb2a5c48b
Alessandro Caproni <acaproni@eso.org>
2020-09-03 13:14:35 +0530

    getJarsFromTar and getJarsFromZip extnds getFilesFromArchive
    As getJarsFromTar and getJarsFromZip only differs from the linux command
    to unpack the archive, the base class implements the heuristic running
    the command to unpack the parchive passed by the specialised class

--- WafBuildTools/src/main/python/IasWafBuildTools/JarTasks.py
+++ WafBuildTools/src/main/python/IasWafBuildTools/JarTasks.py
@@ -11,17 +11,45 @@ from tempfile import mkdtemp
 from waflib.Task import Task
 
 
-class getJarsFromZip(Task):
+class getFilesFromArchive(Task):
     """
-        Get the jars from the ZIP file in input
+        Get a set of files from an archive
 
-        - input: the Node of the ZIP file must be passed as the first and only item of the inputs list
+        The file to be taken are passed as nodes in the outputs list and are saved in the extTools folder.
+
+        This class is the base class of getJarsFromZip and getJarsFromTar that only differ for the command used
+        to get the files out of a ZIP or TAR archive.
+        The command to extract must be passed by calling getFilesFromArchive.setCommand.
+        This allows, if needed, to extract from different types of archives passing the proper command.
+
+        - input: the Node of the archive must be passed as the first item of the inputs list
         - output: the jars to be taken from the zip file must be passed as Node in the outputs list
                   (note that the output nodes also contains the path where the jar must be copied)
     """
+
+    cmdToExtractFiles = None
+
+    def set_command(self, cmd):
+        """
+        Set the command to unpack the archive
+
+        The folder to unpack is set by running this task to a temporary folder: the command
+        must contain "{}" as place holder to add the path of the destination folder usgin the str.format()
+        method. If destination folder is not needed, then cmd must not contain {} as str.format would not
+        change the cmd string.
+
+        :param cmd: The command
+        :return:
+        """
+        self.cmdToExtractFiles = cmd
+
     def run(self):
-        print("ZIP file to scan:", self.inputs)
-        print("JARS to get from zip file:", self.outputs)
+        if not self.cmdToExtractFiles:
+            raise Exception('Command to extract not set: call setCommand() before running this task')
+
+        print("Archive file:", self.inputs)
+        print("Files to get from archive:", self.outputs)
+        print('Command to unpack archive',self.cmdToExtractFiles)
 
         # The dictionary associates the names of the files to their destination paths
         cleanedFileNames = {} # For example {f.out -> /a/b/c/f.out}
@@ -33,24 +61,23 @@ class getJarsFromZip(Task):
         outDir = self.outputs[0].abspath()[:self.outputs[0].abspath().rfind('/')+1]
         print("Output folder:", outDir)
 
-        tempFolder = mkdtemp(prefix="getJarsFromZip.", suffix=".temp")
+        tempFolder = mkdtemp(prefix="getFilesFromArchive.", suffix=".temp")
         print("Using temporary folder", tempFolder)
 
-        print("Unzipping",self.inputs[0].abspath())
-        self.exec_command("unzip -qq -d "+tempFolder+" "+self.inputs[0].abspath())
+        print("Unzipping", self.inputs[0].abspath())
+        self.exec_command(self.cmdToExtractFiles.format(tempFolder))
 
-        print("Extracting jars")
+        print("Extracting files")
         for fileName in cleanedFileNames.keys():
             files = sorted(Path(tempFolder).rglob(fileName))
 
-            print("+++ ",fileName,"===>>>",files)
             if not files:
-                raise FileNotFoundError( errno.ENOENT, os.strerror(errno.ENOENT), fileName)
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), fileName)
             elif len(files)>1:
                 raise Exception("The archive contains "+len(files)+" occurrences of "+fileName+": "+files)
             else:
                 # File found
-                print('File', files[0].name, " will be copied in", cleanedFileNames[fileName])
+                print('File', files[0].name, " will be copied in ==>", cleanedFileNames[fileName])
                 self.exec_command("cp "+os.fspath(files[0].resolve())+" "+cleanedFileNames[fileName])
 
         # Remove the temp folder
@@ -58,7 +85,21 @@ class getJarsFromZip(Task):
         self.exec_command("rm -rf "+tempFolder)
 
 
-class getJarsFromTar(Task):
+class getJarsFromZip(getFilesFromArchive):
+    """
+        Get the jars from the ZIP file in input.
+
+        - input: the Node of the ZIP file must be passed as the first and only item of the inputs list
+        - output: the jars to be taken from the zip file must be passed as Node in the outputs list
+                  (note that the output nodes also contains the path where the jar must be copied)
+    """
+    def run(self):
+        # Delegates to getFilesFromArchive.run()
+        self.set_command("unzip -qq -d {} "+self.inputs[0].abspath())
+        super(getJarsFromZip, self).run()
+
+
+class getJarsFromTar(getFilesFromArchive):
     """
         Get the jars from the TAR file in input
 
@@ -67,34 +108,9 @@ class getJarsFromTar(Task):
                   (note that the output nodes also contains the path where the jar must be copied)
     """
     def run(self):
-        print("ZIP file to scan:", self.inputs)
-        print("JARS to get from zip file:", self.outputs)
-
-        # The dictionary associates the names of the jar to their destination paths
-        cleanedJarNames = {}
-        for j in self.outputs:
-            p = Path(j.abspath())
-            cleanedJarNames[p.name]=j.abspath()
-
-        # get the output folder from one of the jars
-        outDir = self.outputs[0].abspath()[:self.outputs[0].abspath().rfind('/')+1]
-        print("Output folder:",outDir)
-
-        tempFolder = mkdtemp(prefix="getJarsFromTar.", suffix=".temp")
-        print("Using temporary folder", tempFolder)
-
-        print("Un-tarring",self.inputs[0].abspath())
-        self.exec_command("tar xf "+self.inputs[0].abspath()+" --directory "+tempFolder)
-
-        print("Extracting jars")
-        for path in Path(tempFolder).rglob('*.jar'):
-            if path.name in cleanedJarNames:
-                print('Found', path.name, "in", self.inputs[0].abspath())
-                self.exec_command("cp "+os.fspath(path.resolve())+" "+cleanedJarNames[path.name])
-
-        # Remove the temp folder
-        print("Removing temporary folder",tempFolder)
-        self.exec_command("rm -rf "+tempFolder)
+        # Delegates to getFilesFromArchive.run()
+        self.set_command("tar xf "+self.inputs[0].abspath()+" --directory {}")
+        super(getJarsFromTar, self).run()
 
 
 class copyFiles(Task):
