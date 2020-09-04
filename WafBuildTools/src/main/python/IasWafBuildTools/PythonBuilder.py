8fcd8a4c76ceed930b0a2a44b3d2c6702c6b52b9
Alessandro Caproni <acaproni@eso.org>
2020-09-04 17:51:57 +0530

    Adds a task to copy python modules in the build folder

--- WafBuildTools/src/main/python/IasWafBuildTools/PythonBuilder.py
+++ WafBuildTools/src/main/python/IasWafBuildTools/PythonBuilder.py
@@ -43,3 +43,50 @@ class BuildPythonScripts(Task):
             print('PY SRC',s.abspath(),'=>',d.abspath())
             self.exec_command("cp "+s.abspath()+" "+d.abspath())
             self.exec_command("chmod u+x "+d.abspath())
+
+
+class BuildPythonModules(Task):
+    '''
+    Build python modules.
+
+    Python modules are folder of python scripts that will be carbon copied
+    from the source folder src/main/python
+    to the destination folder build/lib/python<version>/site-packages
+
+    Source folders might be nested.
+    '''
+
+    def getPyModules(self):
+        '''
+        Scan the python source folder and preprae the list of inputs and outputs to be used by the build.
+        :return:
+        '''
+        # python sources in the main folder (to be discarded)
+        pySources = self.env.PYSRCFOLDER.ant_glob("*.py")
+        # All python sources
+        pyModSources = self.env.PYSRCFOLDER.ant_glob("**/*.py")
+        print('>>> pyModSources',pyModSources)
+
+        # A dictionary to associate each input file to the destination
+        self.filesToCopy = {}
+
+        for pySrc in pyModSources:
+            if pySrc not in pySources:
+                # The dest file must take into account subfolders
+                destDir = pySrc.abspath().replace(self.env.PYSRCFOLDER.abspath(),"",1) # Still contains the name of the file
+                pos = destDir.rfind('/')
+                destDir = destDir[1:pos]
+                destNode = self.env.PYMODDSTFOLDER.find_or_declare(destDir)
+                print('>>>>',destDir, ">>>>>",destNode.abspath())
+                self.set_inputs(pySrc)
+                dst = buildDstFileNode(pySrc, destNode)
+                self.set_outputs(dst)
+                self.filesToCopy[pySrc]=dst
+                print('>>>>',pySrc.abspath()," ==> ",dst.abspath())
+
+    def run(self):
+        print("ENV=",self.env)
+
+        for s,d in self.filesToCopy.items():
+            print('PY SRC',s.abspath(),'=>',d.abspath())
+            self.exec_command("cp "+s.abspath()+" "+d.abspath())
