94a7d190edc583539574dec612f3bb83b7cc06ae
Alessandro Caproni <acaproni@eso.org>
2020-09-04 19:34:13 +0530

    Remove the building of executables from python script
    It has been generalized and moved in ExecScripts.py

--- WafBuildTools/src/main/python/IasWafBuildTools/PythonBuilder.py
+++ WafBuildTools/src/main/python/IasWafBuildTools/PythonBuilder.py
@@ -5,45 +5,6 @@ Python classes to support building of python scripts and modules
 from waflib.Task import Task
 from IasWafBuildTools.Utils import buildDstFileNode
 
-class BuildPythonScripts(Task):
-    '''
-    Build the python scripts that are in src/main/python:
-    1. copy the files in build/bin
-    2. remove the .py extension
-    3 ensure they are executables
-    '''
-
-    def getPySources(self):
-        '''
-        Check if the src folder contains python scripts to build and, if it is the case,
-        add them to the list of the sources to build and the outputs to create
-        :return:
-        '''
-        print("Getting python sources")
-
-        # A dictionary to associate each input file to the destination
-        self.filesToCopy = {}
-
-        pySources = self.env.PYSRCFOLDER.ant_glob("*.py")
-        print("Python scripts to build",pySources)
-        self.set_inputs(pySources)
-
-        for pySrc in  pySources:
-            dst = buildDstFileNode(pySrc, self.env.BLDBINFOLDER,None,True)
-
-            self.set_outputs(dst)
-            self.filesToCopy[pySrc]=dst
-
-
-
-    def run(self):
-        print("ENV=",self.env)
-
-        for s,d in self.filesToCopy.items():
-            print('PY SRC',s.abspath(),'=>',d.abspath())
-            self.exec_command("cp "+s.abspath()+" "+d.abspath())
-            self.exec_command("chmod u+x "+d.abspath())
-
 
 class BuildPythonModules(Task):
     '''
@@ -56,6 +17,10 @@ class BuildPythonModules(Task):
     Source folders might be nested.
     '''
 
+    def __init__(self,environ):
+        super(BuildPythonModules, self).__init__(env=environ)
+        self.getPyModules()
+
     def getPyModules(self):
         '''
         Scan the python source folder and preprae the list of inputs and outputs to be used by the build.
@@ -65,7 +30,7 @@ class BuildPythonModules(Task):
         pySources = self.env.PYSRCFOLDER.ant_glob("*.py")
         # All python sources
         pyModSources = self.env.PYSRCFOLDER.ant_glob("**/*.py")
-        print('>>> pyModSources',pyModSources)
+        print('>>> pyModSources', pyModSources)
 
         # A dictionary to associate each input file to the destination
         self.filesToCopy = {}
@@ -77,12 +42,10 @@ class BuildPythonModules(Task):
                 pos = destDir.rfind('/')
                 destDir = destDir[1:pos]
                 destNode = self.env.PYMODDSTFOLDER.find_or_declare(destDir)
-                print('>>>>',destDir, ">>>>>",destNode.abspath())
                 self.set_inputs(pySrc)
                 dst = buildDstFileNode(pySrc, destNode)
                 self.set_outputs(dst)
                 self.filesToCopy[pySrc]=dst
-                print('>>>>',pySrc.abspath()," ==> ",dst.abspath())
 
     def run(self):
         print("ENV=",self.env)
