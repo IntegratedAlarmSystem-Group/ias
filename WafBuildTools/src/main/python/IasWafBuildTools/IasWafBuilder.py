168cecee71f40426379f0d4d44fd2cb266dcc929
Alessandro Caproni <acaproni@eso.org>
2021-01-05 21:07:05 +0530

    Adds a method to install the files

---
+++ WafBuildTools/src/main/python/IasWafBuildTools/IasWafBuilder.py
@@ -0,0 +1,23 @@
+from waflib import Logs
+
+def install(bld):
+    assert bld
+    foldersToInstall = [ 'bin', 'lib', 'extTools', 'config']
+    src = bld.env.DSTNODE
+    dst = bld.env.PREFIX
+
+    Logs.info("Installer: Installing files from %s ---to---> %s", src, dst)
+
+    for folder in foldersToInstall:
+
+        dstFolder = "%s/%s" % (dst, folder)
+        srcNode = src.find_node(folder)
+
+        if srcNode is None:
+            Logs.info("%s is empty: nothing to install", folder)
+
+        bld.install_files(
+            dstFolder,
+            srcNode.ant_glob("**/*"),
+            relative_trick=True,
+            cwd=srcNode)
