feddd66dad25ab97effbf7bb2212f61b8efeda72
Alessandro Caproni <acaproni@eso.org>
2021-06-24 15:22:18 +0530

    Uses common dependencies

--- PythonPluginFeeder/build.gradle.kts
+++ PythonPluginFeeder/build.gradle.kts
@@ -5,20 +5,22 @@ plugins {
 }
 
 dependencies {
+    val g = project.gradle
+    if (g is ExtensionAware) {
+      val extension = g as ExtensionAware
+      implementation(extension.extra["commons-cli"].toString())
+      implementation(extension.extra["jackson-databind"].toString())
+      implementation(extension.extra["slf4j-api"].toString())
+      implementation(extension.extra["junit-jupiter-api"].toString())
+      implementation(extension.extra["junit-jupiter-engine"].toString())
+    }
     
-    implementation("commons-cli:commons-cli:1.4")
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.4")
-
     implementation(project(":Plugin"))
     implementation(project(":Heartbeat"))
     implementation(project(":BasicTypes"))
     implementation(project(":Tools"))
     implementation(project(":Cdb"))
     implementation(project(":CommandsAndReplies"))
-
-    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
 
 base.archivesBaseName = "ias"+project.name
