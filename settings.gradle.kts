520fe60047076643edef116c9923d5278afa1f5f
Alessandro Caproni <acaproni@eso.org>
2021-06-24 15:26:54 +0530

    Uses the common dependencies with kafka-streams added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -59,5 +59,6 @@ if (gradle is ExtensionAware) {
 	extension.extra["junit-jupiter-engine"] = "org.junit.jupiter:junit-jupiter-engine:5.7.2"
 	extension.extra["commons-cli"] = "commons-cli:commons-cli:1.4"
 	extension.extra["kafka-clients"] = "org.apache.kafka:kafka-clients:2.8.0"
+	extension.extra["kafka-streams"] = "org.apache.kafka:kafka-streams:2.8.0"
 }
 
