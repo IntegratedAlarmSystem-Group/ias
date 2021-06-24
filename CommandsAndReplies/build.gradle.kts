d72793f2ded93051241c21a56b7bab8b45e0a03b
Alessandro Caproni <acaproni@eso.org>
2021-06-24 15:14:18 +0530

    Uses common dependencies

--- CommandsAndReplies/build.gradle.kts
+++ CommandsAndReplies/build.gradle.kts
@@ -5,18 +5,21 @@ plugins {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.5")
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.4")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    val g = project.gradle
+    if (g is ExtensionAware) {
+      val extension = g as ExtensionAware
+      implementation(extension.extra["scala-library"].toString())
+      implementation(extension.extra["jackson-databind"].toString())
+      implementation(extension.extra["slf4j-api"].toString())
+      implementation(extension.extra["logback-classic"].toString())
+      implementation(extension.extra["junit-jupiter-api"].toString())
+      implementation(extension.extra["junit-jupiter-engine"].toString())
+    }
+
     implementation(project(":Tools"))
     implementation(project(":KafkaUtils"))
     implementation(project(":BasicTypes"))
     implementation(project(":Heartbeat"))
-
-    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-
 }
 
 base.archivesBaseName = "ias"+project.name
