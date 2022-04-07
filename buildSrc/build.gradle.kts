43756872e9876ba023cbe4ab6e4aa1c9cc001412
Alessandro Caproni <acaproni@eso.org>
2022-04-07 13:35:33 +0530

    Upgrades kotlin-gradle-plugin to v1.6.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
 
     /* Depend on the kotlin plugin, since we want to access it in our plugin */
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
 
     /* Depend on the default Gradle API's since we want to build a custom plugin */
     compileOnly(gradleApi())
