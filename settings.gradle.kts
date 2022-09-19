2a65b82f094beded849c54fd58c6cdca95127748
Alessandro Caproni <acaproni@eso.org>
2022-09-19 20:52:29 +0530

    New version of kafka 3.2.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -60,9 +60,9 @@ if (gradle is ExtensionAware) {
 	extension.extra["junit-jupiter-engine"] = "org.junit.jupiter:junit-jupiter-engine:5.8.2"
 	extension.extra["junit-platform-console-standalone"] = "org.junit.platform:junit-platform-console-standalone:1.8.2"
 	extension.extra["commons-cli"] = "commons-cli:commons-cli:1.5.0"
-	extension.extra["kafka-clients"] = "org.apache.kafka:kafka-clients:3.2.0"
-	extension.extra["kafka-streams"] = "org.apache.kafka:kafka-streams:3.2.0"
-	extension.extra["kafka-connect-api"] = "org.apache.kafka:connect-api:3.2.0"
+	extension.extra["kafka-clients"] = "org.apache.kafka:kafka-clients:3.2.1"
+	extension.extra["kafka-streams"] = "org.apache.kafka:kafka-streams:3.2.1"
+	extension.extra["kafka-connect-api"] = "org.apache.kafka:connect-api:3.2.1"
 	extension.extra["hibernate-jpa"] = "org.hibernate.javax.persistence:hibernate-jpa-2.1-api:1.0.0.Final"
 }
 
