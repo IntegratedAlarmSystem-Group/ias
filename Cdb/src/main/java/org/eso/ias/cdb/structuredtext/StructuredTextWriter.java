db7279faa55cd2d40e6d91ac5a9e3369d90ec516
Alessandro Caproni <acaproni@eso.org>
2022-11-09 10:21:13 +0000

    Implements the writing if IASIOs.
    The writing of a set of IASIOs has been simplified by reading all the IASIOs the adding the new one (without repetitions) and write the entire new set.
    The append does not preserve the original ordering of the IASIOs

--- Cdb/src/main/java/org/eso/ias/cdb/structuredtext/StructuredTextWriter.java
+++ Cdb/src/main/java/org/eso/ias/cdb/structuredtext/StructuredTextWriter.java
@@ -1,19 +1,31 @@
 package org.eso.ias.cdb.structuredtext;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.eso.ias.cdb.CdbWriter;
 import org.eso.ias.cdb.IasCdbException;
-import org.eso.ias.cdb.pojos.IasDao;
+import org.eso.ias.cdb.pojos.*;
 import org.eso.ias.cdb.structuredtext.json.CdbFiles;
+import org.eso.ias.cdb.structuredtext.json.pojos.JsonSupervisorDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Objects;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public abstract class StructuredTextWriter implements CdbWriter {
 
@@ -121,5 +133,189 @@ public abstract class StructuredTextWriter implements CdbWriter {
         } catch (Throwable t) {
             throw new IasCdbException("Error writing JSON IAS",t);
         }
+    }
+
+    /**
+     * Serialize the Supervisor in the JSON file.
+     *
+     * @param superv The Supervisor configuration to write in the file
+     */
+    @Override
+    public void writeSupervisor(SupervisorDao superv) throws IasCdbException  {
+        if (closed.get()) {
+            throw new IasCdbException("The writer is shut down");
+        }
+        if (!initialized.get()) {
+            throw new IasCdbException("The writer is not initialized");
+        }
+
+        Objects.requireNonNull(superv);
+        File f;
+        try {
+            f = cdbFileNames.getSuperivisorFilePath(superv.getId()).toFile();
+        }catch (IOException ioe) {
+            throw new IasCdbException("Error getting Supervisor file",ioe);
+        }
+
+        JsonSupervisorDao jsonSup = new JsonSupervisorDao(superv);
+        ObjectMapper mapper = getObjectMapper();
+        mapper.setDefaultPrettyPrinter(new DefaultPrettyPrinter());
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(f, jsonSup);
+        }catch (Throwable t) {
+            throw new IasCdbException("Error writing JSON Supervisor",t);
+        }
+    }
+
+    /**
+     * Serialize the IASIO in the structured text file
+     * by delegation to {@link #writeIasios(Set, boolean)}
+     *
+     * @param iasio The IASIO configuration to write in the file
+     * @param append: if <code>true</code> the passed iasio is appended to the file
+     *                otherwise a new file is created
+     *
+     * @see CdbWriter#writeIasio(IasioDao, boolean)
+     */
+    @Override
+    public void writeIasio(IasioDao iasio, boolean append) throws IasCdbException {
+        if (closed.get()) {
+            throw new IasCdbException("The writer is shut down");
+        }
+        if (!initialized.get()) {
+            throw new IasCdbException("The writer is not initialized");
+        }
+
+        Set<IasioDao> iasios = new HashSet<>();
+        iasios.add(iasio);
+
+        writeIasios(iasios,append);
+    }
+
+    /**
+     * Serialize the IASIOs in the JSON file.
+     *
+     * <P>If <code>append</code> is <code>false</code> then a new file is created otherwise
+     * the IASIOs in the passed files are written at the end of the file.
+     * <BR>If a IASIO in <code>iasios</code> already exists in the file, the latter
+     * is replaced by that in the set.
+     *
+     * @param iasios The IASIOs to write in the file
+     * @param append: if <code>true</code> the passed iasios are appended to the file
+     *                otherwise a new file is created
+     *
+     * @see CdbWriter#writeIasios(Set, boolean)
+     */
+    @Override
+    public void writeIasios(Set<IasioDao> iasios, boolean append) throws IasCdbException {
+        if (closed.get()) {
+            throw new IasCdbException("The writer is shut down");
+        }
+        if (!initialized.get()) {
+            throw new IasCdbException("The writer is not initialized");
+        }
+        Objects.requireNonNull(iasios);
+
+        File f;
+        try {
+            f = cdbFileNames.getIasioFilePath("UnusedID").toFile();
+        }catch (IOException ioe) {
+            throw new IasCdbException("Error getting IASIOs file",ioe);
+        }
+        ObjectMapper mapper = getObjectMapper();
+        mapper.setDefaultPrettyPrinter(new DefaultPrettyPrinter());
+        if (!f.exists() || !append) {
+            try {
+                mapper.writerWithDefaultPrettyPrinter().writeValue(f, iasios);
+            }catch (Throwable t) {
+                throw new IasCdbException("Error writing JSON IASIOs",t);
+            }
+        } else {
+
+            // Reads the IASIOs from the file
+            IasioDao[] iasioDaos;
+            try {
+                iasioDaos = mapper.readValue(f, IasioDao[].class);
+            } catch (Exception e) {
+                throw new IasCdbException("Error reading IASIOs from" + f.getAbsolutePath(), e);
+            }
+
+            // The IASIOs to write are those read from the file plus those passed
+            // to this method, without repetitions
+            Map<String, IasioDao> iasiosToWrite = new HashMap<>();
+            for (IasioDao iDao: iasioDaos) {
+                iasiosToWrite.put(iDao.getId(), iDao);
+            }
+            // Adds the IASIOs read from the files (the map revoes repetitions)
+            for (IasioDao iDao: iasios) {
+                 iasiosToWrite.put(iDao.getId(), iDao);
+            }
+            try {
+                mapper.writerWithDefaultPrettyPrinter().writeValue(f, iasiosToWrite.values());
+            }catch (Throwable t) {
+                throw new IasCdbException("Error writing JSON IASIOs",t);
+            }
+        }
+    }
+
+    /**
+     * Get the next IasioDao from the passed parser, if it exists
+     *
+     * @param jp The jackson parser
+     * @return The IasioDao read from the parser if found
+     * @throws IOException In case of error getting the next IASIO
+     */
+    private IasioDao getNextIasio(JsonParser jp) throws IOException {
+        String iasioId=null;
+        String iasioDesc=null;
+        String iasioType=null;
+        String iasioUrl=null;
+        String templateId=null;
+        String emails = null;
+        String soundType=null;
+        while(jp.nextToken() != JsonToken.END_OBJECT){
+            String name = jp.getCurrentName();
+            if ("id".equals(name)) {
+                jp.nextToken();
+                iasioId=jp.getText();
+            }
+            if ("shortDesc".equals(name)) {
+                jp.nextToken();
+                iasioDesc=jp.getText();
+            }
+            if ("iasType".equals(name)) {
+                jp.nextToken();
+                iasioType=jp.getText();
+            }
+            if ("docUrl".equals(name)) {
+                jp.nextToken();
+                iasioUrl=jp.getText();
+            }
+            if ("templateId".equals(name)) {
+                jp.nextToken();
+                templateId=jp.getText();
+            }
+            if ("emails".equals(name)) {
+                jp.nextToken();
+                String temp = jp.getText();
+                if (temp!=null) emails=temp;
+            }
+            if ("sound".equals(name)) {
+                jp.nextToken();
+                soundType=jp.getText();
+            }
+        }
+
+        IasioDao ret = new IasioDao(iasioId,iasioDesc, IasTypeDao.valueOf(iasioType),iasioUrl);
+        if (templateId!=null && !templateId.trim().isEmpty()) {
+            ret.setTemplateId(templateId);
+        }
+        if (emails!=null && !emails.trim().isEmpty()) {
+            ret.setEmails(emails);
+        }
+        if (soundType!=null && !soundType.trim().isEmpty()) {
+            ret.setSound(SoundTypeDao.valueOf(soundType));
+        }
+        return ret;
     }
 }
