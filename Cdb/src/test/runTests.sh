1ccc4180f94ce708d0e168919c78ef239627df1b
Alessandro Caproni <acaproni@eso.org>
2022-11-16 14:12:03 +0000

    Fixes the tests

--- Cdb/src/test/runTests.sh
+++ Cdb/src/test/runTests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.json.CdbFoldersTest
+iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.CdbFoldersTest
 iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestTextFileType
 iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestStructTextReader
-iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.json.TestJsonCdb
-iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.json.TestYamlCdb
+iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestJsonCdb
+iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestYamlCdb
 #iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.rdb.TestRdbCdb
 #export IAS_EXTERNAL_JARS="src/test/ExtJARS/" && mkdir -p $IAS_EXTERNAL_JARS && iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.CdbReaderFactoryTest
 #alchemyTest
