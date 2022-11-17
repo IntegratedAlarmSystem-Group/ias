0ea50360b2a3c67060c58cc6cee51e059d25324c
Alessandro Caproni <acaproni@eso.org>
2022-11-17 16:17:41 +0000

    Fixes tests

--- Cdb/src/test/runTests.sh
+++ Cdb/src/test/runTests.sh
@@ -5,5 +5,5 @@ iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestSt
 iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestJsonCdb
 iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.TestYamlCdb
 #iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.rdb.TestRdbCdb
-#export IAS_EXTERNAL_JARS="src/test/ExtJARS/" && mkdir -p $IAS_EXTERNAL_JARS && iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.CdbReaderFactoryTest
+export IAS_EXTERNAL_JARS="src/test/ExtJARS/" && mkdir -p $IAS_EXTERNAL_JARS && iasRun org.junit.platform.console.ConsoleLauncher -c org.eso.ias.cdb.test.CdbReaderFactoryTest
 #alchemyTest
