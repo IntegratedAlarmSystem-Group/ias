e19cf916c30d9a376be07b19f0392587cf2f4d0c
Alessandro Caproni <acaproni@eso.org>
2023-01-03 15:59:24 +0100

    Modifies the python Alarm to match with java/scala

--- BasicTypes/src/main/python/IasBasicTypes/Alarm.py
+++ BasicTypes/src/main/python/IasBasicTypes/Alarm.py
@@ -3,117 +3,150 @@ Created on May 10, 2018
 
 @author: acaproni
 '''
-from enum import Enum
 
-class Alarm(Enum):
+from IasBasicTypes.Priority import Priority
+from IasBasicTypes.AlarmState import AlarmState
+
+class Alarm():
     '''
     The alarm as defined in org.eso.ias.types.Alarm
     '''
-    # Critical alarm set
-    SET_CRITICAL = 4
-    
-    #  HIGH alarm set
-    SET_HIGH = 3
-    
-    # Medium alarm set
-    SET_MEDIUM = 2
-    
-    #  Low priority alarm set
-    SET_LOW = 1
-    
-    #  Alarm clear or unset
-    CLEARED = 0
-    
+
+
+    def __init__(self, alarmState, priority):
+        '''
+        Constructor
+
+        The constructor should  be used only from functions of this class.
+
+        Alarms should be manipulated starting from the alarm representing the
+        initial state of the state machine and calling modifiers.
+
+        The alarm is immutable
+
+        :param alarmState: the state
+        :param priority: the priority
+        '''
+        if alarmState is None or priority is None:
+            raise ValueError("Invalid None priority or alarm state")
+
+        self.alarmState = alarmState
+        self.priority = priority
+
+    def __eq__(self, other):
+        '''
+        Redefine equality
+        :param other: the objecty to compare
+        :return: True if the state and priority of this alarm matches with the one of the other;
+                 False otherwise
+        '''
+        if other is None:
+            return False
+        elif isinstance(other, Alarm):
+            return self.alarmState==other.alarmState and self.priority==other.priority
+        else:
+            return False
+
+    @staticmethod
+    def get_initial_alarmstate(priority=Priority.get_default_priority()):
+        '''
+        :param priority: the priority of the initial alarm
+        :return: the initial alarm state
+        '''
+        return Alarm(AlarmState.CLEAR_ACK, priority)
+
+    def ack(self):
+        '''
+        Acknowledge the alarm
+        :return: a new alarm with the acknowledgment
+        '''
+        newState = self.alarmState.ack()
+        if self.alarmState==newState:
+            return self
+        else:
+            return Alarm(newState, self.priority)
+
+    def set(self):
+        '''
+        Set the alarm
+        :return: a new set alarm
+        '''
+        newState = self.alarmState.set()
+        if self.alarmState==newState:
+            return self
+        else:
+            return Alarm(newState, self.priority)
+
+    def clear(self):
+        '''
+        Clear the alarm
+        :return: a new cleared alarm
+        '''
+        newState = self.alarmState.clear()
+        if self.alarmState==newState:
+            return self
+        else:
+            return Alarm(newState, self.priority)
+
+    def set_if(self, condition):
+        if condition:
+            return self.set()
+        else:
+            return self.clear()
+
+    def is_set(self):
+        '''
+        :return: True if the alarm is SET; False otherwise
+        '''
+        return self.alarmState.is_set()
+
+    def is_acked(self):
+        '''
+        :return: True if the alarm is ACKnowledged; False otherwise
+        '''
+        return self.alarmState.is_acked()
+
+    def increase_priority(self):
+        """
+        :return: an alarm with the increased priority
+        """
+        return Alarm(self.alarmState, self.priority.get_higher_priority())
+
+    def lower_priority(self):
+        """
+        :return: an alarm with lowered priority
+        """
+        return Alarm(self.alarmState, self.priority.get_lower_priority())
+
+    def set_priority(self, priority):
+        '''
+        :param priority: the priority to set
+        :return: the alarm with the new priority
+        '''
+        return Alarm(self.alarmState, priority)
+
+    def to_string(self):
+        """
+        :return: the string representation of the alarm
+        """
+        return f"{self.alarmState.to_string()}:{self.priority.to_string()}"
+
     @staticmethod
     def fromString(alarmString):
         '''
-        @param alarmString: the string representation of an Alarm like
+        :param alarmString the string representation of an Alarm like
                       Alarm.SET_CRITICAL or SET_MEDIUM
-        @return the alarmString represented by the passed a string
+        :return the alarm represented by the passed a string
         '''
         if alarmString is None or alarmString=="":
             raise ValueError("Invalid string representation of an alarmString")
-        
+
         temp = str(alarmString)
-        if "." not in temp:
-            temp="Alarm."+temp
-        for alarmState in Alarm:
-            if str(alarmState)==temp:
-                return alarmState
-        # No enumerated matches with alarmString
-        raise NotImplementedError("Not supported/find alarmString: "+alarmString)
-    
-    @staticmethod
-    def getSetDefault():
-        '''
-        @return the default priority for an alarm set
-        '''
-        return Alarm.SET_MEDIUM
-    
-    @staticmethod
-    def cleared(self):
-        '''
-        A method returning the cleared alarm
-        
-        @return the cleared alarm
-        '''
-        return Alarm.CLEARED
-    
-    @staticmethod
-    def getMaxPriorityAlarm():
-        '''
-        @return the alarm with the highest priority
-        '''
-        return Alarm.SET_CRITICAL
-    
-    @staticmethod
-    def getMinPriorityAlarm():
-        '''
-        @return the alarm with the lowest priority
-        '''
-        return Alarm.SET_LOW
-    
-    def increasePriority(self):
-        '''
-        Return an alarm of a lowered priority if it exists,
-        otherwise return the same alarm.
-      
-        Lowering the priority of a {@link #CLEARED} 
-        alarm is not allowed and the method throws an exception
-      
-        @return the alarm with increased priority
-        '''
-        if (self.value==0):
-            raise ValueError("Cannot increase the priority of an alarm that is not set")
-        elif self.value==1:
-            return Alarm.SET_MEDIUM
-        if (self.value==2):
-            return Alarm.SET_HIGH
-        if (self.value==3 or self.value==4):
-            Alarm.SET_CRITICAL
-    
-    def lowerPriority(self):
-        '''
-        Return an alarm of a lowered priority if it exists,
-        otherwise return the same alarm.
-         
-        Lowering the priority of a CLEARED 
-        alarm is not allowed and the method throws an exception
-         
-        @return the alarm with increased priority
-        '''
-        if (self.value==0):
-            raise ValueError("Cannot lower the priority of an alarm that is not set")
-        elif self.value==1 or self.value==2:
-            return Alarm.SET_LOW
-        if (self.value==3):
-            return Alarm.SET_MEDIUM
-        if (self.value==4):
-            Alarm.SET_HIGH
-    
-    def isSet(self):
-        '''
-        @return True if the alarm is set and False otherwise
-        '''
-        return self.value>0       
-    
+        parts = alarmString.split(":")
+        if len(parts)!=2:
+            raise ValueError(f"Malformed alarm string: [$alarmString]")
+
+        alState = AlarmState.value_of(parts[0])
+        prio = Priority.value_of(parts[1])
+
+        return Alarm(alState, prio)
---
+++ BasicTypes/src/main/python/IasBasicTypes/AlarmState.py
@@ -0,0 +1,82 @@
+'''
+The alarm state and state machine of alarms as defined in org.eso.ias.types.AlarmState
+'''
+
+from enum import Enum
+
+class AlarmState(Enum):
+
+    # Alarm clear and acknowledged
+    CLEAR_ACK = 0
+
+    # Alarm clear and unacknowledged
+    CLEAR_UNACK = 1
+
+    # Alarm set and unacknowledged
+    SET_UNACK = 2
+
+    # Alarm set and acknowledged
+    SET_ACK = 3
+
+    def is_set(self):
+        '''
+        :return True if the alarm is set; False otherwise
+        '''
+        return self==AlarmState.SET_ACK or self==AlarmState.SET_UNACK
+
+    def is_acked(self):
+        '''
+        :return True if the alarm is acknowledged; False otherwise
+        '''
+        return self==AlarmState.SET_ACK or self==AlarmState.CLEAR_ACK
+
+    def ack(self):
+        '''
+        The alarm has been acknowledged (by the operator)
+        :return:
+        '''
+        if self==AlarmState.SET_ACK or self==AlarmState.CLEAR_ACK:
+            return self
+        elif self==AlarmState.SET_UNACK:
+            return AlarmState.SET_ACK
+        else: # CLEAR_UNACK
+            return AlarmState.CLEAR_ACK
+
+    def set(self):
+        '''
+        The alarm has been set (by the TF)
+        :return:
+        '''
+        if self==AlarmState.SET_ACK or self==AlarmState.SET_UNACK:
+            return self
+        else:
+            return AlarmState.SET_UNACK
+
+    def clear(self):
+        if self==AlarmState.CLEAR_ACK or self==AlarmState.CLEAR_UNACK:
+            return self
+        elif self==AlarmState.SET_ACK:
+            return AlarmState.CLEAR_ACK
+        else: #SET_UNACK
+            return AlarmState.CLEAR_UNACK
+
+    @staticmethod
+    def value_of(name):
+        '''
+        Get and return the AlarmState from the passed name.
+
+        It delegates to Enum.__getitem__(cls, name)
+        :param cls:  the class
+        :param name: the name of the alarm state
+        :return: the state with the given name
+        :raise KeyError if the name does not correspond to a priority
+        '''
+        return AlarmState.__getitem__(name)
+
+    def to_string(self):
+        '''
+
+        :param cls: the class
+        :return: the string representation of the alarm state
+        '''
+        return self.name
