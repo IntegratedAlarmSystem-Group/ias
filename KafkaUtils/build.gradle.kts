7811264fd56c89017aa04cbb75a850773ab99a0b
Alessandro Caproni <acaproni@eso.org>
2023-02-01 17:36:42 +0100

    Begins the implemenattion of one consumer for more topics - Issue#195

--- KafkaUtils/build.gradle.kts
+++ KafkaUtils/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    `java`
+    scala
+    java
     `java-library-distribution`
     id("org.eso.ias.build.plugin")
 }
@@ -8,11 +9,14 @@ dependencies {
     val g = project.gradle
     if (g is ExtensionAware) {
       val extension = g as ExtensionAware
-      implementation(extension.extra["slf4j-api"].toString())
-      implementation(extension.extra["junit-jupiter-api"].toString())
-      implementation(extension.extra["junit-jupiter-engine"].toString())
-      implementation(extension.extra["kafka-clients"].toString())
-      implementation(extension.extra["logback-classic"].toString())
+        implementation(extension.extra["scala-library"].toString())
+        implementation(extension.extra["scalatest"].toString())
+        implementation(extension.extra["scala-logging"].toString())
+        implementation(extension.extra["slf4j-api"].toString())
+        implementation(extension.extra["junit-jupiter-api"].toString())
+        implementation(extension.extra["junit-jupiter-engine"].toString())
+        implementation(extension.extra["kafka-clients"].toString())
+        implementation(extension.extra["logback-classic"].toString())
     }
 
     implementation(project(":BasicTypes"))
---
+++ KafkaUtils/src/main/scala/org/eso/ias/kafkaneo/IasTopic.scala
@@ -0,0 +1,23 @@
+package org.eso.ias.kafkaneo
+
+/** The topics for the IAS */
+enum IasTopic(val kafkaTopicName: String) extends Enum[IasTopic] {
+
+  /** The topic where plugins pushes values */
+  case Plugins extends IasTopic("PluginsKTopic")
+
+  /** The topic where converters and DASUs push the IASIOs */
+  case Core extends IasTopic("BsdbCoreKTopic")
+
+  /** The topic where tools publish the heartbeat */
+  case Heartbeat extends IasTopic("HeartbeatTopic")
+
+  /** The topic for commands */
+  case Command extends IasTopic("CmdTopic")
+
+  /** The topic for replies to commands */
+  case Reply extends IasTopic("ReplyTopic")
+
+  /** @return the name of the Kafka topic */
+  def kafkaTopicName(topic: IasTopic): String = topic.toString
+}
