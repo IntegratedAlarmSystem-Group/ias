22b09b624e6800dad5d24751f07bda5c1e2f77ed
Alessandro Caproni <acaproni@eso.org>
2023-09-21 16:58:45 +0200

    Pgrades IAS tools

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -67,19 +67,19 @@ if (gradle is ExtensionAware) {
 	extension.extra["JdkVersion"] = 11
 
         // IAS common dependencies
-	extension.extra["scala-library"] = "org.scala-lang:scala3-library_3:3.2.2"
-	extension.extra["scalatest"] = "org.scalatest:scalatest_3:3.2.15"
+	extension.extra["scala-library"] = "org.scala-lang:scala3-library_3:3.3.1"
+	extension.extra["scalatest"] = "org.scalatest:scalatest_3:3.2.17"
 	extension.extra["slf4j-api"] = "org.slf4j:slf4j-api:1.7.36"
-	extension.extra["scala-logging"] = "com.typesafe.scala-logging:scala-logging_3:3.9.4"
+	extension.extra["scala-logging"] = "com.typesafe.scala-logging:scala-logging_3:3.9.5"
 	extension.extra["logback-classic"] = "ch.qos.logback:logback-classic:1.2.11"
 	extension.extra["jackson-databind"] = "com.fasterxml.jackson.core:jackson-databind:2.13.4.2"
 	extension.extra["junit-jupiter-api"] = "org.junit.jupiter:junit-jupiter-api:5.9.1"
 	extension.extra["junit-jupiter-engine"] = "org.junit.jupiter:junit-jupiter-engine:5.9.1"
 	extension.extra["junit-platform-console-standalone"] = "org.junit.platform:junit-platform-console-standalone:1.9.1"
 	extension.extra["commons-cli"] = "commons-cli:commons-cli:1.5.0"
-	extension.extra["kafka-clients"] = "org.apache.kafka:kafka-clients:3.3.1"
-	extension.extra["kafka-streams"] = "org.apache.kafka:kafka-streams:3.3.1"
-	extension.extra["kafka-connect-api"] = "org.apache.kafka:connect-api:3.3.1"
+	extension.extra["kafka-clients"] = "org.apache.kafka:kafka-clients:3.5.1"
+	extension.extra["kafka-streams"] = "org.apache.kafka:kafka-streams:3.5.1"
+	extension.extra["kafka-connect-api"] = "org.apache.kafka:connect-api:3.5.1"
 	extension.extra["hibernate-jpa"] = "org.hibernate.javax.persistence:hibernate-jpa-2.1-api:1.0.0.Final"
 }
 
