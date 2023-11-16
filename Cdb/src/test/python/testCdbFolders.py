d6ccf6adba8c725c439d7d59c7ae2acb8a55e364
Alessandro Caproni <acaproni@eso.org>
2023-11-16 14:43:21 +0100

    Adds tests (and fixes) for CdbTxtFiles and CdbFolders

--- Cdb/src/test/python/testCdbFolders.py
+++ Cdb/src/test/python/testCdbFolders.py
@@ -1,15 +1,68 @@
+#! /usr/bin/env python3
 """
 Test the CdbFolders.py
 """
 import unittest
+import os
+import shutil
 
-from CdbFolders import CdbFolders
+from IasCdb.CdbFolders import CdbFolders, Folders
 
 class TestCdbFolders(unittest.TestCase):
+    """
+    Test CdbFolder.
+
+    It uses a temporary folder (self.temp_dir) that is removed at end.
+    """
     def setUp(self):
-        pass
+        self.temp_dir = "src/test/PyTestCDB"
+        try:
+            os.makedirs(os.path.join(self.temp_dir,"CDB"))
+        except FileExistsError:
+            # The folder should not exist: a leftover from a previous test?
+            print(f"Warning: {os.path.join(self.temp_dir,'CDB')} should not exist")
+        self.assertTrue(os.path.join(self.temp_dir,"CDB"))
 
     def tearDown(self):
-        pass
+        try:
+            shutil.rmtree(self.temp_dir)
+        except Exception as e: 
+            print(f"Error deleting {os.path.join(self.temp_dir,'CDB')}:",e)
+        pass 
 
-    def test
+    def testStaticGetFolderNoCreation(self):
+        folder = CdbFolders(Folders.SUPERVISOR, self.temp_dir).get_folder(False)
+        self.assertEqual("src/test/PyTestCDB/CDB/Supervisor", folder)
+        # Ensure the folder has not been created
+        self.assertFalse(os.path.isdir(folder))
+
+    def testStaticGetAndCreate(self):
+        folder = CdbFolders(Folders.SUPERVISOR, self.temp_dir).get_folder(True)
+        self.assertEqual("src/test/PyTestCDB/CDB/Supervisor", folder)
+        # Ensure the folder has not been created
+        self.assertTrue(os.path.exists(folder))
+
+    def testCdbFolderObject(self):
+        """
+        Test the methods of an object by calling its method
+        """
+        cdb_folder =  CdbFolders(Folders.DASU, self.temp_dir)
+        self.assertFalse(os.path.isdir(cdb_folder.get_folder(False)))
+        self.assertFalse(cdb_folder.exists())
+        self.assertTrue(os.path.isdir(cdb_folder.get_folder(True)))
+        self.assertTrue(cdb_folder.exists())
+        cdb_folder.delete()
+        self.assertFalse(cdb_folder.exists())
+    
+    def testStaticCreateFolders(self):
+        """
+        Test the creation of the main structure of the CDB
+        i.e. all the subfolders
+        """
+        CdbFolders.create_folders(self.temp_dir)
+        for folder in Folders:
+            folder = CdbFolders(folder, self.temp_dir)
+            self.assertTrue(folder.exists())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Cdb/src/test/python/testCdbTxtFiles.py
@@ -0,0 +1,102 @@
+#! /usr/bin/env python3
+"""
+Test the CdbTxtFiles.py
+"""
+import unittest
+import pathlib
+
+from IasCdb.CdbTxtFiles import CdbTxtFiles
+from IasCdb.TextFileType import FileType
+
+class TestCdbFiles(unittest.TestCase):
+
+    def setUp(self):
+        self.json_cdb = "src/test/testJsonCdb/"
+        self.yaml_cdb = "src/test/testYamlCdb"
+
+    def tearDown(self):
+        pass
+
+    def testGetJsonIas(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_ias_file_path()
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlIas(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_ias_file_path()
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetJsonSuperv(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_supervisor_file_path("Supervisor-ID1")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlSuperv(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_supervisor_file_path("Supervisor-ID1")
+        self.assertTrue(pathlib.Path(file_path))
+
+
+    def testGetJsonDasu(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_supervisor_file_path("DasuID2")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlDasu(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_supervisor_file_path("DasuID2")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetJsonAsce(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_supervisor_file_path("ASCE-WITH-TEMPLATED-INPUTS")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlAsce(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_supervisor_file_path("ASCE-WITH-TEMPLATED-INPUTS")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetJsonIasio(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_iasio_file_path()
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlIasio(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_iasio_file_path()
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetJsonTf(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_tf_file_path()
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlTf(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_tf_file_path()
+        self.assertTrue(pathlib.Path(file_path))
+    
+    def testGetJsonClient(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_client_file_path("test")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlClient(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_client_file_path("test")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetJsonPlugin(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.JSON)
+        file_path=reader.get_plugin_file_path("PluginID2")
+        self.assertTrue(pathlib.Path(file_path))
+
+    def testGetYamlPlugin(self):
+        reader = CdbTxtFiles(self.json_cdb, FileType.YAML)
+        file_path=reader.get_plugin_file_path("PluginID2")
+        self.assertTrue(pathlib.Path(file_path))
+
+if __name__ == '__main__':
+    unittest.main()
