7b444529833c126bd3c88e23d782de91f5519108
Alessandro Caproni <acaproni@eso.org>
2023-11-21 13:50:15 +0100

    Intial commit of the alarm GUI

---
+++ iasAlarmGui/iasAlarmGui.py
@@ -0,0 +1,76 @@
+# This Python file uses the following encoding: utf-8
+import sys, os, logging
+
+from PySide6.QtCore import Slot, QCommandLineOption, QCommandLineParser
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_AlarmGui
+from connect_to_ias_dlg import ConnectToIasDlg
+
+class MainWindow(QMainWindow, Ui_AlarmGui):
+    def __init__(self, ias_cdb, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_AlarmGui()
+        self.ui.setupUi(self)
+        self.ias_cdb=ias_cdb
+        # the dialog to connect to the IAS
+        self.connectDlg = None
+        print("Started with CDB",self.ias_cdb)
+
+    @Slot()
+    def on_action_Connect_triggered(self):
+        self.connectDlg = ConnectToIasDlg(self.ias_cdb, self)
+        self.connectDlg.open()
+        self.connectDlg.finished.connect(self.on_ConnectDialog_finished)
+
+    # @Slot()
+    def on_ConnectDialog_finished(self):
+        print("on_ConnectDialog_finished")
+        dlg_ret_code = self.connectDlg.result()
+        if dlg_ret_code==1:
+            # The user pressed the Ok button ==> Connect!
+            print("URL",self.connectDlg.getBrokers())
+        self.connectDlg = None
+
+def parse(app):
+    """
+    Parse the command line arguments
+    """
+    parser = QCommandLineParser()
+    parser.addHelpOption()
+    parser.addVersionOption()
+
+    cdb_option = QCommandLineOption(
+            ["c", "jcdb"],
+            "The parent folder of the IAS CDB",
+            "ias_cdb"
+        )
+    parser.addOption(cdb_option)
+    parser.process(app)
+
+    if parser.isSet(cdb_option):
+        return parser.value(cdb_option)
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    logging.debug("IAS Alarm GUI started")
+    try:
+        os.environ["IAS_ROOT"]
+    except:
+        logging.error("Environment not set: IAS_ROOT undefined!")
+        sys.exit(1)
+    app = QApplication(sys.argv)
+    app.setApplicationName("IasAlarmGui")
+    app.setApplicationVersion("1.0")
+    cdb_parent_folder=parse(app)
+    logging.info("IAS CDB parent path is %s",cdb_parent_folder)
+    widget = MainWindow(ias_cdb=cdb_parent_folder)
+    widget.show()
+    sys.exit(app.exec())
---
+++ iasAlarmGui/ui_connect_dialog.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'connect_dialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QHBoxLayout, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QVBoxLayout, QWidget)
+
+class Ui_ConnectToIas(object):
+    def setupUi(self, ConnectToIas):
+        if not ConnectToIas.objectName():
+            ConnectToIas.setObjectName(u"ConnectToIas")
+        ConnectToIas.resize(320, 189)
+        self.buttonBox = QDialogButtonBox(ConnectToIas)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setGeometry(QRect(10, 150, 301, 32))
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.verticalLayoutWidget = QWidget(ConnectToIas)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(10, 30, 303, 121))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.fetchFromCdbBtn = QPushButton(ConnectToIas)
+        self.fetchFromCdbBtn.setObjectName(u"fetchFromCdbBtn")
+        self.fetchFromCdbBtn.setGeometry(QRect(10, 10, 301, 25))
+        self.layoutWidget = QWidget(ConnectToIas)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 40, 301, 111))
+        self.horizontalLayout = QHBoxLayout(self.layoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.layoutWidget)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.lineEdit = QLineEdit(self.layoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setText(u"")
+        self.lineEdit.setDragEnabled(False)
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+
+        self.retranslateUi(ConnectToIas)
+        self.buttonBox.rejected.connect(ConnectToIas.reject)
+        self.fetchFromCdbBtn.clicked.connect(ConnectToIas.onFetchBtnClicked)
+        self.lineEdit.textChanged.connect(ConnectToIas.onLineEditTxtChanged)
+        self.buttonBox.accepted.connect(ConnectToIas.onOkBtnClicked)
+
+        QMetaObject.connectSlotsByName(ConnectToIas)
+    # setupUi
+
+    def retranslateUi(self, ConnectToIas):
+        ConnectToIas.setWindowTitle(QCoreApplication.translate("ConnectToIas", u"Connect to IAS...  ", None))
+#if QT_CONFIG(tooltip)
+        self.fetchFromCdbBtn.setToolTip(QCoreApplication.translate("ConnectToIas", u"Select the path of the CDB", None))
+#endif // QT_CONFIG(tooltip)
+        self.fetchFromCdbBtn.setText(QCoreApplication.translate("ConnectToIas", u"Fetch from CDB", None))
+        self.label.setText(QCoreApplication.translate("ConnectToIas", u"Kafka brokers:", None))
+#if QT_CONFIG(tooltip)
+        self.lineEdit.setToolTip(QCoreApplication.translate("ConnectToIas", u"list of comma separated server:port to connect to IAS", None))
+#endif // QT_CONFIG(tooltip)
+    # retranslateUi
+
