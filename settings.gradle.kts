a79afbe9caf1135916228925f5fb39971d898bd5
Alessandro Caproni <acaproni@eso.org>
2023-11-24 08:02:06 +0100

    Upgrades logback classic that was not compatible with slf4j

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -71,7 +71,7 @@ if (gradle is ExtensionAware) {
 	extension.extra["scalatest"] = "org.scalatest:scalatest_3:3.2.17"
 	extension.extra["slf4j-api"] = "org.slf4j:slf4j-api:2.0.9"
 	extension.extra["scala-logging"] = "com.typesafe.scala-logging:scala-logging_3:3.9.5"
-	extension.extra["logback-classic"] = "ch.qos.logback:logback-classic:1.2.11"
+	extension.extra["logback-classic"] = "ch.qos.logback:logback-classic:1.4.11"
 	extension.extra["jackson-databind"] = "com.fasterxml.jackson.core:jackson-databind:2.13.4.2"
 	extension.extra["junit-jupiter-api"] = "org.junit.jupiter:junit-jupiter-api:5.10.0"
 	extension.extra["junit-jupiter-engine"] = "org.junit.jupiter:junit-jupiter-engine:5.10.0"
