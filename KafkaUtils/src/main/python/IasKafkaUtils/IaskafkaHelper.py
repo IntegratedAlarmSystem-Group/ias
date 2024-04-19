92ca292e6e4623fc05a1874dfc416d9228260b36
acaproni <acaproni@eso.org>
2024-04-19 15:53:01 +0200

    Removes check_kafka_cmd because a local installation of kafka is not required anymore

--- KafkaUtils/src/main/python/IasKafkaUtils/IaskafkaHelper.py
+++ KafkaUtils/src/main/python/IasKafkaUtils/IaskafkaHelper.py
@@ -13,21 +13,3 @@ class IasKafkaHelper():
         'plugin': "PluginsKTopic",
         'cmd': "CmdTopic",
         'reply': "ReplyTopic"}
-
-    @classmethod
-    def check_kafka_cmd(cls, kafkaCommand):
-        '''
-        Check if kafka commands exists
-        :rtype: bool
-        :param kafkaCommand: kafkaCommand the command to run from KAFKA_HOME
-        :return: True if kafka is available
-        '''
-        try:
-            kafkaHome = os.environ["KAFKA_HOME"]
-        except:
-            print("KAFKA_HOME environment variable not defined")
-            return False
-
-        if not os.access(kafkaHome+os.sep+kafkaCommand, os.X_OK):
-            return False
-        return True
