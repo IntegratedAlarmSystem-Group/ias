33ac2c727d26216da034c80d668eb1ce798710e2
acaproni <acaproni@eso.org>
2024-04-26 12:02:10 +0200

    Adds helper functions to manage the topics

--- KafkaUtils/src/main/python/IasKafkaUtils/IaskafkaHelper.py
+++ KafkaUtils/src/main/python/IasKafkaUtils/IaskafkaHelper.py
@@ -4,6 +4,8 @@ A collection of methods to help dealing with kafka
 
 import os
 
+from confluent_kafka.admin import (AdminClient, NewTopic)
+
 class IasKafkaHelper():
 
     # Associates human readable topic names with their names in kafka
@@ -13,3 +15,90 @@ class IasKafkaHelper():
         'plugin': "PluginsKTopic",
         'cmd': "CmdTopic",
         'reply': "ReplyTopic"}
+    
+    @classmethod
+    def topicExists(cks, topicName: str, kafkaBrokers: str) -> bool:
+        """
+        Check if a topic with the given name exists
+
+        Args:
+            topicName: the neame of the topic to check
+            kafkaBrokers: the kafka brokers
+        Return:
+          True if the topic exists; False otherwise
+        """
+        admin = AdminClient({'bootstrap.servers': kafkaBrokers})
+        metadata = admin.list_topics()
+        for t in iter(metadata.topics.values()):
+            if t.topic == topicName:
+                return True
+        return False
+
+    @classmethod
+    def createTopic(cls, topicName: str, kafkaBrokers: str):
+        """
+        Create a topic
+
+        It delegates to the AdminClient for creating the topic with the given name
+
+        Args:
+            topicName: the neame of the topic to create
+            kafkaBrokers: the kafka brokers
+        Return:
+          True if the topic was created or already exists; False otherwise
+        """
+        if IasKafkaHelper.topicExists(topicName, kafkaBrokers):
+            return True
+        admin = AdminClient({'bootstrap.servers': kafkaBrokers})
+        #new_topic = NewTopic(topic, num_partitions=6, replication_factor=3) 
+        new_topic = NewTopic(topicName) 
+        result_dict = admin.create_topics([new_topic])
+        for topic, future in result_dict.items():
+            try:
+                future.result()  # The result itself is None
+                print("Topic {} created".format(topic))
+                return True
+            except Exception as e:
+                print("Failed to create topic {}: {}".format(topic, e))
+                return False
+    
+    @classmethod
+    def deleteTopic(cls, topicName: str, kafkaBrokers: str):
+        """
+        Create a topic
+
+        It delegates to the AdminClient for creating the topic with the given name
+
+        Args:
+            topicName: the neame of the topic to create
+            kafkaBrokers: the kafka brokers
+        Return:
+          True if the topic was delete or does not exists; False otherwise
+        """
+        if not IasKafkaHelper.topicExists(topicName, kafkaBrokers):
+            return True
+        admin = AdminClient({'bootstrap.servers': kafkaBrokers})
+        fs = admin.delete_topics([topicName], operation_timeout=30)
+
+        # Wait for operation to finish.
+        for topic, f in fs.items():
+            try:
+                f.result()  # The result itself is None
+                print("Topic {} deleted".format(topic))
+                return True
+            except Exception as e:
+                print("Failed to delete topic {}: {}".format(topic, e))
+                return False
+            
+    @classmethod
+    def getTopicNames(cls,kafkaBrokers: str) -> list[str]:
+        """
+        Returns:
+          The list of the topic names
+        """
+        admin = AdminClient({'bootstrap.servers': kafkaBrokers})
+        metadata = admin.list_topics()
+        ret = []
+        for t in iter(metadata.topics.values()):
+            ret.append(t.topic)
+        return ret
