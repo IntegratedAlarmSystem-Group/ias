37d879ecfb17324a44650ce9d83c201696497139
Alessandro Caproni <acaproni@eso.org>
2024-05-08 07:30:56 +0200

    Adds the shebang

--- CommandsAndReplies/src/test/runIntTests.sh
+++ CommandsAndReplies/src/test/runIntTests.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/bash
 iasRun -r org.junit.platform.console.ConsoleLauncher -c org.eso.ias.command.test.TestCommandManager
 iasRun -r org.junit.platform.console.ConsoleLauncher -c org.eso.ias.command.test.TestCommandSender
