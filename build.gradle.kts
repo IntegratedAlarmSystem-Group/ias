e07f131ab769122b4e5e13aa879a5af044f6d28b
Alessandro Caproni <acaproni@eso.org>
2025-04-29 16:59:20 +0200

    #247 encloses the actions of the install task in a doLast block

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,18 +18,20 @@ tasks.register("build") {
 }
 
 tasks.register("install") {
-    var envVar: String? = System.getenv("IAS_ROOT")
-    val extension = gradle as ExtensionAware
-    val gitBranchStr = extension.extra["GitBranch"].toString()
-    org.jetbrains.kotlin.konan.file.File(envVar + "/" + "RELEASE.txt").writeText("Built from git branch " + gitBranchStr)
+    doLast {
+        var envVar: String? = System.getenv("IAS_ROOT")
+        val extension = gradle as ExtensionAware
+        val gitBranchStr = extension.extra["GitBranch"].toString()
+        org.jetbrains.kotlin.konan.file.File(envVar + "/" + "RELEASE.txt").writeText("Built from git branch " + gitBranchStr)
 
-    val licFile = File("LICENSE.md")
-    val outLicFile = File(envVar + "/" + "LICENSE.md")
-    java.nio.file.Files.copy(licFile.toPath(), outLicFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+        val licFile = File("LICENSE.md")
+        val outLicFile = File(envVar + "/" + "LICENSE.md")
+        java.nio.file.Files.copy(licFile.toPath(), outLicFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
 
-    val readmeFile = File("README.md")
-    val outReadmeFile = File(envVar + "/" + "README.md")
-    java.nio.file.Files.copy(readmeFile.toPath(), outReadmeFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+        val readmeFile = File("README.md")
+        val outReadmeFile = File(envVar + "/" + "README.md")
+        java.nio.file.Files.copy(readmeFile.toPath(), outReadmeFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+    }
 }
 
 subprojects {
