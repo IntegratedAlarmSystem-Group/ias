568c914cb5c3134129291c2919c75cca95a6485b
Alessandro Caproni <acaproni@eso.org>
2025-05-26 10:48:43 +0200

    #236 Gradle runs junit tests

--- Cdb/build.gradle.kts
+++ Cdb/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("com.mchange:c3p0:0.9.5.2")
     implementation("org.hibernate:hibernate-c3p0:5.2.6.Final")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.0")
+    
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 sourceSets {
@@ -30,10 +32,17 @@ sourceSets {
     }
 }
 
+
 distributions {
     main {
         contents {
             from("src/main/resources")
         }
     }
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+    exclude("**/TestRdbCdb.class")
 }
