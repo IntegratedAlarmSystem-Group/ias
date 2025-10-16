62a91b250017591a0e4c53d433d0a1f996bff96a
Alessandro Caproni <acaproni@eso.org>
2025-10-16 12:20:02 +0000

    Issue #262 CdbFolders returns the path of the folders
    It does not create any folder if it does not exist
    that would trigger a failure if the CDB is not writable.
    There is nothing to test so the test file has been deleted.

--- Cdb/src/main/java/org/eso/ias/cdb/structuredtext/CdbFolders.java
+++ Cdb/src/main/java/org/eso/ias/cdb/structuredtext/CdbFolders.java
@@ -3,7 +3,6 @@ package org.eso.ias.cdb.structuredtext;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Objects;
 
 /**
  * <P>The structure of the folders and the names of the files 
@@ -37,9 +36,9 @@ import java.util.Objects;
  *     | ...
  *     | <ClientId-x>.conf
  * }
- * </pre>
+ * <code>CdbFolders</code> returns the path and the names of the folders of the IAS CDB.
  * 
- * Note that <code>CdbFolders</code> only deals with the folders of the CDB 
+ * <code>CdbFolders</code> only deals with the folders of the CDB 
  * but not with the name of the files that ultimately depends on the selected format.
  * The example shown above is for JSON files.
  * 
@@ -73,48 +72,6 @@ public enum CdbFolders {
 	}
 	
 	/**
-	 * Delete the subfolder, if it exists.
-	 * 
-	 * @param folderToDelete: The subfolder to delete
-	 * @return <code>true</code> if the folder has been deleted,
-	 * 	       <code>false</code> otherwise
-	 */
-	public boolean delete(Path folderToDelete) {
-		if (folderToDelete==null) {
-			throw new NullPointerException("The parent path of the CDB can't be null");
-		}
-		Path folderPath = buildPath(folderToDelete);
-		if (!exists(folderToDelete)) {
-			// The folder does not exist ==> nothing to do
-			return false;
-		}
-		
-		deleteFolder(folderPath.toFile());
-		return true;
-	}
-	
-	/**
-	 * Recursively delete a folder from the file system.
-	 * 
-	 * @param folderToDelete
-	 */
-	private void deleteFolder(File folderToDelete) {
-		Objects.requireNonNull(folderToDelete, "Cannot delete a NULL folder");
-		
-		File[] files = folderToDelete.listFiles();
-	    if(files!=null) { //some JVMs return null for empty dirs
-	        for(File f: files) {
-	            if(f.isDirectory()) {
-	                deleteFolder(f);
-	            } else {
-	                f.delete();
-	            }
-	        }
-	    }
-	    folderToDelete.delete();
-	}
-	
-	/**
 	 * Build the path of the subfolder
 	 * 
 	 * @param cdbParentPath: The path to the parent of the CDB
@@ -134,30 +91,17 @@ public enum CdbFolders {
 	}
 	
 	/**
-	 * Get the path of the CDB folder and if it is the case, creates it.
+	 * Get the path of the CDB folder.
 	 * 
 	 * @param cdbParentPath: The path to the parent of the CDB
-	 * @param create: if <code>true</code> and the subfolder does not exist, then create it
 	 * @return the path to the subfolder.
 	 * @throws IOException If the folder is not writeable
 	 */
-	public Path getFolder(Path cdbParentPath, boolean create) throws IOException {
+	public Path getFolder(Path cdbParentPath) throws IOException {
 		if (cdbParentPath==null) {
 			throw new NullPointerException("The parent path of the CDB can't be null");
 		}
-		Path folderPath=buildPath(cdbParentPath);
-		File f = folderPath.toFile();
-		if (exists(cdbParentPath)) {
-			if (f.isDirectory() && f.canWrite()) {
-				// No need to create the folder
-				return folderPath;
-			} else {
-				throw new IOException(f.toString()+" exists but unusable: check permissions and type");
-			}
-		} else {
-			f.mkdirs();
-			return folderPath;
-		}
+		return buildPath(cdbParentPath);
 	}
 	
 	/**
@@ -178,28 +122,13 @@ public enum CdbFolders {
 	 * 
 	 * @param cdbParentPath: The path to the parent of the CDB
 	 * @param folder: the CDB folder to create 
-	 * @param create: if <code>true</code> and the subfolder does not exist, then create it
 	 * @return the path to the subfolder.
 	 * @throws IOException In case of error getting the path
 	 */
-	public static Path getSubfolder(Path cdbParentPath, CdbFolders folder, boolean create) throws IOException {
+	public static Path getSubfolder(Path cdbParentPath, CdbFolders folder) throws IOException {
 		if (folder==null) {
 			throw new NullPointerException("CDB subfolder can't be null");
 		}
-		return folder.getFolder(cdbParentPath, create);
-	}
-	
-	/**
-	 * Create all the subfolders of the CDB.
-	 * 
-	 * If a subfolder already exists then nothing is done
-	 * 
-	 * @param cdbParentPath: The path to the parent of the CDB
-	 * @throws IOException in case of error creating the folders
-	 */
-	public static void createFolders(Path cdbParentPath) throws IOException {
-		for (CdbFolders folder: CdbFolders.values()) {
-			folder.getFolder(cdbParentPath, true);
-		}
+		return folder.getFolder(cdbParentPath);
 	}
 }
