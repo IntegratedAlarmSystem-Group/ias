1f890a9a183c8fb36d4508dd5205c0d0af5442ed
Alessandro Caproni <acaproni@eso.org>
2025-10-31 17:20:43 +0100

    New syntax to run tests with junit6

--- KafkaUtils/src/test/runIntTests.sh
+++ KafkaUtils/src/test/runIntTests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-iasRun -r org.junit.platform.console.ConsoleLauncher -c org.eso.ias.kafkautils.test.ConsumerProducerTest
-iasRun -r org.junit.platform.console.ConsoleLauncher -c org.eso.ias.kafkautils.test.KafkaIasiosConsumerTest
-iasRun -r org.junit.platform.console.ConsoleLauncher -c org.eso.ias.kafkautils.test.SlowIasiosProcessorTest
+iasRun -r org.junit.platform.console.ConsoleLauncher execute --select-class org.eso.ias.kafkautils.test.ConsumerProducerTest
+iasRun -r org.junit.platform.console.ConsoleLauncher execute --select-class org.eso.ias.kafkautils.test.KafkaIasiosConsumerTest
+iasRun -r org.junit.platform.console.ConsoleLauncher execute --select-class org.eso.ias.kafkautils.test.SlowIasiosProcessorTest
 iasRun -r org.scalatest.run org.eso.ias.kafkaneo.test.consumer.ListenerTest
 iasRun -r org.scalatest.run org.eso.ias.kafkaneo.test.consumer.ConsumerHelperTest
 iasRun -r org.scalatest.run org.eso.ias.kafkaneo.test.consumer.ConsumerTest
