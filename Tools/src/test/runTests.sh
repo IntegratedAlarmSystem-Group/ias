76aad8604e381d39810e3b04be7a6451dd262e9b
Alessandro Caproni <acaproni@eso.org>
2025-11-28 08:41:07 +0100

    Renames python unittest files before migrationg to pytest

--- Tools/src/test/runTests.sh
+++ Tools/src/test/runTests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-testCreateModule
-export IAS_EXTERNAL_JARS="$PWD/externalJars1:$PWD/externalJars2" && testCommonDefs
+test_create_module
+export IAS_EXTERNAL_JARS="$PWD/externalJars1:$PWD/externalJars2" && test_common_defs
 iasRun -r org.scalatest.run org.eso.ias.utils.test.ISO8601Test
 iasRun -r org.scalatest.run org.eso.ias.utils.test.H2NVCacheTest
 iasRun -r org.scalatest.run org.eso.ias.utils.test.InMemoryCacheTest
