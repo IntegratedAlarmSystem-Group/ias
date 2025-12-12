bd3a7838c10a6f08121bee16a5825a62197b5ae7
Alessandro Caproni <acaproni@eso.org>
2025-12-12 12:35:55 +0100

    Ports the tests to pytest

--- Cdb/src/test/python/test_cdb_reader.py
+++ Cdb/src/test/python/test_cdb_reader.py
@@ -3,52 +3,48 @@
 Test the CdbReader.py
 """
 
-import unittest
 from IasCdb.Dao.IasDao import IasDao
 from IasCdb.Dao.LogLevelDao import LogLevelDao
 from IasCdb.CdbReader import CdbReader
 from IasCdb.TextFileType import FileType, TextFileType
 
-class TestCdbReader(unittest.TestCase):
+class TestCdbReader():
 
-    def setUp(self):
+    def setup_method(self):
         self.json_cdb = "src/test/testJsonCdb/"
         self.yaml_cdb = "src/test/testYamlCdb"
 
-    def tearDown(self):
+    def teardown_method(self):
         pass
 
     def testFileType(self):
-        self.assertEqual(CdbReader(self.json_cdb).cdbTxtFiles.files_type, FileType.JSON)
-        self.assertEqual(CdbReader(self.yaml_cdb).cdbTxtFiles.files_type, FileType.YAML)
+        assert CdbReader(self.json_cdb).cdbTxtFiles.files_type == FileType.JSON
+        assert CdbReader(self.yaml_cdb).cdbTxtFiles.files_type == FileType.YAML
 
-    def testGetIasJson(self):
+    def test_get_ias_json(self):
         reader = CdbReader(self.json_cdb)
         ias = reader.get_ias()
-        self.assertIsNotNone(ias)
-        self.assertEqual(ias.log_level, LogLevelDao.INFO)
-        self.assertEqual(ias.refresh_rate,5)
-        self.assertEqual(ias.validity_threshold,11)
-        self.assertEqual(ias.hb_frequency, 10)
-        self.assertEqual(ias.smtp, 'acaproni:pswd@smtp.test.org')
-        self.assertEqual(ias.bsdb_url, '127.0.0.1:9092')
+        assert ias is not None
+        assert ias.log_level == LogLevelDao.INFO
+        assert ias.refresh_rate == 5
+        assert ias.validity_threshold == 11
+        assert ias.hb_frequency == 10
+        assert ias.smtp == 'acaproni:pswd@smtp.test.org'
+        assert ias.bsdb_url == '127.0.0.1:9092'
 
         props = { 'PropName1':'PropValue1', 'PropName2':'PropValue2'}
-        self.assertEqual(ias.props, props)
+        assert ias.props == props
 
-    def testGetIasYaml(self):
+    def test_getIas_aml(self):
         reader = CdbReader(self.yaml_cdb)
         ias = reader.get_ias()
-        self.assertIsNotNone(ias)
-        self.assertEqual(ias.log_level, LogLevelDao.INFO)
-        self.assertEqual(ias.refresh_rate,5)
-        self.assertEqual(ias.validity_threshold,11)
-        self.assertEqual(ias.hb_frequency, 10)
-        self.assertEqual(ias.smtp, 'acaproni:pswd@smtp.test.org')
-        self.assertEqual(ias.bsdb_url, '127.0.0.1:9092')
+        assert ias is not None
+        assert ias.log_level == LogLevelDao.INFO
+        assert ias.refresh_rate == 5
+        assert ias.validity_threshold == 11
+        assert ias.hb_frequency == 10
+        assert ias.smtp == 'acaproni:pswd@smtp.test.org'
+        assert ias.bsdb_url == '127.0.0.1:9092'
 
         props = { 'P1-Name':'A value for property 1', 'P2-Name':'A value for another property'}
-        self.assertEqual(ias.props, props)
-
-if __name__ == '__main__':
-    unittest.main()
+        assert ias.props == props
--- Supervisor/src/integrationTest/python/test_py_ack.py
+++ Supervisor/src/integrationTest/python/test_py_ack.py
@@ -21,11 +21,9 @@ than waiting for a resonable time before getting the alarms
 """
 
 
-import unittest
 import subprocess
 import time
 import uuid
-import logging
 
 from IASLogging.logConf import Log
 from IasCmdReply.IasCommandSender import IasCommandSender
@@ -78,7 +76,7 @@ class IasioListener(IasValueListener):
             self.last_alarm_received = Alarm.fromString(iasValue.value)
             logger.info("Alarm received %s", self.last_alarm_received.to_string())
 
-class TestPyAck(unittest.TestCase):
+class TestPyAck():
 
     # The ID of the Supervisor to run
     supervisorId = "SupervisorWithKafka"
@@ -133,7 +131,7 @@ class TestPyAck(unittest.TestCase):
 
 
     @classmethod
-    def setUpClass(cls):
+    def setup_class(cls):
         """
         Run the Supervisor, the listeners etc.
         """
@@ -161,7 +159,7 @@ class TestPyAck(unittest.TestCase):
         logger.info("Test set up")
 
     @classmethod
-    def tearDownClass(cls):
+    def teardown_class(cls):
         """
         Terminate the Supervisor, the listeners, etc.
         """
@@ -179,7 +177,7 @@ class TestPyAck(unittest.TestCase):
             # Throws TimeoutExpired if the Supervisor does not terminate
             TestPyAck.completedSupervisorPopen.wait(10)
 
-    def testAckFromPython(self):
+    def test_ack_from_python(self):
         logger.info("Test started")
 
         value = TestPyAck.buildIasio(0)
@@ -187,9 +185,9 @@ class TestPyAck(unittest.TestCase):
         # Give the supervisor time to produce the alarm 
         time.sleep(10)
         alarm = TestPyAck.iasioListener.last_alarm_received
-        self.assertIsNotNone(alarm)
-        self.assertFalse(alarm.is_set())
-        self.assertTrue(alarm.is_acked())
+        assert alarm is not None
+        assert not alarm.is_set()
+        assert alarm.is_acked()
 
         # set the alarm
         value = TestPyAck.buildIasio(100)
@@ -197,9 +195,9 @@ class TestPyAck(unittest.TestCase):
         # Give the supervisor time to produce the alarm 
         time.sleep(10)
         alarm = TestPyAck.iasioListener.last_alarm_received
-        self.assertIsNotNone(alarm)
-        self.assertTrue(alarm.is_set())
-        self.assertFalse(alarm.is_acked())
+        assert alarm is not None
+        assert alarm.is_set()
+        assert not alarm.is_acked()
 
         # Send the ACk command to the Supervisor
         logger.info("Sending the ACK to the Supervisor")
@@ -210,267 +208,15 @@ class TestPyAck(unittest.TestCase):
                                               ack_cmd_params,
                                               None,
                                               30)
-        self.assertIsNotNone(reply)
-        self.assertEqual(reply.exitStatus, reply.exitStatus.OK)
+        assert reply  is not None
+        assert reply.exitStatus == reply.exitStatus.OK
         logger.info("Reply from cmd %s", str(reply.exitStatus.name))
 
         # Give the supervisor time to produce the ACK'ed alarm 
         time.sleep(10)
         alarm = TestPyAck.iasioListener.last_alarm_received
-        self.assertIsNotNone(alarm)
-        self.assertTrue(alarm.is_set())
-        self.assertTrue(alarm.is_acked())
+        assert alarm  is not None
+        assert alarm.is_set()
+        assert alarm.is_acked()
 
         logger.info("Test done")
-
-if __name__ == "__main__":
-    unittest.main()
-##############################################################
-
-# class Identifier:
-#     """
-#     Class representing an identifier.
-
-#     Attributes:
-#         value (str): The value of the identifier.
-#         identifier_type (str): The type of the identifier.
-#     """
-
-#     def __init__(self, value: str, identifier_type: str):
-#         self.value = value
-#         self.identifier_type = identifier_type
-
-# class Process:
-#     """
-#     Class representing a process.
-
-#     Attributes:
-#         command (List[str]): The command to run in the process.
-#     """
-
-#     def __init__(self, command: List[str]):
-#         self.command = command
-
-# class CommandSender:
-#     """
-#     Class for sending commands to a supervisor.
-
-#     Attributes:
-#         kafka_producer (KafkaProducer): The Kafka producer used to send commands.
-#     """
-
-#     def __init__(self, kafka_brokers: str, client_id: str):
-#         self.kafka_producer = KafkaProducer(bootstrap_servers=kafka_brokers, client_id=client_id)
-
-#     def send_sync(self, topic_name: str, command_type: str, params: List[str], props: dict, timeout: int):
-#         """
-#         Send a synchronous command to the supervisor.
-
-#         Args:
-#             topic_name (str): The name of the Kafka topic.
-#             command_type (str): The type of the command.
-#             params (List[str]): The parameters of the command.
-#             props (dict): Additional properties for the command.
-#             timeout (int): The timeout in seconds.
-
-#         Returns:
-#             dict: The result of sending the command, containing an exit status.
-#         """
-#         try:
-#             self.kafka_producer.send(topic_name, value={"type": command_type, "params": params})
-#             return {"exit_status": 0}
-#         except Exception as e:
-#             logging.error(f"Error sending command to supervisor: {e}")
-#             return {"exit_status": -1}
-
-#     def send_async(self, topic_name: str, command_type: str, params: List[str], props: dict):
-#         """
-#         Send an asynchronous command to the supervisor.
-
-#         Args:
-#             topic_name (str): The name of the Kafka topic.
-#             command_type (str): The type of the command.
-#             params (List[str]): The parameters of the command.
-#             props (dict): Additional properties for the command.
-#         """
-#         self.kafka_producer.send(topic_name, value={"type": command_type, "params": params})
-
-# class IasValue:
-#     """
-#     Class representing an IASIO.
-
-#     Attributes:
-#         value (float): The value of the IASIO.
-#         operational_mode (str): The operational mode of the IASIO.
-#         validity (str): The validity of the IASIO.
-#         client_id (str): The client ID of the IASIO.
-#         data_type (str): The data type of the IASIO.
-#         t0 (int): The first timestamp of the IASIO.
-#         t1 (int): The second timestamp of the IASIO.
-#         t2 (int): The third timestamp of the IASIO.
-#         t3 (int): The fourth timestamp of the IASIO.
-#         t4 (int): The fifth timestamp of the IASIO.
-#         t5 (int): The sixth timestamp of the IASIO.
-#         alarm_status (str): The alarm status of the IASIO.
-#         cleared_status (str): The cleared status of the IASIO.
-#     """
-
-#     def __init__(self, value: float, operational_mode: str, validity: str, client_id: str, data_type: str, t0: int, t1: int, t2: int, t3: int, t4: int, t5: int, alarm_status: str, cleared_status: str):
-#         self.value = value
-#         self.operational_mode = operational_mode
-#         self.validity = validity
-#         self.client_id = client_id
-#         self.data_type = data_type
-#         self.t0 = t0
-#         self.t1 = t1
-#         self.t2 = t2
-#         self.t3 = t3
-#         self.t4 = t4
-#         self.t5 = t5
-#         self.alarm_status = alarm_status
-#         self.cleared_status = cleared_status
-
-# class IasiosProducer:
-#     """
-#     Class for producing IASIOs.
-
-#     Attributes:
-#         kafka_producer (KafkaProducer): The Kafka producer used to produce IASIOs.
-#     """
-
-#     def __init__(self, kafka_brokers: str, topic_name: str):
-#         self.kafka_producer = KafkaProducer(bootstrap_servers=kafka_brokers)
-
-#     def send_iasio(self, ias_value: IasValue):
-#         """
-#         Send an IASIO to the supervisor.
-
-#         Args:
-#             ias_value (IasValue): The IASIO to be sent.
-#         """
-#         self.kafka_producer.send(topic_name, value=ias_value.__dict__)
-
-# class IasiosConsumer:
-#     """
-#     Class for consuming IASIOs.
-
-#     Attributes:
-#         kafka_consumer (KafkaConsumer): The Kafka consumer used to consume IASIOs.
-#     """
-
-#     def __init__(self, kafka_brokers: str, topic_name: str, client_id: str):
-#         self.kafka_consumer = KafkaConsumer(bootstrap_servers=kafka_brokers, group_id=client_id)
-
-#     def start_getting_events(self):
-#         """
-#         Start getting events from the IASIOs.
-
-#         Returns:
-#             None
-#         """
-#         for message in self.kafka_consumer:
-#             logging.info(f"Received IASIO: {message.value}")
-#             # Process the received IASIO
-#             ias_value = IasValue(**message.value)
-#             alarms_received[ias_value.client_id] = ias_value
-
-# class SupervisorProcess:
-#     """
-#     Class representing a supervisor process.
-
-#     Attributes:
-#         supervisor_proc (Process): The supervisor process.
-#     """
-
-#     def __init__(self, kafka_brokers: str):
-#         self.supervisor_proc = Process(["supervisor", "SupervisorWithKafka"])
-
-#     def start(self):
-#         """
-#         Start the supervisor process.
-
-#         Returns:
-#             None
-#         """
-#         try:
-#             self.supervisor_proc.command.run()
-#             logging.info("Supervisor started")
-#         except Exception as e:
-#             logging.error(f"Error starting supervisor: {e}")
-
-# class TestHarness:
-#     """
-#     Class for testing IASIOs.
-
-#     Attributes:
-#         kafka_producer (KafkaProducer): The Kafka producer used to send commands.
-#         supervisor_process (SupervisorProcess): The supervisor process.
-#     """
-
-#     def __init__(self, kafka_brokers: str):
-#         self.kafka_producer = KafkaProducer(bootstrap_servers=kafka_brokers)
-#         self.supervisor_process = SupervisorProcess(kafka_brokers)
-
-#     def before_all(self):
-#         """
-#         Method to be called before all tests.
-
-#         Returns:
-#             None
-#         """
-#         logging.info("Starting test")
-#         self.supervisor_process.start()
-#         time.sleep(15)  # Wait for supervisor to start
-#         self.iasios_consumer.start_getting_events()
-
-#     def after_all(self):
-#         """
-#         Method to be called after all tests.
-
-#         Returns:
-#             None
-#         """
-#         logging.info("Stopping test")
-#         self.iasios_consumer.stop_getting_events()
-#         self.kafka_producer.close()
-
-# alarms_received = {}
-
-# class TestPyAck(unittest.TestCase):
-
-#     def setUp(self):
-#         """
-#         Setup method for the test class.
-
-#         Returns:
-#             None
-#         """
-#         self.kafka_brokers = "localhost:9092"
-#         self.topic_name = "iasio_topic"
-#         self.client_id = uuid4()
-
-#     def test_send_iasio(self):
-#         """
-#         Test sending an IASIO.
-
-#         Returns:
-#             None
-#         """
-#         iasios_prod = IasiosProducer(self.kafka_brokers, self.topic_name)
-#         ias_value = IasValue(5.0, "operational", "reliable", self.client_id, "double", 100, 101, 102, 103, 104, 105, None, None)
-#         iasios_prod.send_iasio(ias_value)
-
-#     def test_send_async(self):
-#         """
-#         Test sending an asynchronous command.
-
-#         Returns:
-#             None
-#         """
-#         cmd_sender = CommandSender(self.kafka_brokers, self.client_id)
-#         reply = cmd_sender.send_sync("ack_topic", "ACK", [self.client_id], {"comment": "User provided comment for ACK"}, 15)
-#         assert reply["exit_status"] == 0
-
-
-
-
