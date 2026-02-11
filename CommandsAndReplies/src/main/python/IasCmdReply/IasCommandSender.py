84577d5c27bf12ae215362d75d41cc927c3a69e7
Alessandro Caproni <acaproni@eso.org>
2026-02-11 17:05:48 +0100

    Renames some parameter to improve readability #296

--- CommandsAndReplies/src/main/python/IasCmdReply/IasCommandSender.py
+++ CommandsAndReplies/src/main/python/IasCmdReply/IasCommandSender.py
@@ -24,7 +24,7 @@ class IasCommandSender(IasLogListener):
 
     """
 
-    def __init__(self, sender_full_running_id: str, sender_id: str, brokers: str, string_producer: Producer|None=None):
+    def __init__(self, sender_full_running_id: str, bsdb_sender_id: str, brokers: str, string_producer: Producer|None=None):
         """
         Constructor
 
@@ -32,17 +32,17 @@ class IasCommandSender(IasLogListener):
             senderFullRuningId The full runing id of the sender
             stringProducer The string producer to publish commands
                            (if None builds a new producer)
-            senderId The id of the sender
+            senderId The id of the sender (i.e. the BSDB client.id of the Producer and the Consumer)
             brokers URL of kafka brokers
         """
         if not sender_full_running_id:
             raise ValueError("Invalid null/empty full running ID of the sender")
         self.logger = Log.getLogger(__name__)
         self.sender_full_running_id = sender_full_running_id
-        self.sender_id = sender_id
+        self.bsdb_sender_id = bsdb_sender_id
 
         # Kafka producer of comands
-        conf = { 'bootstrap.servers': brokers, 'client.id': sender_id}
+        conf = { 'bootstrap.servers': brokers, 'client.id': bsdb_sender_id}
         if string_producer is None:
             self.cmd_producer = Producer(conf)
         else:
@@ -53,8 +53,8 @@ class IasCommandSender(IasLogListener):
             listener=self,
             kafkabrokers=brokers,
             topic=IasKafkaHelper.topics['reply'],
-            clientid=sender_id,
-            groupid=sender_id)
+            clientid=bsdb_sender_id,
+            groupid=bsdb_sender_id)
         
         self.cmd_id = 0
         self.request_reply_in_progress = False
