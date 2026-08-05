dbfc3a4ffd50f5c635dfec35fea8025835867a40
Alessandro Caproni <acaproni@eso.org>
2026-08-05 10:48:26 +0200

    #339 Adds a missing junit test

--- CommandsAndReplies/src/test/runIntTests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-iasRun -r org.junit.platform.console.ConsoleLauncher execute \
-	--select-class org.eso.ias.command.test.TestCommandManager \
-	--select-class org.eso.ias.command.test.TestCommandSender \
-	--reports-dir build/integration-test-results/junit
-pytest src/integrationTest/python --junitxml="build/integration-test-results/pytest/TEST-CommandsAndReply-pytest.xml"
